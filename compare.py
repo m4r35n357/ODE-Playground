99c9dedef92edb4c3140be5ebe7284591ec9462a
Ian Smith <m4r35n357@gmail.com>
2018-10-21 17:58:52 +0100

    add divergence stuff

---
+++ compare.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from math import sqrt
+from sys import argv, stderr
+
+from matplotlib import pyplot
+
+
+def position_error(a, b):
+    return sqrt((float(a[0]) - float(b[0]))**2 + (float(a[1]) - float(b[1]))**2 + (float(a[2]) - float(b[2]))**2)
+
+
+def main():
+    print("Compare: {}".format(argv))
+    if len(argv) != 4:
+        raise Exception('>>> ERROR! Please supply two file names and a time coordinate <<<')
+    time = int(argv[3])
+    ax1 = pyplot.figure().add_subplot(111)
+    pyplot.grid(b=True, color='0.25', linestyle='-')
+    ax1.set_xlabel('time', color='b')
+    ax1.set_ylabel('x, y', color='b')
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('z, error', color='b')
+    with open(argv[1], 'r') as a, open(argv[2], 'r') as b:
+        line_a = a.readline()
+        line_b = b.readline()
+        s = []
+        t = []
+        u = []
+        v = []
+        w = []
+        x = []
+        y = []
+        z = []
+        while line_a and line_b:
+            if 'nan' in line_a or 'nan' in line_b:
+                break
+            data_a = line_a.split(' ')
+            data_b = line_b.split(' ')
+            s.append(float(data_a[time]))
+            t.append(float(data_a[0]))
+            u.append(float(data_b[0]))
+            v.append(float(data_a[1]))
+            w.append(float(data_b[1]))
+            x.append(float(data_a[2]))
+            y.append(float(data_b[2]))
+            z.append(position_error(data_a, data_b))
+            line_a = a.readline()
+            line_b = b.readline()
+    ax1.plot(s, t, 'ko-', markersize=0)
+    ax1.plot(s, u, 'go-', markersize=0)
+    ax1.plot(s, v, 'ko-', markersize=0)
+    ax1.plot(s, w, 'yo-', markersize=0)
+    ax2.plot(s, x, 'ko-', markersize=0)
+    ax2.plot(s, y, 'co-', markersize=0)
+    ax2.plot(s, z, 'ro-', markersize=0)
+    pyplot.show()
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print(__name__ + " module loaded", file=stderr)
---
+++ divergence.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from math import sqrt
+from sys import argv, stderr
+
+RED = '\x1b[1;31m'
+GREEN = '\x1b[0;32m'
+ORANGE = '\x1b[0;33m'
+YELLOW = '\x1b[1;33m'
+BLUE = '\x1b[1;34m'
+CYAN = '\x1b[0;36m'
+GREY = '\x1B[0m\x1b[2;37m'
+WHITE = '\x1b[0;37m'
+NORMAL = '\x1B[0m'
+
+
+def diverged(x, y, thresh):
+    return sqrt(
+        (float(x[0]) - float(y[0])) ** 2 + (float(x[1]) - float(y[1])) ** 2 + (float(x[2]) - float(y[2])) ** 2) > thresh
+
+
+def main():
+    print("Divergence: {}".format(argv))
+    if len(argv) != 5:
+        raise Exception('>>> ERROR! Please supply two file names and a time column and a threshold <<<')
+    time = int(argv[3])
+    threshold = float(argv[4])
+    if threshold <= 1.0e-18:
+        colour = BLUE
+    elif threshold <= 1.0e-12:
+        colour = GREEN
+    elif threshold <= 1.0e-9:
+        colour = CYAN
+    elif threshold <= 1.0e-6:
+        colour = YELLOW
+    elif threshold <= 1.0e-3:
+        colour = ORANGE
+    else:
+        colour = RED
+    with open(argv[1], 'r') as a, open(argv[2], 'r') as b:
+        line_a = a.readline()
+        line_b = b.readline()
+        while line_a and line_b:
+            data_a = line_a.split(' ')
+            data_b = line_b.split(' ')
+            if diverged(data_a, data_b, threshold):
+                print("{}Threshold: {}{:.1e}{}, Time: {}{:.3f}{}".format(GREY, colour, threshold, GREY, WHITE,
+                                                                         float(data_a[time]), NORMAL), file=stderr)
+                return
+            line_a = a.readline()
+            line_b = b.readline()
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print(__name__ + " module loaded", file=stderr)
