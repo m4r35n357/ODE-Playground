1b196a442ef17108b50c35955033a7401f5c3353
Ian Smith <m4r35n357@gmail.com>
2018-12-17 22:44:53 +0000

    report function values at critical points, not derivatives return results in a named tuple

--- playground.py
+++ playground.py
@@ -1,8 +1,8 @@
 #
 #  (c) 2018 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 #
-
 from sys import argv, stderr
+from collections import namedtuple
 from enum import Enum
 from taylor import t_jet
 from series import Series
@@ -14,11 +14,14 @@ class Solver(Enum):
     INFLECTION = 2
 
 
+Result = namedtuple('ResultType', ['count', 'mode', 'x', 'f', 'dx'])
+
+
 def _print_output(output):
-    print("    {:3d} {:22.15e} {:10.3e} {:10.3e}".format(*output), file=stderr)
+    print("    {:3d} {} {:22.15e} {:10.3e} {:10.3e}".format(*output), file=stderr)
 
 
-def bisect(model, ax, bx, f_tol, x_tol, target=0.0, max_it=100, mode=Solver.ROOT):
+def bisect(model, ax, bx, f_tol, x_tol, max_it, target=0.0, mode=Solver.ROOT):
     a = Series(t_jet(3, ax), diff=True)
     b = Series(t_jet(3, bx), diff=True)
     c = Series(t_jet(3))
@@ -37,10 +40,10 @@ def bisect(model, ax, bx, f_tol, x_tol, target=0.0, max_it=100, mode=Solver.ROOT
         counter += 1
         if counter > max_it:
             break
-    return counter, c.val, fc.jet[mode.value] + target, delta
+    return Result(count=counter, mode=mode.name, x=c.val, f=fc.val + target, dx=delta)
 
 
-def newton(model, initial, f_tol, x_tol, target=0.0, max_it=100, mode=Solver.ROOT):
+def newton(model, initial, f_tol, x_tol, max_it, target=0.0, mode=Solver.ROOT):
     x = Series(t_jet(2 + mode.value, initial), diff=True)
     f = Series(t_jet(2 + mode.value, 1.0))
     delta = 1.0
@@ -52,11 +55,12 @@ def newton(model, initial, f_tol, x_tol, target=0.0, max_it=100, mode=Solver.ROO
         counter += 1
         if counter > max_it:
             break
-    return counter, x.val, f.jet[mode.value] + target, delta
+    return Result(count=counter, mode=mode.name, x=x.val, f=f.val + target, dx=delta)
 
 
 def analyze(model):
-    n_max = 13
+    n_max = 7
+    max_it = 100
     n = int(argv[1])
     assert n == 0 or n == 1 or n == 2
     x0 = float(argv[2])
@@ -68,6 +72,7 @@ def analyze(model):
     f_tol = float(argv[6])
     x_tol = float(argv[7])
     w_x = Series(t_jet(n_max, x0), diff=True)
+    results = []
     if n != 0:
         if n == 1:
             print("Bisection", file=stderr)
@@ -84,9 +89,15 @@ def analyze(model):
                     print("ROOT", file=stderr)
                     if n == 1:
                         # noinspection PyUnboundLocalVariable
-                        _print_output(bisect(model, w_x.val, x_prev, f_tol, x_tol, target=target))
+                        result = bisect(model, w_x.val, x_prev, f_tol, x_tol, max_it, target=target)
+                        _print_output(result)
+                        if result.count < max_it:
+                            results.append(result)
                     elif n == 2:
-                        _print_output(newton(model, w_x.val, f_tol, x_tol, target=target))
+                        result = newton(model, w_x.val, f_tol, x_tol, max_it, target=target)
+                        _print_output(result)
+                        if result.count < max_it:
+                            results.append(result)
                 # noinspection PyUnboundLocalVariable
                 if f_dash_prev * w_f.jet[1] < 0.0:
                     if f_dash_prev > w_f.jet[1]:
@@ -95,9 +106,15 @@ def analyze(model):
                         print("MINIMUM", file=stderr)
                     if n == 1:
                         # noinspection PyUnboundLocalVariable
-                        _print_output(bisect(model, w_x.val, x_prev, f_tol, x_tol, mode=Solver.EXTREMUM))
+                        result = bisect(model, w_x.val, x_prev, f_tol, x_tol, max_it, mode=Solver.EXTREMUM)
+                        _print_output(result)
+                        if result.count < max_it:
+                            results.append(result)
                     elif n == 2:
-                        _print_output(newton(model, w_x.val, f_tol, x_tol, mode=Solver.EXTREMUM))
+                        result = newton(model, w_x.val, f_tol, x_tol, max_it, mode=Solver.EXTREMUM)
+                        _print_output(result)
+                        if result.count < max_it:
+                            results.append(result)
                 # noinspection PyUnboundLocalVariable
                 if f_dash_dash_prev * w_f.jet[2] < 0.0:
                     if f_dash_dash_prev > w_f.jet[2]:
@@ -106,13 +123,20 @@ def analyze(model):
                         print("-INFLECTION", file=stderr)
                     if n == 1:
                         # noinspection PyUnboundLocalVariable
-                        _print_output(bisect(model, w_x.val, x_prev, f_tol, x_tol, mode=Solver.INFLECTION))
+                        result = bisect(model, w_x.val, x_prev, f_tol, x_tol, max_it, mode=Solver.INFLECTION)
+                        _print_output(result)
+                        if result.count < max_it:
+                            results.append(result)
                     elif n == 2:
-                        _print_output(newton(model, w_x.val, f_tol, x_tol, mode=Solver.INFLECTION))
+                        result = newton(model, w_x.val, f_tol, x_tol, max_it, mode=Solver.INFLECTION)
+                        _print_output(result)
+                        if result.count < max_it:
+                            results.append(result)
         x_prev = w_x.val
         f_prev = w_f.val
         f_dash_prev = w_f.jet[1]
         f_dash_dash_prev = w_f.jet[2]
+    return results
 
 
 print(__name__ + " module loaded", file=stderr)
