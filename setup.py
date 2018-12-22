998e047f9e6490abd981bee75ced7c4bc43b7eba
Ian Smith <m4r35n357@gmail.com>
2018-12-22 12:21:48 +0000

    setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='ad',
+      description='Solve ODEs Using the Taylor Series Method (Automatic Differentiation)',
+      version='1.0',
+      py_modules=['taylor', 'series', 'playground'],
+      author='Ian Smith',
+      author_email='m4r35n357@gmail.com',
+      url='https://github.com/m4r35n357/ODE-Playground'
+      )
