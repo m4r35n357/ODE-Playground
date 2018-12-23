1208007da12e39a0474d99db2c3bbfd73a308e8e
Ian Smith <m4r35n357@gmail.com>
2018-12-23 11:31:46 +0000

    add gmpy2 requirement

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 
 setup(name='ad',
       description='Solve ODEs Using the Taylor Series Method (Automatic Differentiation)',
-      version='1.0',
-      py_modules=['taylor', 'series', 'playground'],
       author='Ian Smith',
       author_email='m4r35n357@gmail.com',
-      url='https://github.com/m4r35n357/ODE-Playground'
+      version='1.0',
+      url='https://github.com/m4r35n357/ODE-Playground', requires=['gmpy2'],
+      py_modules=['taylor', 'series', 'playground'],
       )
