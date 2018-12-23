87297e976ce813289eee0ae2e86333404d938d69
Ian Smith <m4r35n357@gmail.com>
2018-12-23 10:27:58 +0000

    add a simpler constructor for interactive use complex checks pointless for gmpy2

--- series.py
+++ series.py
@@ -1,8 +1,8 @@
 #
 #  (c) 2018 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 #
-
 from sys import stderr
+from gmpy2 import mpfr
 from taylor import t_jet, t_prod, t_quot, t_sqr, t_exp, t_sin_cos, t_tan_sec2, t_pwr, t_ln, t_sqrt, t_asin, t_acos, \
     t_atan, D1
 
@@ -14,6 +14,11 @@ class Series:
         self.n = len(self.jet)
         if variable:
             self.jet[1] = D1
+
+    @classmethod
+    def from_numbers(cls, order, value, variable=False):
+        # noinspection PyArgumentList
+        return cls(t_jet(order, mpfr(value)), variable)
 
     def __str__(self):
         string = ""
@@ -43,13 +48,11 @@ class Series:
             assert len(other.jet) == self.n
             return Series([self.jet[k] + other.jet[k] for k in range(self.n)])
         else:
-            assert not isinstance(other, complex)
             add_jet = [self.jet[k] for k in range(self.n)]
             add_jet[0] += other
             return Series(add_jet)
 
     def __radd__(self, other):
-        assert not isinstance(other, complex)
         return self.__add__(other)
 
     def __sub__(self, other):
@@ -57,13 +60,11 @@ class Series:
             assert len(other.jet) == self.n
             return Series([self.jet[k] - other.jet[k] for k in range(self.n)])
         else:
-            assert not isinstance(other, complex)
             sub_jet = [self.jet[k] for k in range(self.n)]
             sub_jet[0] -= other
             return Series(sub_jet)
 
     def __rsub__(self, other):
-        assert not isinstance(other, complex)
         sub_jet = [- self.jet[k] for k in range(self.n)]
         sub_jet[0] += other
         return Series(sub_jet)
@@ -73,11 +74,9 @@ class Series:
             assert len(other.jet) == self.n
             return Series([t_prod(self.jet, other.jet, k) for k in range(self.n)])
         else:
-            assert not isinstance(other, complex)
             return Series([self.jet[k] * other for k in range(self.n)])
 
     def __rmul__(self, other):
-        assert not isinstance(other, complex)
         return self.__mul__(other)
 
     def __truediv__(self, other):
@@ -87,13 +86,11 @@ class Series:
             for k in range(self.n):
                 div_jet[k] = t_quot(div_jet, self.jet, other.jet, k)
         else:
-            assert not isinstance(other, complex)
             for k in range(self.n):
                 div_jet[k] = self.jet[k] / other
         return Series(div_jet)
 
     def __rtruediv__(self, other):
-        assert not isinstance(other, complex)
         other_jet = t_jet(self.n, other)
         rdiv_jet = t_jet(self.n)
         for k in range(self.n):
@@ -101,7 +98,6 @@ class Series:
         return Series(rdiv_jet)
 
     def __pow__(self, a):
-        assert not isinstance(a, complex)
         pow_jet = t_jet(self.n)
         for k in range(self.n):
             pow_jet[k] = t_pwr(pow_jet, self.jet, a, k)
