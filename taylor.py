81d082bc2e6326cc4bf03686ffcd3480a8cd6508
Ian Smith <m4r35n357@gmail.com>
2018-12-28 11:11:16 +0000

    use _ddot() for square and root

--- taylor.py
+++ taylor.py
@@ -26,38 +26,30 @@ def t_horner(jet, n, h):
     return result
 
 
-def t_sqr(u, k):
-    if k == 0:
-        return u[0]**2
-    else:
-        if k % 2 == 1:
-            return D2 * sum(u[j] * u[k - j] for j in range((k - 1) // 2 + 1))
-        else:
-            return D2 * sum(u[j] * u[k - j] for j in range((k - 2) // 2 + 1)) + u[k // 2]**2
-
-
 def t_prod(u, v, k):
     return sum(u[j] * v[k - j] for j in range(k + 1))
 
 
 def t_quot(q, u, v, k):
-    assert abs(v[0]) != D0
     return (u[k] - sum(v[j] * q[k - j] for j in range(1, k + 1))) / v[0]
-
-
-def t_sqrt(r, u, k):
-    assert u[0] > D0
-    if k == 0:
-        return sqrt(u[0])
-    else:
-        if k % 2 == 1:
-            return (u[k] - D2 * sum(r[j] * r[k - j] for j in range(1, (k - 1) // 2 + 1))) / (D2 * r[0])
-        else:
-            return (u[k] - D2 * sum(r[j] * r[k - j] for j in range(1, (k - 2) // 2 + 1)) - r[k // 2]**2) / (D2 * r[0])
 
 
 def _ddot(v, u, k):
     return sum(j * u[j] * v[k - j] for j in range(1, k)) / k
+
+
+def t_sqr(u, k):
+    if k == 0:
+        return u[0]**2
+    else:
+        return D2 * (u[0] * u[k] + _ddot(u, u, k))
+
+
+def t_sqrt(r, u, k):
+    if k == 0:
+        return sqrt(u[0])
+    else:
+        return (u[k] / D2 - _ddot(r, r, k)) / r[0]
 
 
 def t_exp(e, u, k):
@@ -85,6 +77,33 @@ def t_tan_sec2(t, s2, u, k, hyp=False):
         return tn, - s2 if hyp else s2
 
 
+def t_pwr(p, u, a, k):
+    if k == 0:
+        return u[0]**a
+    else:
+        return (a * (p[0] * u[k] + _ddot(p, u, k)) - _ddot(u, p, k)) / u[0]
+
+
+def t_ln(l, u, k):
+    if k == 0:
+        return log(u[0])
+    else:
+        return (u[k] - _ddot(u, l, k)) / u[0]
+
+
+def t_atan(h, v, u, k):
+    if k == 0:
+        return atan(u[0]), D1 + u[0]**2
+    else:
+        return (u[k] - _ddot(v, h, k)) / v[0], D2 * (u[0] * u[k] + _ddot(u, u, k))
+
+
+def _arc(h, v, u, k):
+    h_jet = (u[k] - _ddot(v, h, k)) / v[0]
+    v_jet = u[0] * h_jet + _ddot(u, h, k)
+    return h_jet, - v_jet
+
+
 def t_asin(h, v, u, k):
     if k == 0:
         h = asin(u[0])
@@ -99,37 +118,6 @@ def t_acos(h, v, u, k):
         return h, - sin(h)
     else:
         return _arc(h, v, u, k)
-
-
-def _arc(h, v, u, k):
-    assert abs(v[0]) != D0
-    h_jet = (u[k] - _ddot(v, h, k)) / v[0]
-    v_jet = u[0] * h_jet + _ddot(u, h, k)
-    return h_jet, - v_jet
-
-
-def t_atan(h, v, u, k):
-    if k == 0:
-        return atan(u[0]), D1 + u[0]**2
-    else:
-        assert abs(v[0]) != D0
-        return (u[k] - _ddot(v, h, k)) / v[0], D2 * (u[0] * u[k] + _ddot(u, u, k))
-
-
-def t_pwr(p, u, a, k):
-    assert abs(u[0]) != D0
-    if k == 0:
-        return u[0]**a
-    else:
-        return (a * (p[0] * u[k] + _ddot(p, u, k)) - _ddot(u, p, k)) / u[0]
-
-
-def t_ln(l, u, k):
-    assert u[0] > D0
-    if k == 0:
-        return log(u[0])
-    else:
-        return (u[k] - _ddot(u, l, k)) / u[0]
 
 
 print(__name__ + " module loaded", file=stderr)
