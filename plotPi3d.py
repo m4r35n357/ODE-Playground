f0856801351a85958b3d9f3d70b796a0e57b582b
Ian Smith <m4r35n357@gmail.com>
2019-01-04 20:19:56 +0000

    add basic HUD for time and position to trajectory plots remove dependency on numpy remove __future__ imports

--- plotPi3d.py
+++ plotPi3d.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from numpy import sin, cos, radians, array
+from math import sin, cos, pi
 from sys import stdin, stderr, argv
-
-from pi3d import Sphere, Display, Camera, Shader, Keyboard, screenshot, Lines
+from pi3d import Sphere, Display, Camera, Shader, Keyboard, screenshot, Lines, Font, String
 
 
 class Body(Sphere):
     def __init__(self, shader, colour, radius, position=(0.0, 0.0, 0.0), track_shader=None, track_max=5000):
         super(Body, self).__init__(radius=radius, x=position[0], y=position[1], z=position[2])
         super(Body, self).set_draw_details(shader, [])
-        self.pos = array(position)
+        self.pos = position
         self.track_shader = track_shader
         self.vertices = []
         self.track_length = 0
         self.track_max = track_max
-        self.trace_shape = None
+        self.track = None
         self.set_material(colour)
         self.pointer = Lines(vertices=[(0, 0, 0,), (0, 0, 0,)])
 
-    def position_and_draw(self, trace_material=(0.5, 0.5, 0.5), pointer_material=(0.5, 0.5, 0.5)):
+    def position_and_draw(self, trace_material):
         # body
         self.position(self.pos[0], self.pos[1], self.pos[2])
         self.draw()
-        self.pointer = Lines(vertices=[(0, 0, 0,), self.pos], material=pointer_material)
+        self.pointer = Lines(vertices=[(0, 0, 0,), self.pos], material=(1.0, 1.0, 0.0))
         self.pointer.draw()
         # track
         if self.track_shader:
             self.track_length += 1
-            self.vertices.append(tuple(self.pos))
-            self.trace_shape = Lines(vertices=self.vertices, material=trace_material)
-            self.trace_shape.set_shader(self.track_shader)
+            self.vertices.append(self.pos)
+            self.track = Lines(vertices=self.vertices, material=trace_material)
+            self.track.set_shader(self.track_shader)
             if self.track_length > self.track_max:
                 self.vertices = self.vertices[-self.track_max:]
                 self.track_length = self.track_max
-            if self.trace_shape:
-                self.trace_shape.draw()
+            if self.track:
+                self.track.draw()
+
+
+def radians(degrees):
+    return pi * degrees / 180.0
 
 
 def main():
@@ -49,8 +50,15 @@ def main():
     rot = tilt = 0
     rot_tilt = True
     cam_rad = 50.0
-    # Entities
-    axis = Lines(vertices=[(0, 0, 50,), (0, 0, -50,)], material=(1, 1, 1))
+    hud_font = Font('/usr/share/fonts/truetype/liberation2/LiberationMono-Regular.ttf', color='green',
+                    codepoints='-0123456789. txyz:=+', font_size=18)
+    hud_font.blend = True
+    hud_data = " t {:-5.1f}  x {:-5.1f}  y {:-5.1f}  z {:-5.1f} ".format(0.0, 0.0, 0.0, 0.0)
+    hud_string = String(camera=Camera(is_3d=False), font=hud_font, is_3d=False, string=hud_data)
+    hud_string.set_shader(Shader("uv_flat"))
+    (lt, bm, ft, rt, tp, bk) = hud_string.get_bounds()
+    hud_string.position((-display.width + rt - lt) / 2.0, (0.9 * display.height - tp + bm) / 2.0, 1.0)
+    hud_string.draw()  # NB has to be drawn before quick_change() is called as buffer needs to exist
 
     if len(argv) > 1:
         particle = Body(Shader("mat_light"), (1.0, 0.0, 0.0), 0.05, track_shader=Shader("mat_flat"),
@@ -63,6 +71,10 @@ def main():
     line = stdin.readline()
     while display.loop_running():
         data = line.split(' ')
+        hud_string.quick_change(
+            " t {:-5.1f}  x {:-5.1f}  y {:-5.1f}  z {:-5.1f} ".format(float(data[3]), float(data[0]), float(data[1]),
+                                                                      float(data[2])))
+        hud_string.draw()
         # camera control
         if rot_tilt:
             camera.reset()
@@ -71,7 +83,6 @@ def main():
                              -cam_rad * cos(radians(rot)) * cos(radians(tilt))))
             rot_tilt = False
         # plot the entities
-        # axis.draw()
         particle.pos = [float(data[0]), float(data[1]), float(data[2])]
         particle.position_and_draw(trace_material=(0.0, 0.25, 0.25))
         # process keyboard input
@@ -94,7 +105,7 @@ def main():
                 cam_rad += 0.5
             elif key == 27:
                 keys.close()
-                display.destroy()
+                display.stop()
                 break
         # prepare for next iteration
         line = stdin.readline()
--- plotXY.py
+++ plotXY.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from sys import argv, stdin, stderr
-
 from matplotlib import pyplot
 
 
