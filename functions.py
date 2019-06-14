95be43887fa36295d7c9376a0ec2132ead6c74cb
Ian Smith <m4r35n357@gmail.com>
2019-06-14 12:36:36 +0100

    common function code for testing Dual and Series

---
+++ functions.py
@@ -0,0 +1,40 @@
+
+from sys import stderr
+
+def lorentz(a, value):
+    # Example: ./models.py 0 .001 .999 1001 0 1e-12 1e-12 | ./plotMany.py 1 10 >/dev/null
+    return 1 / (1 - a.sqr).sqrt - value
+
+def cosx_x3(a, value):
+    # Example: ./models.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
+    return a.cos - a * a.sqr - value
+
+def septic(a, value):
+    # Example: ./models.py 2 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 50000 >/dev/null
+    return (a + 7) * (5 + a) * (a + 2.0) * a * (1 - a) * (3.0 - a) * (a - 6) - value
+
+def composite1(a, value):
+    # Example: ./models.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
+    # Example: ./series_test.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
+    return (a.exp + (a.sqr - 4.0).exp).ln - value
+
+def composite2(a, value):
+    # Example: ./models.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
+    return (a.sqr + (a.exp - 4).sqr).sqrt - value
+
+def playground(a, value):
+    # Example: ./models.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
+    # return (2 * a).sin - 2 * a.sin * a.cos - value
+    # return (2 * a).cos - a.cos.sqr + a.sin.sqr - value
+    # return (3 * a).sin - 3 * a.sin + 4 * a.sin * a.sin.sqr - value
+    # return (3 * a).cos + 3 * a.cos - 4 * a.cos * a.cos.sqr - value
+    # return a.sqr.sqrt - value
+    # return a.exp.ln - value
+    # return a.tan - a.sin / a.cos - value
+    # return a.tanh - a.sinh / a.cosh - value
+    # return a.sin.asin - value
+    # return a.cos.acos - value
+    # return a.tan.atan - value
+    return (a + 7) / (3.0 - a)
+
+print(__name__ + " module loaded", file=stderr)
--- series_test.py
+++ series_test.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
+from sys import argv
 from gmpy2 import get_context, acos
-from sys import stderr, argv
-
 get_context().precision = 236  # Set this BEFORE importing any Taylor Series stuff!
-from dual import Dual, make_mpfr
+from dual import Dual, to_mpfr
 from series import Series
 from taylor import t_jet, to_mpfr
+from functions import *
 
 pi = acos(-1)
 a = to_mpfr("3.0")
@@ -117,73 +117,28 @@ print(Dual.get(c, variable=True).atan, file=stderr)
 
 print("", file=stderr)
 
-
-def lorentz(a, value):
-    # Example: ./series_test.py 0 .001 .999 1001 0 1e-12 1e-12 | ./plotMany.py 1 10 >/dev/null
-    return 1 / (1 - a.sqr).sqrt - value
-
-
-def cosx_x3(a, value):
-    # Example: ./series_test.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
-    return a.cos - a * a.sqr - value
-
-
-def septic(a, value):
-    # Example: ./series_test.py 2 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 50000 >/dev/null
-    return (a + 7) * (5 + a) * (a + 2.0) * a * (1 - a) * (3.0 - a) * (a - 6) - value
-
-
-def composite1(a, value):
-    # Example: ./series_test.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
-    # Example: ./models.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
-    return (a.exp + (a.sqr - 4.0).exp).ln - value
-
-
-def composite2(a, value):
-    # Example: ./series_test.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
-    return (a.sqr + (a.exp - 4).sqr).sqrt - value
-
-
-def playground(a, value):
-    # Example: ./series_test.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
-    # return (2 * a).sin - 2 * a.sin * a.cos - value
-    # return (2 * a).cos - a.cos.sqr + a.sin.sqr - value
-    # return (3 * a).sin - 3 * a.sin + 4 * a.sin * a.sin.sqr - value
-    # return (3 * a).cos + 3 * a.cos - 4 * a.cos * a.cos.sqr - value
-    # return a.sqr.sqrt - value
-    # return a.exp.ln - value
-    # return a.tan - a.sin / a.cos - value
-    # return a.tanh - a.sinh / a.cosh - value
-    # return a.sin.asin - value
-    # return a.cos.acos - value
-    # return a.tan.atan - value
-    return (a + 7) / (3.0 - a)
-
-
 def schwartzschild(r, e=to_mpfr(0.962250), p_r=to_mpfr(0), l_z=to_mpfr(4)):  # no t or phi!
     # Example: ./series_test.py 1 -8 8 1001 0 1e-12 1e-12 | ./plotMany.py 8 10 >/dev/null
     return (e**2 / (1 - 2 / r) - p_r**2 * (1 - 2 / r) - (l_z / r).sqr) / 2
-
 
 def analyze(model, n, x0, x1, steps=1000, target=0, f_tol=to_mpfr(1e-12), x_tol=to_mpfr(1e-12), max_it=100, n_max=13):
     w_x = Dual.get(x0, variable=True)
     for k in range(steps):
         w_x.val = x0 + k * (x1 - x0) / steps
-        w_f = model(w_x, make_mpfr(target))
+        w_f = model(w_x, to_mpfr(target))
         print("{:.6e} {}".format(w_x.val, w_f))
         yield w_x.val, w_f.val, w_f.der
 
-
 mode = int(argv[1])
 assert mode == 0 or mode == 1 or mode == 2
-x0 = make_mpfr(argv[2])
-x1 = make_mpfr(argv[3])
+x0 = to_mpfr(argv[2])
+x1 = to_mpfr(argv[3])
 assert x1 > x0
 steps = int(argv[4])
 assert steps > 0
-target = make_mpfr(argv[5])
-f_tol = make_mpfr(argv[6])
-x_tol = make_mpfr(argv[7])
+target = to_mpfr(argv[5])
+f_tol = to_mpfr(argv[6])
+x_tol = to_mpfr(argv[7])
 
-for result in analyze(composite1, mode, x0, x1, steps, target, f_tol, x_tol):
+for result in analyze(playground, mode, x0, x1, steps, target, f_tol, x_tol):
     pass
