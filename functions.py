12dab3a94696adff60bfaf2bd6452a4cecdb7aee
Ian Smith <m4r35n357@gmail.com>
2019-07-03 09:46:37 +0100

    updated example functions

--- functions.py
+++ functions.py
@@ -30,16 +30,17 @@ def playground(a):
     # return (2 * a).cos - a.cos.sqr + a.sin.sqr
     # return (3 * a).sin - 3 * a.sin + 4 * a.sin * a.sin.sqr
     # return (3 * a).cos + 3 * a.cos - 4 * a.cos * a.cos.sqr
-    # return a.sqr.sqrt
-    # return (a**2).sqrt
-    # return a.sqr**0.5
-    return 4**a
-    # return a.exp.ln
+    # return a.sqr.sqrt - abs(a)
+    # return (a**2)**0.5 - abs(a)
+    # return a.exp.ln - a
     # return a.tan - a.sin / a.cos
     # return a.tanh - a.sinh / a.cosh
     # return a.sin.asin
     # return a.cos.acos
     # return a.tan.atan
     # return (a + 7) / (3.0 - a)
+    # return a**4
+    return (-4)**a
+    # return (2 + 3 * a)**(2 * a - 5)
 
 print(__name__ + " module loaded", file=stderr)
