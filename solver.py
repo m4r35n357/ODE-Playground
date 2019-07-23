309a73348a2a170371f14fa8a7a2449d9fa2b794
Ian Smith <m4r35n357@gmail.com>
2019-07-23 22:39:16 +0100

    use midpoint of initial bracket for Newton etc

--- solver.py
+++ solver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# Example:  ./solver.py NA 2.0 0.0 1e-12 1e-12 1000 2.0
+# Example:  ./solver.py NA -1.0  1.0 1e-15 1e-15 1000 2.0
+# Example:  ./solver.py NA  1.0  3.0 1e-15 1e-15 1000 2.0
 
 from sys import argv, exit, stderr
 from playground import Solver, bisect, falsi, secant, newton, householder
@@ -19,9 +20,9 @@ x_tol = float(argv[5])
 max_it = int(argv[6])
 target = float(argv[7])
 
-# model = lambda x: x**2 / (x.cosh + 1).ln - 1
-model = lambda x: x**2
-
+model = lambda x: (x - 1)**2 / (x.cosh + 1).ln - 1
+# model = lambda x: x**2
+xc = (xa + xb) / 2.0
 if (method == Solver.NA) or (method == Solver.BI):
     bisect(model, xa, xb, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
     print("")
@@ -35,19 +36,19 @@ if (method == Solver.NA) or (method == Solver.SC):
     secant(model, xa, xb, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
     print("")
 if (method == Solver.NA) or (method == Solver.NT):
-    newton(model, xa, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
+    newton(model, xc, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
     print("")
 if (method == Solver.NA) or (method == Solver.H1):
-    householder(model, xa, 2, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
+    householder(model, xc, 2, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
     print("")
 if (method == Solver.NA) or (method == Solver.H2):
-    householder(model, xa, 3, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
+    householder(model, xc, 3, y=target, εf=f_tol, εx=x_tol, limit=max_it, debug=True)
     print("")
 if (method == Solver.NA) or (method == Solver.H3):
-    householder(model, xa, 4, y=target, εf=f_tol, εx=x_tol, limit=max, debug=True)
+    householder(model, xc, 4, y=target, εf=f_tol, εx=x_tol, limit=max, debug=True)
     print("")
 if (method == Solver.NA) or (method == Solver.H4):
-    householder(model, xa, 5, y=target, εf=f_tol, εx=x_tol, limit=max, debug=True)
+    householder(model, xc, 5, y=target, εf=f_tol, εx=x_tol, limit=max, debug=True)
     print("")
 
 
@@ -56,12 +57,12 @@ if (method == Solver.NA) or (method == Solver.H4):
 from ad import *
 from playground import *
 
-model = lambda x: x**2 / (x.cosh + 1).ln - 1
-model = lambda x: x**2
+model = lambda x: (x - 1)**2 / (x.cosh + 1).ln - 1
 
-x0 = 2.0
-x1 = 0.0
-y = 2.0
+x0 = 1.0
+x1 = 3.0
+x2 = (x0 + x1) / 2.0
+y = 0.0
 
 bisect(model, x0, x1, y=y, debug=True)
 falsi(model, x0, x1, y=y, ill=False, debug=True)
@@ -77,10 +78,10 @@ timeit(bisect(model, x0, x1, y=y))
 timeit(falsi(model, x0, x1, y=y, ill=False))
 timeit(falsi(model, x0, x1, y=y))
 timeit(secant(model, x0, x1, y=y))
-timeit(newton(model, x0, y=y))
-timeit(householder(model, x0, 2, y=y))
-timeit(householder(model, x0, 3, y=y))
-timeit(householder(model, x0, 4, y=y))
-timeit(householder(model, x0, 5, y=y))
+timeit(newton(model, x2, y=y))
+timeit(householder(model, x2, 2, y=y))
+timeit(householder(model, x2, 3, y=y))
+timeit(householder(model, x2, 4, y=y))
+timeit(householder(model, x2, 5, y=y))
 
 '''
