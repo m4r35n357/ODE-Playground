23d4f8c2fdb44ed0235db9a560115de5b7729c84
Ian Smith <m4r35n357@gmail.com>
2019-10-14 12:41:51 +0100

    more asserts fix domain check error lose two more lines!

--- taylor-ode.c
+++ taylor-ode.c
@@ -18,7 +18,6 @@ void t_xyz_output (mpfr_t x, mpfr_t y, mpfr_t z, mpfr_t t) {
 void t_stepper (char **argv, long *n, mpfr_t *t, mpfr_t *h, long *nsteps) {
     mpfr_set_default_prec(strtod(argv[1], NULL) * 3.32);
     *n = strtol(argv[2], NULL, BASE);
-    assert(*n > 1);
     mpfr_init_set_ui(*t, 0, RND);
     mpfr_init_set_str(*h, argv[3], BASE, RND);
     *nsteps = strtol(argv[4], NULL, BASE);
@@ -51,11 +50,13 @@ void t_horner (mpfr_t *sum, const mpfr_t *jet, int n, mpfr_t h) {
 }
 
 void t_abs (mpfr_t *a, const mpfr_t *u, int k) {
+    assert(a != u);
     assert(k >= 0);
     mpfr_mul_si(*a, u[k], mpfr_sgn(u[0]), RND);
 }
 
 void t_sqr (mpfr_t *s, const mpfr_t *u, int k) {
+    assert(s != u);
     assert(k >= 0);
     int upper = (k - (k % 2 == 0 ? 2 : 1)) / 2 + 1;
     mpfr_set_zero(*s, 1);
@@ -69,6 +70,7 @@ void t_sqr (mpfr_t *s, const mpfr_t *u, int k) {
 }
 
 void t_prod (mpfr_t *p, const mpfr_t *u, const mpfr_t *v, int k) {
+    assert(p != u && p != v);
     assert(k >= 0);
     mpfr_set_zero(*p, 1);
     for (int j = 0; j < k + 1; j++) {
@@ -111,8 +113,9 @@ void t_sqrt (mpfr_t *r, const mpfr_t *u, int k) {
 }
 
 void t_pwr (mpfr_t *p, const mpfr_t *u, mpfr_t a, int k, mpfr_t *_) {
-    assert(mpfr_sgn(u[0]) != 0);
+    assert(mpfr_sgn(u[0]) > 0);
     assert(p != u);
+    assert(_ != p && _ != u);
     assert(k >= 0);
     if (k == 0) {
         mpfr_pow(p[0], u[0], a, RND);
@@ -131,6 +134,7 @@ void t_pwr (mpfr_t *p, const mpfr_t *u, mpfr_t a, int k, mpfr_t *_) {
 
 void t_exp (mpfr_t *e, const mpfr_t *u, int k, mpfr_t *_) {
     assert(e != u);
+    assert(_ != e && _ != u);
     assert(k >= 0);
     if (k == 0) {
         mpfr_exp(e[0], u[0], RND);
@@ -145,8 +149,8 @@ void t_exp (mpfr_t *e, const mpfr_t *u, int k, mpfr_t *_) {
 
 void t_ln (mpfr_t *l, const mpfr_t *u, int k, mpfr_t *_) {
     assert(mpfr_sgn(u[0]) > 0);
-    assert(_ != l && _ != u);
     assert(l != u);
+    assert(_ != l && _ != u);
     assert(k >= 0);
     if (k == 0) {
         mpfr_log(l[0], u[0], RND);
@@ -163,6 +167,7 @@ void t_ln (mpfr_t *l, const mpfr_t *u, int k, mpfr_t *_) {
 
 void t_sin_cos (mpfr_t *s, mpfr_t *c, const mpfr_t *u, int k, mpfr_t *_, geometry g) {
     assert(s != c && s != u && c != u);
+    assert(_ != s && _ != c && _ != u);
     assert(k >= 0);
     if (k == 0) {
         if (g == TRIG) {
@@ -187,29 +192,28 @@ void t_sin_cos (mpfr_t *s, mpfr_t *c, const mpfr_t *u, int k, mpfr_t *_, geometr
 
 void t_tan_sec2 (mpfr_t *t, mpfr_t *s2, const mpfr_t *u, int k, mpfr_t *_, geometry g) {
     assert(t != s2 && t != u && s2 != u);
+    assert(_ != t && _ != s2 && _ != u);
     assert(k >= 0);
     if (k == 0) {
         if (g == TRIG) {
             mpfr_tan(t[0], u[0], RND);
             mpfr_sec(s2[0], u[0], RND);
-            mpfr_sqr(s2[0], s2[0], RND);
         } else {
             mpfr_tanh(t[0], u[0], RND);
             mpfr_sech(s2[0], u[0], RND);
-            mpfr_sqr(s2[0], s2[0], RND);
         }
+        mpfr_sqr(s2[0], s2[0], RND);
     } else {
         mpfr_set_zero(t[k], 1);
-        mpfr_set_zero(s2[k], 1);
         for (int j = 0; j < k; j++) {
             mpfr_mul_d(*_, s2[j], (k - j) / (double)k, RND);
             mpfr_fma(t[k], *_, u[k - j], t[k], RND);
         }
+        mpfr_mul(s2[k], t[0], t[k], RND);
         for (int j = 1; j < k; j++) {
             mpfr_mul_d(*_, t[j], (k - j) / (double)k, RND);
             mpfr_fma(s2[k], *_, t[k - j], s2[k], RND);
         }
-        mpfr_fma(s2[k], t[0], t[k], s2[k], RND);
         mpfr_mul_2ui(s2[k], s2[k], 1, RND);
         if (g == HYP) {
             mpfr_neg(s2[k], s2[k], RND);
