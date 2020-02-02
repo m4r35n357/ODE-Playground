9877111eae7fb2385da3eec0058a30bc4ba177d2
Ian Smith <m4r35n357@gmail.com>
2020-02-02 17:24:09 +0000

    more doc consistency and formatting improvements

--- taylor-ode.h
+++ taylor-ode.h
@@ -74,12 +74,12 @@ mpfr_t *t_abs (mpfr_t *a, mpfr_t *u, int k);
  *
  *  S = U.U
  *
- *  S = sum{j=0->k} U[j].U[k - j]
+ *  S = sum{j=0->k} U[j].U[k-j]
  *
  *  or, from symmetry:
  *
- *  S = sum{j=0->(k-1)/2} U[j].U[k - j]             if k odd
- *  S = sum{j=0->(k-2)/2} U[j].U[k - j] + U[k/2]^2  if k even
+ *  S = sum{j=0->(k-1)/2} U[j].U[k-j]             if k odd
+ *  S = sum{j=0->(k-2)/2} U[j].U[k-j] + U[k/2]^2  if k even
  */
 mpfr_t *t_sqr (mpfr_t *S, mpfr_t *U, int k);
 
@@ -88,25 +88,25 @@ mpfr_t *t_sqr (mpfr_t *S, mpfr_t *U, int k);
  *
  *  P = U.V
  *
- *  P = sum{j=0->k} U[j].V[k - j]
+ *  P = sum{j=0->k} U[j].V[k-j]
  */
 mpfr_t *t_prod (mpfr_t *P, mpfr_t *U, mpfr_t *V, int k);
 
 /*
- * Returns a pointer to kth element of U / V, results stored in jet Q, DOMAIN RESTRICTION v[0] != 0.0
+ * Returns a pointer to kth element of U / V, results accumulated in jet Q, DOMAIN RESTRICTION v[0] != 0.0
  *
  *    Q = U / V ==> U = Q.V
  *
- *                U[k] = sum{j=0->k} Q[j].V[k - j]
+ *                U[k] = sum{j=0->k} Q[j].V[k-j]
  *
- *                     = sum{j=0->k-1} Q[j].V[k - j] + Q[k]. V[0]
+ *                     = sum{j=0->k-1} Q[j].V[k-j] + Q[k].V[0]
  *
- *                Q[k] = (U[k] - sum{j=0->k-1} Q[j].V[k - j]) / V[0]
+ *                Q[k] = (U[k] - sum{j=0->k-1} Q[j].V[k-j]) / V[0]
  */
 mpfr_t *t_quot (mpfr_t *Q, mpfr_t *U, mpfr_t *V, int k);
 
 /*
- * Returns a pointer to kth element of the square root of U, results stored in jet R, DOMAIN RESTRICTION U[0] > 0.0
+ * Returns a pointer to kth element of the square root of U, results accumulated in jet R, DOMAIN RESTRICTION U[0] > 0.0
  *
  *    U = R.R
  *
@@ -114,12 +114,12 @@ mpfr_t *t_quot (mpfr_t *Q, mpfr_t *U, mpfr_t *V, int k);
  *
  *      = sum{j=1->k-1} R[j].R[k - j] + 2.R[k].R[0]
  *
- * R[k] = (U[k] - sum{j=1->k-1} R[j].R[k - j]) / (2.R[0])
+ * R[k] = (U[k] - sum{j=1->k-1} R[j].R[k-j]) / (2.R[0])
  *
  * or, from symmetry:
  *
- * R[k] = (U[k] - sum{j=1->(k-1)/2} R[j].R[k - j]) / (2.0.R[0])             if k odd
- * R[k] = (U[k] - sum{j=1->(k-2)/2} R[j].R[k - j] - R[k/2]^2) / (2.0.R[0])  if k even
+ * R[k] = (U[k] - sum{j=1->(k-1)/2} R[j].R[k-j]) / (2.0.R[0])             if k odd
+ * R[k] = (U[k] - sum{j=1->(k-2)/2} R[j].R[k-j] - R[k/2]^2) / (2.0.R[0])  if k even
  */
 mpfr_t *t_sqrt (mpfr_t *R, mpfr_t *U, int k);
 
@@ -129,73 +129,73 @@ mpfr_t *t_sqrt (mpfr_t *R, mpfr_t *U, int k);
  *          F' = (df/du).U'
  *             =       H.U'
  *
- * Using F'[k] = (k + 1).F[k + 1]  ==>  F'[k - 1] = k.F[k], we can replace F' with F, and U' with U as follows:
+ * Using F'[k] = (k+1)F[k+1]  ==>  F'[k-1] = kF[k], we can replace F' with F, and U' with U as follows:
  *
- * From product rule above, (note that F' and U' have one fewer elements than F and U)
+ * from product rule above, (note that F' and U' have one fewer elements than F and U)
  *
- *   F'[k - 1] = sum{j=0->k-1} H[j].(k - 1 - j).U'[k - 1 - j]
+ *     F'[k-1] = sum{j=0->k-1} H[j].U'[k-1-j]
  *
- *      k.F[k] = sum{j=0->k-1} H[j].(k - j).U[k - j]
+ *       kF[k] = sum{j=0->k-1} H[j].(k-j)U[k-j]
  *
- *        F[k] = sum{j=0->k-1} H[j].(k - j).U[k - j] / k
+ *        F[k] = sum{j=0->k-1} H[j].(k-j)U[k-j]/k
  */
 
 /*
- * Returns a pointer to kth element of the exponential of U, results stored in jet E
+ * Returns a pointer to kth element of the exponential of U, results accumulated in jet E
  *
  *      E' = E.U'
  *
- *    E[k] = sum{j=0->k-1} E[j].(k - j).U[k - j] / k
+ *    E[k] = sum{j=0->k-1} E[j].(k-j)U[k-j]/k
  */
 mpfr_t *t_exp (mpfr_t *E, mpfr_t *U, int k, mpfr_t *tmp);
 
 /*
- * Returns a pair of pointers to kth elements of the sine and cosine of U, results stored in jets S and C
+ * Returns a pair of pointers to kth elements of the sine and cosine of U, results accumulated in jets S and C
  *
- *      S' =      C.U'
- *      C' = (+/-)S.U'   (+ for cosh, - for cos)
+ *      S' =       C.U'
+ *      C' = (+/-) S.U'   (+ for cosh(g=HYP), - for cos(g=TRIG))
  *
- *    S[k] = sum{j=0->k-1}      C(u)[j].(k - j).U[k - j] / k
- *    C[k] = sum{j=0->k-1} (+/-)S(u)[j].(k - j).U[k - j] / k
+ *    S[k] = sum{j=0->k-1}       C[j].(k-j)U[k-j]/k
+ *    C[k] = sum{j=0->k-1} (+/-) S[j].(k-j)U[k-j]/k
  */
 tuple t_sin_cos (mpfr_t *S, mpfr_t *C, mpfr_t *U, int k, mpfr_t *tmp, geometry g);
 
 /*
- * Returns a pair of pointers to kth elements of the tangent and squared secant of U, results stored in jets T and S2
+ * Returns a pair of pointers to kth elements of the tangent and squared secant of U, results accumulated in jets T and S2
  *
- *      T' =      S^2.U'
- *    S^2' = (+/-)2.T.T'   (+ for sec^2, - for sech^2)
+ *      T' =       S2.U'
+ *     S2' = (+/-)2 T.T'   (+ for sec^2(g=TRIG), - for sech^2(g=HYP))
  *
- *    T[k] = sum{j=0->k-1}      S^2[j].(k - j).U[k - j] / k
- *  S^2[k] = sum{j=0->k-1} (+/-)2.T[j].(k - j).T[k - j] / k
+ *    T[k] = sum{j=0->k-1}       S2[j].(k-j)U[k-j]/k
+ *   S2[k] = sum{j=0->k-1} (+/-)2 T[j].(k-j)T[k-j]/k
  */
 tuple t_tan_sec2 (mpfr_t *T, mpfr_t *S2, mpfr_t *U, int k, mpfr_t *tmp, geometry g);
 
 /*
- * Returns a pointer to kth element of P = U^a (where a is scalar), results stored in jet P, DOMAIN RESTRICTION U[0] > 0.0
+ * Returns a pointer to kth element of P = U^a (where a is scalar), results accumulated in jet P, DOMAIN RESTRICTION U[0] > 0.0
  *
- *                    P'= U^a' = a.U^(a-1).U'
- *                      U.U^a' = a.U^a.U'
- *                        U.P' = a.P.U'
+ *                    P'= U^a' = a U^(a-1).U'
+ *                      U.U^a' = a U^a.U'
+ *                        U.P' = a P.U'
  *
- * sum{j=0->k} U[k - j].j.P[j] = sum{j=0->k-1} a.P[j].(k - j).U[k - j]
+ *    sum{j=0->k} U[k-j].jP[j] =  a sum{j=0->k-1} P[j].(k-j)U[k-j]
  *
- *                 U[0].k.P[k] = sum{j=0->k-1} a.P[j].(k - j).U[k - j] - sum{j=0->k-1} U[k - j].j.P[j]
+ *                  U[0].kP[k] =  a sum{j=0->k-1} P[j].(k-j)U[k-j]   - sum{j=0->k-1} U[k-j].jP[j]
  *
- *                        P[k] = (sum{j=0->k-1} a.P[j].(k - j).U[k - j] / k - sum{j=0->k-1} U[k - j].j.P[j] / k) / U[0]
+ *                        P[k] = (a sum{j=0->k-1} P[j].(k-j)U[k-j]/k - sum{j=0->k-1} U[k-j].jP[j]/k) / U[0]
  */
 mpfr_t *t_pwr (mpfr_t *P, mpfr_t *U, double a, int k, mpfr_t *tmpA, mpfr_t *tmpB);
 
 /*
- * Returns a pointer to kth element of the natural logarithm of U, result stored in jet L, DOMAIN RESTRICTION U[0] > 0.0
+ * Returns a pointer to kth element of the natural logarithm of U, result accumulated in jet L, DOMAIN RESTRICTION U[0] > 0.0
  *
  *     L' = U' / U ==> U' = U.L'
  *
- *                 k.U[k] = sum{j=0->k-1} U[j].(k - j).L[k - j]
+ *                 k.U[k] = sum{j=0->k-1} U[j].(k-j)L[k-j]
  *
- *                        = sum{j=1->k-1} U[j].(k - j).L[k - j] + U[0].k.L[k]
+ *                        = sum{j=1->k-1} U[j].(k-j)L[k-j] + U[0].kL[k]
  *
- *                   L[k] = (U[k] - sum{j=1->k-1} U[j].(k - j).L[k - j] / k) / U[0]
+ *                   L[k] = (U[k] - sum{j=1->k-1} U[j].(k-j)L[k-j]/k) / U[0]
  */
 mpfr_t *t_ln (mpfr_t *L, mpfr_t *U, int k, mpfr_t *tmp);
 
