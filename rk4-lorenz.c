635a023b739f3d3b9dd6697e3a321600faf75b9c
Ian Smith <m4r35n357@gmail.com>
2020-03-13 14:00:38 +0000

    RK4 tweaks pre-commit text output

--- rk4-lorenz.c
+++ rk4-lorenz.c
@@ -13,7 +13,7 @@
 #include <mpfr.h>
 #include "taylor-ode.h"
 
-static void dx (mpfr_t *lhs, mpfr_t x, mpfr_t y, mpfr_t z, mpfr_t s) {
+static void dx (mpfr_t *lhs, mpfr_t x, mpfr_t y, mpfr_t s) {
     mpfr_fmms(*lhs, s, y, s, x, RND);
 }
 
@@ -38,50 +38,50 @@ static mpfr_t *sum (mpfr_t *b, mpfr_t a1, mpfr_t a2, mpfr_t a3, mpfr_t a4) {
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t t, x0, y0, z0, sigma, rho, beta, h, _, h_2, _x, _y, _z;
+    mpfr_t t, x, y, z, sigma, rho, beta, h, _, h_2, _x, _y, _z;
     mpfr_t k1, l1, m1, k2, l2, m2, k3, l3, m3, k4, l4, m4;
 
     assert(argc == 12);
     t_stepper(argv, &n, &t, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &z0, &sigma, &rho, &beta, &_);
+    t_args(argv, argc, &x, &y, &z, &sigma, &rho, &beta, &_);
     mpfr_div(beta, beta, _, RND);
     mpfr_inits(h_2, _x, _y, _z, k1, l1, m1, k2, l2, m2, k3, l3, m3, k4, l4, m4, NULL);
 
     mpfr_div_2ui(h_2, h, 1, RND);
 
-    t_xyz_output(x0, y0, z0, t);
+    t_xyz_output(x, y, z, t);
     for (long step = 1; step < nsteps + 1; step++) {
-        dx(&k1, x0, y0, z0, sigma);
-        dy(&l1, x0, y0, z0, rho);
-        dz(&m1, x0, y0, z0, beta);
+        dx(&k1, x, y, sigma);
+        dy(&l1, x, y, z, rho);
+        dz(&m1, x, y, z, beta);
 
-        mpfr_fma(_x, k1, h_2, x0, RND);
-        mpfr_fma(_y, l1, h_2, y0, RND);
-        mpfr_fma(_z, m1, h_2, z0, RND);
-        dx(&k2, _x, _y, _z, sigma);
+        mpfr_fma(_x, k1, h_2, x, RND);
+        mpfr_fma(_y, l1, h_2, y, RND);
+        mpfr_fma(_z, m1, h_2, z, RND);
+        dx(&k2, _x, _y, sigma);
         dy(&l2, _x, _y, _z, rho);
         dz(&m2, _x, _y, _z, beta);
 
-        mpfr_fma(_x, k2, h_2, x0, RND);
-        mpfr_fma(_y, l2, h_2, y0, RND);
-        mpfr_fma(_z, m2, h_2, z0, RND);
-        dx(&k3, _x, _y, _z, sigma);
+        mpfr_fma(_x, k2, h_2, x, RND);
+        mpfr_fma(_y, l2, h_2, y, RND);
+        mpfr_fma(_z, m2, h_2, z, RND);
+        dx(&k3, _x, _y, sigma);
         dy(&l3, _x, _y, _z, rho);
         dz(&m3, _x, _y, _z, beta);
 
-        mpfr_fma(_x, k3, h, x0, RND);
-        mpfr_fma(_y, l3, h, y0, RND);
-        mpfr_fma(_z, m3, h, z0, RND);
-        dx(&k4, _x, _y, _z, sigma);
+        mpfr_fma(_x, k3, h, x, RND);
+        mpfr_fma(_y, l3, h, y, RND);
+        mpfr_fma(_z, m3, h, z, RND);
+        dx(&k4, _x, _y, sigma);
         dy(&l4, _x, _y, _z, rho);
         dz(&m4, _x, _y, _z, beta);
 
-        mpfr_fma(x0, h, *sum(&_, k1, k2, k3, k4), x0, RND);
-        mpfr_fma(y0, h, *sum(&_, l1, l2, l3, l4), y0, RND);
-        mpfr_fma(z0, h, *sum(&_, m1, m2, m3, m4), z0, RND);
+        mpfr_fma(x, h, *sum(&_, k1, k2, k3, k4), x, RND);
+        mpfr_fma(y, h, *sum(&_, l1, l2, l3, l4), y, RND);
+        mpfr_fma(z, h, *sum(&_, m1, m2, m3, m4), z, RND);
 
         mpfr_mul_ui(t, h, step, RND);
-        t_xyz_output(x0, y0, z0, t);
+        t_xyz_output(x, y, z, t);
     }
     return 0;
 }
