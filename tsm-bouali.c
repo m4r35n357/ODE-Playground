m4r35n357/ODE-Playground

commit 9322a4d6bec442bf2daf9c1faad8d19db5016c84

client code reduction

tsm-bouali.c
@@ -12,17 +12,15 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, y0, z0, a, b, g, m, h, _;
+    mpfr_t a, b, g, m, h, _;
 
     // initialize from command arguments
     assert(argc == 13);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &z0, &a, &b, &g, &m);
-    mpfr_init(_);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0), y = t_jet_c(n + 1, y0), z = t_jet_c(n + 1, z0);
+    series x = t_jet(n + 1), y = t_jet(n + 1), z = t_jet(n + 1);
+    t_args(argv, argc, x.a, y.a, z.a, &a, &b, &g, &m);
     series gx2 = t_jet(n);
+    mpfr_init(_);
 
     t_output(x.a[0], y.a[0], z.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-constant.c
@@ -12,15 +12,13 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, a, h, _;
+    mpfr_t a, h, _;
 
     // initialize from command arguments
     assert(argc == 10);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &_, &_, &a);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0);
+    series x = t_jet(n + 1);
+    t_args(argv, argc, x.a, &_, &_, &a);
 
     t_output(x.a[0], x.a[0], x.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-genesio-tesi.c
@@ -12,16 +12,14 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, y0, z0, a, b, c, h, _;
+    mpfr_t a, b, c, h, _;
 
     // initialize from command arguments
     assert(argc == 12);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &z0, &a, &b, &c);
+    series x = t_jet(n + 1), y = t_jet(n + 1), z = t_jet(n + 1);
+    t_args(argv, argc, x.a, y.a, z.a, &a, &b, &c);
     mpfr_init(_);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0), y = t_jet_c(n + 1, y0), z = t_jet_c(n + 1, z0);
 
     t_output(x.a[0], y.a[0], z.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-halvorsen.c
@@ -12,16 +12,14 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, y0, z0, a, h, _, w4x, w4y, w4z;
+    mpfr_t a, h, _, w4x, w4y, w4z;
 
     // initialize from command arguments
     assert(argc == 10);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &z0, &a);
+    series x = t_jet(n + 1), y = t_jet(n + 1), z = t_jet(n + 1);
+    t_args(argv, argc, x.a, y.a, z.a, &a);
     mpfr_inits(_, w4x, w4y, w4z, NULL);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0), y = t_jet_c(n + 1, y0), z = t_jet_c(n + 1, z0);
 
     t_output(x.a[0], y.a[0], z.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-logistic.c
@@ -12,15 +12,13 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, a, h, _;
+    mpfr_t a, h, _;
 
     // initialize from command arguments
     assert(argc == 10);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &_, &_, &a);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0);
+    series x = t_jet(n + 1);
+    t_args(argv, argc, x.a, &_, &_, &a);
 
     t_output(x.a[0], x.a[0], x.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-lorenz.c
@@ -12,16 +12,14 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, y0, z0, sigma, rho, beta, h, _;
+    mpfr_t sigma, rho, beta, h, _;
 
     // initialize from command arguments
     assert(argc == 13);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &z0, &sigma, &rho, &beta, &_);
+    series x = t_jet(n + 1), y = t_jet(n + 1), z = t_jet(n + 1);
+    t_args(argv, argc, x.a, y.a, z.a, &sigma, &rho, &beta, &_);
     mpfr_div(beta, beta, _, RND);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0), y = t_jet_c(n + 1, y0), z = t_jet_c(n + 1, z0);
 
     t_output(x.a[0], y.a[0], z.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-lotka-volterra.c
@@ -12,16 +12,14 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, y0, a, b, c, d, h, _, xy;
+    mpfr_t a, b, c, d, h, _, xy;
 
     // initialize from command arguments
     assert(argc == 13);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &_, &a, &b, &c, &d);
+    series x = t_jet(n + 1), y = t_jet(n + 1);
+    t_args(argv, argc, x.a, y.a, &_, &a, &b, &c, &d);
     mpfr_init(xy);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0), y = t_jet_c(n + 1, y0);
 
     t_output(x.a[0], y.a[0], x.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

tsm-rf.c
@@ -13,21 +13,17 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t x0, y0, z0, gamma, h, d3, _, x2_1;
+    mpfr_t gamma, h, d3, _, x2_1;
 
     // initialize from command arguments
     assert(argc == 11);
     t_stepper(argv, &n, &h, &nsteps);
-    t_args(argv, argc, &x0, &y0, &z0, &_, &gamma);
-    mpfr_inits(d3, x2_1, NULL);
-
-    // initialize the derivative and temporary jets
-    series x = t_jet_c(n + 1, x0), y = t_jet_c(n + 1, y0), z = t_jet_c(n + 1, z0);
-    series alpha = t_jet_c(n, _);
-    series a = t_jet(n), b = t_jet(n), c = t_jet(n);
-    mpfr_set_ui(d3, 3, RND);
-    mpfr_set_ui(_, 1, RND);
-    series w1 = t_jet_c(n, _);
+    series x = t_jet(n + 1), y = t_jet(n + 1), z = t_jet(n + 1), alpha = t_jet(n);
+    t_args(argv, argc, x.a, y.a, z.a, alpha.a, &gamma);
+    series a = t_jet(n), b = t_jet(n), c = t_jet(n), w1 = t_jet(n);
+    mpfr_set_ui(w1.a[0], 1, RND);
+    mpfr_inits(x2_1, _, NULL);
+    mpfr_init_set_ui(d3, 3, RND);
 
     t_output(x.a[0], y.a[0], z.a[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {

commit bb3428a3eb04e5f48bad4c13905eb4a9abc75002

set full precision before initializing MPFR variables! mpfr_init move from c907160419574790dfc3f42b442569a5949ec82a was over-enthusiastic (indirect fallout from 9322a4d6bec442bf2daf9c1faad8d19db5016c84)

tsm-bouali.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 13);
-    mpfr_inits(a, b, g, m, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, b, g, m, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a, &b, &g, &m);
     series gx2 = t_series(n);

tsm-constant.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 10);
-    mpfr_inits(a, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, _, NULL);
     series x = t_series(n + 1);
     t_args(argv, argc, x.jet, &_, &_, &a);
 

tsm-genesio-tesi.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 12);
-    mpfr_inits(a, b, c, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, b, c, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a, &b, &c);
 

tsm-halvorsen.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 10);
-    mpfr_inits(a, w4x, w4y, w4z, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, w4x, w4y, w4z, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a);
 

tsm-logistic.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 10);
-    mpfr_inits(a, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, _, NULL);
     series x = t_series(n + 1);
     t_args(argv, argc, x.jet, &_, &_, &a);
 

tsm-lorenz.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 13);
-    mpfr_inits(sigma, rho, beta, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(sigma, rho, beta, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &sigma, &rho, &beta, &_);
     mpfr_div(beta, beta, _, RND);

tsm-lotka-volterra.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 13);
-    mpfr_inits(a, b, c, d, xy, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, b, c, d, xy, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, &_, &a, &b, &c, &d);
 

tsm-rf.c
@@ -17,8 +17,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 11);
-    mpfr_inits(gamma, d3, x2_1, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(gamma, d3, x2_1, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1), alpha = t_series(n);
     t_args(argv, argc, x.jet, y.jet, z.jet, alpha.jet, &gamma);
     series a = t_series(n), b = t_series(n), c = t_series(n), w1 = t_series(n);

tsm-rossler.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 12);
-    mpfr_inits(a, c, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, c, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1), b = t_series(n);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a, b.jet, &c);
 

tsm-sj.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 11);
-    mpfr_inits(a, _, __, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, _, __, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     series w_b = t_series(n);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a, w_b.jet);

tsm-sprott-minimal.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 10);
-    mpfr_inits(a, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a);
 

tsm-sprott-thomas.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 11);
-    mpfr_inits(a, b, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, b, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a, &b);
     series sax = t_series(n), say = t_series(n), saz = t_series(n);

tsm-thomas.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 10);
-    mpfr_inits(b, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(b, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &b);
     series sx = t_series(n), sy = t_series(n), sz = t_series(n), cx = t_series(n), cy = t_series(n), cz = t_series(n);

tsm-wimol-banlue.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 10);
-    mpfr_init(_);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_init(_);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     series wa = t_series(n);
     t_args(argv, argc,x.jet, y.jet, z.jet, wa.jet);

tsm-yu-wang.c
@@ -16,8 +16,8 @@ int main (int argc, char **argv) {
 
     // initialize from command arguments
     assert(argc == 13);
-    mpfr_inits(a, b, c, d, _, NULL);
     t_stepper(argv, &n, &h, &nsteps);
+    mpfr_inits(a, b, c, d, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1);
     t_args(argv, argc, x.jet, y.jet, z.jet, &a, &b, &c, &d);
     series xy = t_series(n), e_xy = t_series(n);