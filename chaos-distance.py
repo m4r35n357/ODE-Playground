9a7c47be36ad70b39918ee09201bcd06b4435b03
Ian Smith <m4r35n357@gmail.com>
2020-08-03 19:58:27 +0100

    process two simulations

--- chaos-distance.py
+++ chaos-distance.py
@@ -13,39 +13,62 @@ def line_to_data(line):
     return data
 
 def scan():
-    if len(argv) != 10:
-        raise Exception(">>> ERROR! Please supply seven file names, the intended data file length, and a threshold <<<")
-    data_a, data_b, data_c, data_d, data_e, data_f, data_g = [], [], [], [], [], [], []
-    with open(argv[1]) as a, open(argv[2]) as b, open(argv[3]) as c, open(argv[4]) as d, open(argv[5]) as e, open(argv[6]) as f, open(argv[7]) as g:
+    if len(argv) != 4:
+        raise Exception(">>> ERROR! Please the intended data file length, and two separations <<<")
+    data_a1, data_b1, data_c1, data_d1, data_e1, data_f1, data_g1 = [], [], [], [], [], [], []
+    data_a2, data_b2, data_c2, data_d2, data_e2, data_f2, data_g2 = [], [], [], [], [], [], []
+    with open('/tmp/dataA') as a, open('/tmp/dataB') as b, open('/tmp/dataC') as c, open('/tmp/dataD') as d, open('/tmp/dataE') as e, open('/tmp/dataF') as f, open('/tmp/dataG') as g:
         for line_a, line_b, line_c, line_d, line_e, line_f, line_g in zip(a, b, c, d, e, f, g):
-            data_a.append(line_to_data(line_a))
-            data_b.append(line_to_data(line_b))
-            data_c.append(line_to_data(line_c))
-            data_d.append(line_to_data(line_d))
-            data_e.append(line_to_data(line_e))
-            data_f.append(line_to_data(line_f))
-            data_g.append(line_to_data(line_g))
-    data_g_length = len(data_g)
-    data_length = int(argv[8])
-    threshold = float(argv[9])
-    results = []
-    r = 0.0
-    if data_g_length == data_length:
-        for ref, trajectory in zip(data_g, zip(data_a, data_b, data_c, data_d, data_e, data_f)):
+            data_a1.append(line_to_data(line_a))
+            data_b1.append(line_to_data(line_b))
+            data_c1.append(line_to_data(line_c))
+            data_d1.append(line_to_data(line_d))
+            data_e1.append(line_to_data(line_e))
+            data_f1.append(line_to_data(line_f))
+            data_g1.append(line_to_data(line_g))
+    data_g1_length = len(data_g1)
+    with open('/tmp/dataA') as a, open('/tmp/dataB') as b, open('/tmp/dataC') as c, open('/tmp/dataD') as d, open('/tmp/dataE') as e, open('/tmp/dataF') as f, open('/tmp/dataG') as g:
+        for line_a, line_b, line_c, line_d, line_e, line_f, line_g in zip(a, b, c, d, e, f, g):
+            data_a2.append(line_to_data(line_a))
+            data_b2.append(line_to_data(line_b))
+            data_c2.append(line_to_data(line_c))
+            data_d2.append(line_to_data(line_d))
+            data_e2.append(line_to_data(line_e))
+            data_f2.append(line_to_data(line_f))
+            data_g2.append(line_to_data(line_g))
+    data_g2_length = len(data_g2)
+    assert(data_g1_length == data_g2_length)
+    data_length = int(argv[1])
+    separation1 = float(argv[2])
+    separation2 = float(argv[3])
+    results1, results2 = [], []
+    r1, r2 = 0.0, 0.0
+    if data_g1_length == data_length:
+        for ref, trajectory in zip(data_g1, zip(data_a1, data_b1, data_c1, data_d1, data_e1, data_f1)):
             d_max = 0.0
             for point in trajectory:
                 dist = sqrt((ref[0] - point[0])**2 + (ref[1] - point[1])**2 + (ref[2] - point[2])**2)
                 d_max = dist if dist > d_max else d_max
-            results.append(d_max)
-        r = results[-1]
-    if data_g_length < data_length:
-        print(f'UNBOUNDED ({"dummy value"} = {-0.1:.3e} {data_g_length} lines out of {data_length})')
-    elif r >= threshold:
-        print(f'  CHAOTIC ({"final value"} = {r:.3e} >= {threshold:.3e})')
-    elif r >= threshold / 1000:
-        print(f' PERIODIC ({"final value"} = {r:.3e}  < {threshold:.3e})')
+            results1.append(d_max)
+        r1 = results1[-1]
+    if data_g2_length == data_length:
+        for ref, trajectory in zip(data_g2, zip(data_a2, data_b2, data_c2, data_d2, data_e2, data_f2)):
+            d_max = 0.0
+            for point in trajectory:
+                dist = sqrt((ref[0] - point[0])**2 + (ref[1] - point[1])**2 + (ref[2] - point[2])**2)
+                d_max = dist if dist > d_max else d_max
+            results2.append(d_max)
+        r2 = results1[-1]
+    if data_g1_length < data_length or data_g2_length < data_length:
+        print(f'UNBOUNDED ({"dummy value"} = {-0.1:.1f} {data_g1_length} / {data_g2_length} lines out of {data_length})')
+    elif r1 < separation1 or r2 < separation2:
+        print(f'   STABLE ({"final value"} = {r1:.3e} {r2:.3e})')
+    elif r1 / r2 < 0.1 * separation1 / separation2:
+        print(f'  CHAOTIC ({"final value"} = {r1:.3e} {r2:.3e})')
+    elif 0.9 * separation1 / separation2 < r1 / r2 < 1.1 * separation1 / separation2:
+        print(f' PERIODIC ({"final value"} = {r1:.3e} {r2:.3e})')
     else:
-        print(f'   STABLE ({"final value"} = {r:.3e}  < {(threshold / 1000):.3e})')
+        print(f'  UNKNOWN ')
 
 print(f'SCAN: {argv}', file=stderr)
 scan()
