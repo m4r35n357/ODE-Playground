2fd8659efc9d4f257d26191b02839a3f9d2e8000
Ian Smith <m4r35n357@gmail.com>
2020-09-22 10:42:54 +0100

    RK4 versions of all ODEs

---
+++ rk4-halvorsen.c
@@ -0,0 +1,37 @@
+/*
+ * Halvorsen Cyclic Attractor
+ *
+ * Example: ./rk4-halvorsen-dbg NA NA 1 .01 10000 1 0 0 1.4
+ *
+ * (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include "taylor-ode.h"
+
+typedef struct {
+    real a;
+} parameters;
+
+static components ode (real x, real y, real z, void *params) {
+    parameters *p = (parameters *)params;
+    return (components) {
+        .x = - p->a * x - 4.0 * (y + z) - y * y,
+        .y = - p->a * y - 4.0 * (z + x) - z * z,
+        .z = - p->a * z - 4.0 * (x + y) - x * x
+    };
+}
+
+static void *get_p (int argc, char **argv, long order) {
+    (void)order;
+    parameters *p = malloc(sizeof (parameters));
+    t_args(argv, argc, &p->a);
+    return p;
+}
+
+int main (int argc, char **argv) {
+    assert(argc == 10);
+    rk4(argc, argv, ode, get_p);
+    return 0;
+}
---
+++ rk4-thomas.c
@@ -0,0 +1,38 @@
+/*
+ * Thomas' cyclically symmetric attractor
+ *
+ * Example: ./rk4-thomas-dbg NA NA 1 0.1 30000 1 0 0 .19
+ *
+ * (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "taylor-ode.h"
+
+typedef struct {
+    real b;
+} parameters;
+
+static components ode (real x, real y, real z, void *params) {
+    parameters *p = (parameters *)params;
+    return (components) {
+        .x = sin(y) - p->b * x,
+        .y = sin(z) - p->b * y,
+        .z = sin(x) - p->b * z
+    };
+}
+
+static void *get_p (int argc, char **argv, long order) {
+    (void)order;
+    parameters *p = malloc(sizeof (parameters));
+    t_args(argv, argc, &p->b);
+    return p;
+}
+
+int main (int argc, char **argv) {
+    assert(argc == 10);
+    rk4(argc, argv, ode, get_p);
+    return 0;
+}
