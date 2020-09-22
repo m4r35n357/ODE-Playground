28cade7b1a10467e4b46be9b90a0803ce145a172
Ian Smith <m4r35n357@gmail.com>
2020-09-22 23:00:35 +0100

    Rabinovich–Fabrikant TSM improvement

--- tsm-rf.c
+++ tsm-rf.c
@@ -13,29 +13,29 @@
 
 int main (int argc, char **argv) {
     long n, nsteps;
-    mpfr_t gamma, h, d3, _, x2_1;
+    mpfr_t gamma, h, d4, _;
 
     // initialize from command arguments
     assert(argc == 11);
     t_stepper(argv, &n, &h, &nsteps);
-    mpfr_inits(gamma, d3, x2_1, _, NULL);
+    mpfr_inits(gamma, d4, _, NULL);
     series x = t_series(n + 1), y = t_series(n + 1), z = t_series(n + 1), alpha = t_series(n);
     t_args(argv, argc, x.jet, y.jet, z.jet, alpha.jet, &gamma);
     series a = t_series(n), b = t_series(n), c = t_series(n), w1 = t_series(n);
     mpfr_set_ui(w1.jet[0], 1, RND);
-    mpfr_set_ui(d3, 3, RND);
+    mpfr_set_ui(d4, 4, RND);
 
     t_output(x.jet[0], y.jet[0], z.jet[0], h, 0);
     for (long step = 1; step <= nsteps; step++) {
         // build the jet of taylor coefficients
         for (int k = 0; k < n; k++) {
-            mpfr_sub(x2_1, *t_sqr(x, k), w1.jet[k], RND);
             //  x' = y(z - 1 + x^2) + Gx
-            mpfr_add(a.jet[k], z.jet[k], x2_1, RND);
+            mpfr_sub(a.jet[k], *t_sqr(x, k), w1.jet[k], RND);
+            mpfr_add(a.jet[k], z.jet[k], a.jet[k], RND);
             mpfr_fma(_, gamma, x.jet[k], *t_prod(y, a, k), RND);
             t_next(x, _, k, POS);
             //  y' = x(3z + 1 - x^2) + Gy
-            mpfr_fms(b.jet[k], d3, z.jet[k], x2_1, RND);
+            mpfr_fms(b.jet[k], d4, z.jet[k], a.jet[k], RND);
             mpfr_fma(_, gamma, y.jet[k], *t_prod(x, b, k), RND);
             t_next(y, _, k, POS);
             //  z' = -2z(A + xy)
