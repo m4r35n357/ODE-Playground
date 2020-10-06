4295bc0026b87fa6b7479472338b5ec704ab17d8
Ian Smith <m4r35n357@gmail.com>
2020-10-06 09:35:50 +0100

    another parameter for halvorsen

--- rk4-halvorsen.c
+++ rk4-halvorsen.c
@@ -1,7 +1,7 @@
 /*
  * Halvorsen Cyclic Attractor
  *
- * Example: ./rk4-halvorsen-dbg 15 _ 1 .01 10000 1 0 0 1.4
+ * Example: ./rk4-halvorsen-dbg 15 _ 1 .01 10000 1 0 0 1.4 4
  *
  * (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
  */
@@ -12,25 +12,26 @@
 
 typedef struct {
     real a;
+    real b;
 } parameters;
 
 static void *get_p (int argc, char **argv) {
     parameters *p = malloc(sizeof (parameters));
-    t_params(argv, argc, &p->a);
+    t_params(argv, argc, &p->a, &p->b);
     return p;
 }
 
 static components ode (real x, real y, real z, void *params) {
     parameters *p = (parameters *)params;
     return (components) {
-        .x = - p->a * x - 4.0 * (y + z) - y * y,
-        .y = - p->a * y - 4.0 * (z + x) - z * z,
-        .z = - p->a * z - 4.0 * (x + y) - x * x
+        .x = - p->a * x - p->b * (y + z) - y * y,
+        .y = - p->a * y - p->b * (z + x) - z * z,
+        .z = - p->a * z - p->b * (x + y) - x * x
     };
 }
 
 int main (int argc, char **argv) {
-    assert(argc == 10);
+    assert(argc == 11);
     rk4(argc, argv, ode, get_p);
     return 0;
 }
--- tsm-halvorsen.c
+++ tsm-halvorsen.c
@@ -1,7 +1,7 @@
 /*
  * Halvorsen Cyclic Attractor
  *
- * Example: ./tsm-halvorsen-dbg 15 _ 10 .01 10000 1 0 0 1.4
+ * Example: ./tsm-halvorsen-dbg 15 _ 10 .01 10000 1 0 0 1.4 4
  *
  * (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
  */
@@ -12,12 +12,13 @@
 
 typedef struct {
     real a;
+    real b;
 } parameters;
 
 static void *get_p (int argc, char **argv, long order) {
     (void)order;
     parameters *p = malloc(sizeof (parameters));
-    t_params(argv, argc, &p->a);
+    t_params(argv, argc, &p->a, &p->b);
     return p;
 }
 
@@ -25,14 +26,14 @@ static components ode (series x, series y, series z, void *params, void *inters,
     parameters *p = (parameters *)params;
     (void)inters;
     return (components) {
-        .x = - p->a * x[k] - 4.0 * (y[k] + z[k]) - t_sqr(y, k),
-        .y = - p->a * y[k] - 4.0 * (z[k] + x[k]) - t_sqr(z, k),
-        .z = - p->a * z[k] - 4.0 * (x[k] + y[k]) - t_sqr(x, k)
+        .x = - p->a * x[k] - p->b * (y[k] + z[k]) - t_sqr(y, k),
+        .y = - p->a * y[k] - p->b * (z[k] + x[k]) - t_sqr(z, k),
+        .z = - p->a * z[k] - p->b * (x[k] + y[k]) - t_sqr(x, k)
     };
 }
 
 int main (int argc, char **argv) {
-    assert(argc == 10);
+    assert(argc == 11);
     tsm(argc, argv, ode, get_p, NULL);
     return 0;
 }
