846ab55f4ed2044b961fc4bde0f052c157617b62
Ian Smith <m4r35n357@gmail.com>
2020-10-16 12:24:56 +0100

    PNG output option for chaos scanning

--- plotChaos-cns.py
+++ plotChaos-cns.py
@@ -2,14 +2,16 @@
 #
 #  (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 #
+#  Example basename='images/auto'; t=1000; while [ $t -le 10000 ]; do echo "$basename-$t"; time -p ./chaos-scan-cns thomas .01 .21 .1 $t 2>/dev/null | ./plotChaos-cns.py "$basename-$t"; t=$((t + 1000)); done
 
 from sys import argv, stdin, stderr
 from matplotlib import pyplot
 
 def main():
-    print("Chaos Plotter: {}".format(argv), file=stderr)
-    if len(argv) > 1:
-        raise Exception('>>> ERROR! No parameters, thanks! <<<')
+    print(f'Chaos Plotter: {argv}', file=stderr)
+    save = False
+    if len(argv) == 2:
+        save = True
     coordinate_a, coordinate_b, coordinate_c = 0, 1, 4
     line = stdin.readline()
     ax1 = pyplot.figure().add_subplot(111)
@@ -40,7 +42,7 @@ def main():
         n += 1
     ax1.plot(x, y, 'bo-', markersize=0)
     ax2.plot(x, z, 'ro-', markersize=0)
-    pyplot.show()
+    pyplot.savefig(argv[1]) if save else pyplot.show()
 
 if __name__ == "__main__":
     main()
