58992815a60cb8e0ce41a7aebc516d0bc7732e8d
Ian Smith <m4r35n357@gmail.com>
2020-10-17 10:37:31 +0100

    add an "even better" Taylor integrator for CNS also add a better "step only" one for RK4

--- cns-functions.sh
+++ cns-functions.sh
@@ -2,7 +2,7 @@
 #  (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 #
 
-quarterstep () {
+quarterstep () {  # step / 4
     start="$1 $2 $3 $4"
     step=$(echo "scale=6; $5 / 4;" | bc)
     steps=$(($6 * 4))
@@ -12,7 +12,17 @@ quarterstep () {
     $start $step $steps $end | sed -n '1~4p' >$fileA &
 }
 
-orderplus2 () {
+eightthstep () {  # step / 8
+    start="$1 $2 $3 $4"
+    step=$(echo "scale=6; $5 / 8;" | bc)
+    steps=$(($6 * 8))
+    shift 6
+    end="$*"
+    echo 'Better:' $start $step $steps $end >&2
+    $start $step $steps $end | sed -n '1~8p' >$fileA &
+}
+
+orderplus2 () {  # order + 2
     start="$1 $2 $3"
     order=$(($4 + 2))
     shift 4
@@ -21,7 +31,7 @@ orderplus2 () {
     $start $order $end >$fileA &
 }
 
-orderstep () {
+orderstep () {  # order + 1, step / 2
     start="$1 $2 $3"
     order=$(($4 + 1))
     step=$(echo "scale=6; $5 / 2;" | bc)
@@ -30,4 +40,15 @@ orderstep () {
     end="$*"
     echo 'Better:' $start $order $step $steps $end >&2
     $start $order $step $steps $end | sed -n '1~2p' >$fileA &
+}
+
+orderstep2 () {  # order + 2, step / 4
+    start="$1 $2 $3"
+    order=$(($4 + 2))
+    step=$(echo "scale=6; $5 / 4;" | bc)
+    steps=$(($6 * 4))
+    shift 6
+    end="$*"
+    echo 'Better:' $start $order $step $steps $end >&2
+    $start $order $step $steps $end | sed -n '1~4p' >$fileA &
 }
