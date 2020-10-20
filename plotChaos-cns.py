683e696eaa01e02a5a84e13ca2ed4a045c798eab
Ian Smith <m4r35n357@gmail.com>
2020-10-20 11:55:13 +0100

    bring up to date with tiny branch scripts

--- plotChaos-cns.py
+++ plotChaos-cns.py
@@ -2,14 +2,16 @@
 #
 #  (c) 2018-2020 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 #
+#  Example basename='images/auto'; t=1000; while [ $t -le 10000 ]; do echo "$basename-$t"; time -p ./chaos-scan-cns thomas .01 .21 .1 $t 2>/dev/null | ./plotChaos-cns.py "$basename-$t"; t=$((t + 1000)); done
 
 from sys import argv, stdin, stderr
 from matplotlib import pyplot
 
 def main():
-    print("Chaos Plotter: {}".format(argv), file=stderr)
-    if len(argv) > 1:
-        raise Exception('>>> ERROR! No parameters, thanks! <<<')
+    print(f'Chaos Plotter: {argv}', file=stderr)
+    save = False
+    if len(argv) == 2:
+        save = True
     coordinate_a, coordinate_b, coordinate_c = 0, 1, 4
     line = stdin.readline()
     ax1 = pyplot.figure().add_subplot(111)
@@ -24,13 +26,14 @@ def main():
         p = line.split()
         x.append(float(p[coordinate_a]))
         y.append(float(p[coordinate_c]))
-        if 'LIMIT-CYCLE' in str(p[coordinate_b]):
+        text = str(p[coordinate_b])
+        if 'LIMIT-CYCLE' in text or 'CONVERGED' in text:
             classification = 0.0
-        elif 'CHAOTIC' in str(p[coordinate_b]):
+        elif 'CHAOTIC' in text:
             classification = 1.0
-        elif 'UNCLASSIFIED' in str(p[coordinate_b]):
+        elif 'UNCLASSIFIED' in text:
             classification = 0.5
-        elif 'UNBOUNDED' in str(p[coordinate_b]):
+        elif 'UNBOUNDED' in text:
             classification = - 0.1
         else:
             raise Exception('>>> Classification Error! <<<')
@@ -39,7 +42,7 @@ def main():
         n += 1
     ax1.plot(x, y, 'bo-', markersize=0)
     ax2.plot(x, z, 'ro-', markersize=0)
-    pyplot.show()
+    pyplot.savefig(argv[1]) if save else pyplot.show()
 
 if __name__ == "__main__":
     main()
