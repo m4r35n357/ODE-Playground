d9c18f4e8992853e6eda07fae88546583bdcf323
Ian Smith <m4r35n357@gmail.com>
2020-11-06 22:55:47 +0000

    sync

--- plot3d.py
+++ plot3d.py
@@ -46,15 +46,11 @@ def main():
     print(f'Multi 3D ODE Plotter: {argv}', file=stderr)
     argc = len(argv)
     if argc == 1 or argc == 2:  # single plot, optional second arg is track length!
-        file1 = stdin
-    elif argc == 8:  # IC script
-        file1 = open(argv[1])
-        file2 = open(argv[2])
-        file3 = open(argv[3])
-        file4 = open(argv[4])
-        file5 = open(argv[5])
-        file6 = open(argv[6])
-        file7 = open(argv[7])
+        files = [stdin]
+    elif argc == 8:  # called by IC script
+        files = []
+        for arg in argv[1:]:
+            files.append(open(arg))
     else:
         raise Exception('>>> ERROR! Please supply 1, 2 or 7 arguments! <<<')
     # Setup display and initialise pi3d
@@ -74,41 +70,35 @@ def main():
     hud_string.position((-display.width + rt - lt) / 2.0, (0.9 * display.height - tp + bm) / 2.0, 1.0)
     hud_string.draw()  # NB has to be drawn before quick_change() is called as buffer needs to exist
 
-    particle1 = Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat'))
-    if argc == 2:
-        particle1 = Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.05, track_shader=Shader('mat_flat'), track_max=int(argv[1]))
-    if argc == 8:
-        particle2 = Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat'))
-        particle3 = Body(Shader('mat_light'), (1.0, 1.0, 0.0), 0.1, track_shader=Shader('mat_flat'))
-        particle4 = Body(Shader('mat_light'), (1.0, 1.0, 0.0), 0.1, track_shader=Shader('mat_flat'))
-        particle5 = Body(Shader('mat_light'), (1.0, 0.0, 1.0), 0.1, track_shader=Shader('mat_flat'))
-        particle6 = Body(Shader('mat_light'), (1.0, 0.0, 1.0), 0.1, track_shader=Shader('mat_flat'))
-        particle7 = Body(Shader('mat_light'), (1.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat'))
+    particles = []
+    if argc == 1:
+        particles.append(Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat')))
+    elif argc == 2:
+        particles.append(Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.05, track_shader=Shader('mat_flat'), track_max=int(argv[1])))
+    elif argc == 8:
+        particles.append(Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat')))
+        particles.append(Body(Shader('mat_light'), (0.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat')))
+        particles.append(Body(Shader('mat_light'), (1.0, 1.0, 0.0), 0.1, track_shader=Shader('mat_flat')))
+        particles.append(Body(Shader('mat_light'), (1.0, 1.0, 0.0), 0.1, track_shader=Shader('mat_flat')))
+        particles.append(Body(Shader('mat_light'), (1.0, 0.0, 1.0), 0.1, track_shader=Shader('mat_flat')))
+        particles.append(Body(Shader('mat_light'), (1.0, 0.0, 1.0), 0.1, track_shader=Shader('mat_flat')))
+        particles.append(Body(Shader('mat_light'), (1.0, 1.0, 1.0), 0.1, track_shader=Shader('mat_flat')))
     # Enable key presses and mouse
     keys = Keyboard()
     mouse = Mouse(restrict=False)
     mouse.start()
     omx, omy = mouse.position()
     # Display scene
-    line1 = file1.readline()
-    if argc == 8:
-        line2 = file2.readline()
-        line3 = file3.readline()
-        line4 = file4.readline()
-        line5 = file5.readline()
-        line6 = file6.readline()
-        line7 = file7.readline()
+    lines = []
+    for file in files:
+        lines.append(file.readline())
     while display.loop_running():
-        data1 = line1.split()
+        data = [lines[0].split()]
         if argc == 8:
-            data2 = line2.split()
-            data3 = line3.split()
-            data4 = line4.split()
-            data5 = line5.split()
-            data6 = line6.split()
-            data7 = line7.split()
+            for line in lines:
+                data.append(line.split())
         hud_string.quick_change(
-            f' t {float(data1[3]):-5.1f}  x {float(data1[0]):-5.1f}  y {float(data1[1]):-5.1f}  z {float(data1[2]):-5.1f} ')
+            f' t {float(data[0][3]):-5.1f}  x {float(data[0][0]):-5.1f}  y {float(data[0][1]):-5.1f}  z {float(data[0][2]):-5.1f} ')
         hud_string.draw()
         # camera control
         camera.reset()
@@ -116,58 +106,44 @@ def main():
         r_rot, r_tilt = radians(rot), radians(tilt)
         camera.position((cam_rad * sin(r_rot) * cos(r_tilt), cam_rad * sin(r_tilt), - cam_rad * cos(r_rot) * cos(r_tilt)))
         # plot the entities
-        particle1.pos = [float(data1[0]), float(data1[1]), float(data1[2])]
-        particle1.position_and_draw(trace_material=(0.0, 0.25, 0.0))
+        particles[0].pos = [float(data[0][0]), float(data[0][1]), float(data[0][2])]
+        particles[0].position_and_draw(trace_material=(0.0, 0.25, 0.0))
         if argc == 8:
-            particle2.pos = [float(data2[0]), float(data2[1]), float(data2[2])]
-            particle2.position_and_draw(trace_material=(0.0, 0.25, 0.0))
-            particle3.pos = [float(data3[0]), float(data3[1]), float(data3[2])]
-            particle3.position_and_draw(trace_material=(0.4, 0.0, 0.0))
-            particle4.pos = [float(data4[0]), float(data4[1]), float(data4[2])]
-            particle4.position_and_draw(trace_material=(0.4, 0.0, 0.0))
-            particle5.pos = [float(data5[0]), float(data5[1]), float(data5[2])]
-            particle5.position_and_draw(trace_material=(0.0, 0.0, 0.5))
-            particle6.pos = [float(data6[0]), float(data6[1]), float(data6[2])]
-            particle6.position_and_draw(trace_material=(0.0, 0.0, 0.5))
-            particle7.pos = [float(data7[0]), float(data7[1]), float(data7[2])]
-            particle7.position_and_draw(trace_material=(0.25, 0.25, 0.25))
-        # process mouse and keyboard input
+            particles[1].pos = [float(data[1][0]), float(data[1][1]), float(data[1][2])]
+            particles[1].position_and_draw(trace_material=(0.0, 0.25, 0.0))
+            particles[2].pos = [float(data[2][0]), float(data[2][1]), float(data[2][2])]
+            particles[2].position_and_draw(trace_material=(0.4, 0.0, 0.0))
+            particles[3].pos = [float(data[3][0]), float(data[3][1]), float(data[3][2])]
+            particles[3].position_and_draw(trace_material=(0.4, 0.0, 0.0))
+            particles[4].pos = [float(data[4][0]), float(data[4][1]), float(data[4][2])]
+            particles[4].position_and_draw(trace_material=(0.0, 0.0, 0.5))
+            particles[5].pos = [float(data[5][0]), float(data[5][1]), float(data[7][2])]
+            particles[5].position_and_draw(trace_material=(0.0, 0.0, 0.5))
+            particles[6].pos = [float(data[6][0]), float(data[6][1]), float(data[6][2])]
+            particles[6].position_and_draw(trace_material=(0.25, 0.25, 0.25))
+        # process mouse input
         mx, my = mouse.position()
         if mouse.button_status() == mouse.LEFT_BUTTON:
             rot += (mx - omx) * 0.2
             tilt += (my - omy) * 0.2
+        elif mouse.button_status() == mouse.RIGHT_BUTTON:
+            cam_rad += (my - omy) * 0.2
         omx, omy = mx, my
+        # process keyboard input
         key = keys.read()
         if key > -1:
             if key == 112:  # 'p'
                 screenshot('trajectory.jpg')
-            elif key == 119:  # 'w' rotate camera up
-                tilt += 2.0
-            elif key == 115:  # 's' down
-                tilt -= 2.0
-            elif key == 97:  # 'a' left
-                rot -= 2.0
-            elif key == 100:  # 'd' right
-                rot += 2.0
-            elif key == 61:  # '-' zoom in
-                cam_rad -= 0.5
-            elif key == 45:  # '=' zoom out
-                cam_rad += 0.5
             elif key == 27:  # 'ESC'
                 keys.close()
                 mouse.stop()
                 display.stop()
                 break
         # prepare for next iteration
-        line1 = file1.readline()
-        if argc == 8:
-            line2 = file2.readline()
-            line3 = file3.readline()
-            line4 = file4.readline()
-            line5 = file5.readline()
-            line6 = file6.readline()
-            line7 = file7.readline()
-        if not line1:
+        lines = []
+        for file in files:
+            lines.append(file.readline())
+        if not lines[0]:
             display.stop()
 
 main()
