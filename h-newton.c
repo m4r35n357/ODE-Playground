eda4d5b65fc934f96f4133a238a81ab108f85dfe
Ian Smith <m4r35n357@gmail.com>
2021-06-22 13:55:42 +0100

    fix Newton

--- h-newton.c
+++ h-newton.c
@@ -19,12 +19,12 @@
 #include "symplectic.h"
 #include "dual.h"
 
-static dual h (real m, real gm, dual q_r, dual p_r, dual p_phi) {
-    return d_sub(d_scale(d_add(d_sqr(p_r), d_div(d_sqr(p_phi), d_sqr(q_r))), 0.5L / m), d_scale(d_inv(q_r), gm));
+static dual h (real gm, dual q_r, dual p_r, dual p_phi) {
+    return d_sub(d_scale(d_add(d_sqr(p_r), d_div(d_sqr(p_phi), d_sqr(q_r))), 0.5L), d_scale(d_inv(q_r), gm));
 }
 
 typedef struct {
-    real m, gm;  // central mass, m X g
+    real m;  // central mass
     real q_r, p_r, q_phi, p_phi;  // coordinates & momenta
     real h0;  // stored initial value of Hamiltonian
 } parameters;
@@ -33,33 +33,32 @@ static parameters *get_p (int argc, char **argv, int va_begin) {
     parameters *p = malloc(sizeof (parameters));
     real r0, l_fac;
     t_variables(argv, va_begin, argc, &p->m, &r0, &l_fac);
-    p->gm = p->m;  //p->gm = 6.6743e-11L * p->m;
     p->q_r = r0;
     p->p_r = 0.0L;
     p->q_phi = 0.0L;
     p->p_phi = l_fac * p->m * sqrtl(r0);
-    p->h0 = h(p->m, p->gm, d_dual(p->q_r), d_dual(p->p_r), d_dual(p->p_phi)).val;
+    p->h0 = h(p->m, d_dual(p->q_r), d_dual(p->p_r), d_dual(p->p_phi)).val;
     return p;
 }
 
 void update_q (void *params, real c) {
     parameters *p = (parameters *)params;
-    real q_r = c * h(p->m, p->gm, d_dual(p->q_r), d_var(p->p_r), d_dual(p->p_phi)).dot;
-    real q_phi = c * h(p->m, p->gm, d_dual(p->q_r), d_dual(p->p_r), d_var(p->p_phi)).dot;
+    real q_r = c * h(p->m, d_dual(p->q_r), d_var(p->p_r), d_dual(p->p_phi)).dot;
+    real q_phi = c * h(p->m, d_dual(p->q_r), d_dual(p->p_r), d_var(p->p_phi)).dot;
     p->q_r += q_r;
     p->q_phi += q_phi;
 }
 
 void update_p (void *params, real d) {
     parameters *p = (parameters *)params;
-    p->p_r -= d * h(p->m, p->gm, d_var(p->q_r), d_dual(p->p_r), d_dual(p->p_phi)).dot;
+    p->p_r -= d * h(p->m, d_var(p->q_r), d_dual(p->p_r), d_dual(p->p_phi)).dot;
 }
 
 static void plot (long dp, void *params, real t) {
     parameters *p = (parameters *)params;
     char fs[128];
     sprintf(fs, "%%+.%ldLe %%+.%ldLe %%+.%ldLe %%+.6Le %%+.3Le %%+.3Le\n", dp, dp, dp);
-    real h_now = h(p->m, p->gm, d_dual(p->q_r), d_dual(p->p_r), d_dual(p->p_phi)).val;
+    real h_now = h(p->m, d_dual(p->q_r), d_dual(p->p_r), d_dual(p->p_phi)).val;
     printf(fs, p->q_r * sinl(p->q_phi), p->q_r * cosl(p->q_phi), 0.0L, t, error(h_now - p->h0), h_now);
 }
 
