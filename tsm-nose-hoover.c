06a1ae1aabf22d174356747409bebb0923910e31
Ian Smith <m4r35n357@gmail.com>
2021-09-06 20:07:00 +0100

    add Nose-Hoover attractor

---
+++ tsm-nose-hoover.c
@@ -0,0 +1,30 @@
+/*
+ * Nose-Hoover System
+ *
+ * Example: ./tsm-nose-hoover-dbg 15 10 0.01 10000 1 0 0 6.0
+ *
+ * (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include "taylor-ode.h"
+
+typedef struct { real a; } parameters;
+
+void *get_p (int argc, char **argv, long order) {
+    assert(argc == 9);
+    (void)order;
+    parameters *p = malloc(sizeof (parameters));
+    t_params(argv, argc, &p->a);
+    return p;
+}
+
+components ode (series x, series y, series z, void *params, int k) {
+    parameters *p = (parameters *)params;
+    return (components) {
+        .x = y[k],
+        .y = t_prod(y, z, k) - x[k],
+        .z = p->a - t_sqr(y, k)
+    };
+}
