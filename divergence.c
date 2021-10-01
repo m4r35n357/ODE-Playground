45a60d1b91a1943aae365b4ce65c0f88ade5b543
Ian Smith <m4r35n357@gmail.com>
2021-10-01 18:41:20 +0100

    getting CNS to work

--- divergence.c
+++ divergence.c
@@ -19,11 +19,10 @@ int main(int argc, char **argv) {
         exit(1);
     }
     real xA, yA, zA, xB, yB, zB, t;
-    char bl[2];
     for (int i = 3; i < argc; i++) {
         real threshold = strtold(argv[i], NULL);
-        while(fscanf(fileA, "%Le %Le %Le %Le %s %s %s", &xA, &yA, &zA, &t, bl, bl, bl) != EOF &&
-              fscanf(fileB, "%Le %Le %Le %Le %s %s %s", &xB, &yB, &zB, &t, bl, bl, bl) != EOF) {
+        while(fscanf(fileA, "%Le %Le %Le %Le", &xA, &yA, &zA, &t) != EOF &&
+              fscanf(fileB, "%Le %Le %Le %Le", &xB, &yB, &zB, &t) != EOF) {
             if (sqrtl((xA - xB) * (xA - xB) + (yA - yB) * (yA - yB) + (zA - zB) * (zA - zB)) > threshold) {
                 fprintf(stdout, "%s %.1Le %s %.3Lf\n", "Threshold:", threshold, "t:", t);
                 break;
