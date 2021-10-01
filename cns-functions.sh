4932e0ba652ef974a4f2d265dee79714f1a4bab5
Ian Smith <m4r35n357@gmail.com>
2021-10-01 18:19:01 +0100

    thinking about CNS

---
+++ cns-functions.sh
@@ -0,0 +1,57 @@
+#
+#  (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+#
+
+fileA="/tmp/$USER/dataA"  # results of the "better" simulation
+fileB="/tmp/$USER/dataB"  # results of the requested simulation
+
+quarterstep () {  # step / 4
+    start="$1 $2 $3"
+    step=$(echo "scale=6; $4 / 4;" | /usr/bin/bc)
+    steps=$(($5 * 4))
+    shift 5
+    end="$*"
+    echo 'Better:' $start $step $steps $end >&2
+    $start $step $steps $end | sed -n '1~4p' >$fileA &
+}
+
+eightthstep () {  # step / 8
+    start="$1 $2 $3"
+    step=$(echo "scale=6; $4 / 8;" | /usr/bin/bc)
+    steps=$(($5 * 8))
+    shift 5
+    end="$*"
+    echo 'Better:' $start $step $steps $end >&2
+    $start $step $steps $end | sed -n '1~8p' >$fileA &
+}
+
+orderplus2 () {  # order + 2
+    start="$1 $2"
+    order=$(($3 + 2))
+    shift 3
+    end="$*"
+    echo 'Better:' $start $order $end >&2
+    $start $order $end >$fileA &
+}
+
+orderstep () {  # order + 1, step / 2
+    start="$1 $2"
+    order=$(($3 + 1))
+    step=$(echo "scale=6; $4 / 2;" | /usr/bin/bc)
+    steps=$(($5 * 2))
+    shift 5
+    end="$*"
+    echo 'Better:' $start $order $step $steps $end >&2
+    $start $order $step $steps $end | sed -n '1~2p' >$fileA &
+}
+
+orderstep2 () {  # order + 2, step / 4
+    start="$1 $2"
+    order=$(($3 + 2))
+    step=$(echo "scale=6; $4 / 4;" | /usr/bin/bc)
+    steps=$(($5 * 4))
+    shift 5
+    end="$*"
+    echo 'Better:' $start $order $step $steps $end >&2
+    $start $order $step $steps $end | sed -n '1~4p' >$fileA &
+}
---
+++ divergence.c
@@ -0,0 +1,33 @@
+/*
+ * (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ *
+ * Example:  ./divergence /tmp/$USER/dataA /tmp/$USER/dataB 0.000000000001 0.000000001 0.000001 0.001 1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+typedef long double real;
+
+int main(int argc, char **argv) {
+    assert(argc >= 4);
+    FILE *fileA, *fileB;
+    if ((fileA = fopen(argv[1], "r")) == NULL || (fileB = fopen(argv[2], "r")) == NULL) {
+        fprintf(stderr, "divergence: Cannot read data files!\n");
+        exit(1);
+    }
+    real xA, yA, zA, xB, yB, zB, t;
+    char bl[2];
+    for (int i = 3; i < argc; i++) {
+        real threshold = strtold(argv[i], NULL);
+        while(fscanf(fileA, "%Le %Le %Le %Le %s %s %s", &xA, &yA, &zA, &t, bl, bl, bl) != EOF &&
+              fscanf(fileB, "%Le %Le %Le %Le %s %s %s", &xB, &yB, &zB, &t, bl, bl, bl) != EOF) {
+            if (sqrtl((xA - xB) * (xA - xB) + (yA - yB) * (yA - yB) + (zA - zB) * (zA - zB)) > threshold) {
+                fprintf(stdout, "%s %.1Le %s %.3Lf\n", "Threshold:", threshold, "t:", t);
+                break;
+            }
+        }
+    }
+}
