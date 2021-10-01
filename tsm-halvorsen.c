cb4df6aa0e274b89c95871d8a3f8c5ab3eb2465d
Ian Smith <m4r35n357@gmail.com>
2021-10-01 14:52:41 +0100

    simplify Halvorsen

--- tsm-halvorsen.c
+++ tsm-halvorsen.c
@@ -11,35 +11,32 @@
 #include <mpfr.h>
 #include "taylor-ode.h"
 
-typedef struct { mpfr_t a, b, w4x, w4y, w4z, _; } parameters;
+typedef struct { mpfr_t a, b, _; } parameters;
 
 void *get_p (int argc, char **argv, long n) {
     assert(argc == 11);
     (void)n;
     parameters *p = malloc(sizeof (parameters));
     t_params(argv, argc, &p->a, &p->b);
-    mpfr_inits(p->w4x, p->w4y, p->w4z, p->_, NULL);
+    mpfr_init(p->_);
     return p;
 }
 
 void ode (series x, series y, series z, components *c, void *params, int k) {
     parameters *p = (parameters *)params;
-    mpfr_mul(p->w4x, x[k], p->b, RND);
-    mpfr_mul(p->w4y, y[k], p->b, RND);
-    mpfr_mul(p->w4z, z[k], p->b, RND);
-    //  x' = - Ax - 4y - 4z - y^2
-    mpfr_fma(p->_, p->a, x[k], *t_sqr(y, k), RND);
-    mpfr_add(p->_, p->w4y, p->_, RND);
-    mpfr_add(p->_, p->w4z, p->_, RND);
+    //  x' = - Ax - By - Bz - y^2
+    mpfr_fmma(p->_, p->b, y[k], p->b, z[k], RND);
+    mpfr_fma(p->_, p->a, x[k], p->_, RND);
+    mpfr_add(p->_, *t_sqr(y, k), p->_, RND);
     mpfr_neg(c->x, p->_, RND);
-    //  y' = - Ay - 4z - 4x - z^2
-    mpfr_fma(p->_, p->a, y[k], *t_sqr(z, k), RND);
-    mpfr_add(p->_, p->w4z, p->_, RND);
-    mpfr_add(p->_, p->w4x, p->_, RND);
+    //  y' = - Ay - Bz - Bx - z^2
+    mpfr_fmma(p->_, p->b, z[k], p->b, x[k], RND);
+    mpfr_fma(p->_, p->a, y[k], p->_, RND);
+    mpfr_add(p->_, *t_sqr(z, k), p->_, RND);
     mpfr_neg(c->y, p->_, RND);
-    //  z' = - Az - 4x - 4y - x^2
-    mpfr_fma(p->_, p->a, z[k], *t_sqr(x, k), RND);
-    mpfr_add(p->_, p->w4x, p->_, RND);
-    mpfr_add(p->_, p->w4y, p->_, RND);
+    //  z' = - Az - Bx - By - x^2
+    mpfr_fmma(p->_, p->b, x[k], p->b, y[k], RND);
+    mpfr_fma(p->_, p->a, z[k], p->_, RND);
+    mpfr_add(p->_, *t_sqr(x, k), p->_, RND);
     mpfr_neg(c->z, p->_, RND);
 }
