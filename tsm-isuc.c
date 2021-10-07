f49e24d19d290aa1743f572390c76afeeca4f828
Ian Smith <m4r35n357@gmail.com>
2021-10-07 20:37:37 +0100

    new model

---
+++ tsm-isuc.c
@@ -0,0 +1,35 @@
+/*
+ * Inverted smooth unimodal chaos http://www.atomosyd.net/spip.php?article218
+ *
+ * Example: ./tsm-isuc-dbg 9 32 10 .01 10000 .05 -.05 .3 .1 .1 4.2
+ *
+ * (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <mpfr.h>
+#include "taylor-ode.h"
+
+typedef struct { mpfr_t a, b, c; series x2py2; } parameters;
+
+void *get_p (int argc, char **argv, int n) {
+    assert(argc == 12);
+    parameters *p = malloc(sizeof (parameters));
+    t_params(argv, argc, &p->a, &p->b, &p->c);
+    p->x2py2 = t_jet(n);
+    return p;
+}
+
+void ode (series x, series y, series z, components *c, void *params, int k) {
+    parameters *p = (parameters *)params;
+    //  x' = z - y
+    mpfr_sub(c->x, z[k], y[k], RND);
+    //  y' = x - Ay
+    mpfr_fms(c->y, p->a, y[k], x[k], RND);
+    mpfr_neg(c->y, c->y, RND);
+    //  z' = B + Cz - (x^2 + y^2)z
+    mpfr_add(p->x2py2[k], *t_sqr(x, k), *t_prod(y, y, k), RND);
+    mpfr_fms(c->z, p->c, z[k], *t_prod(p->x2py2, z, k), RND);
+    mpfr_add(c->z, c->z, *t_const(p->b, k), RND);
+}
--- tsm-rf.c
+++ tsm-rf.c
@@ -12,14 +12,14 @@
 #include <mpfr.h>
 #include "taylor-ode.h"
 
-typedef struct { mpfr_t alpha, gamma, d1, d4; series a, b, c; } parameters;
+typedef struct { mpfr_t alpha, gamma, D1, D4; series a, b, c; } parameters;
 
 void *get_p (int argc, char **argv, int n) {
     assert(argc == 11);
     parameters *p = malloc(sizeof (parameters));
     t_params(argv, argc, &p->alpha, &p->gamma);
-    mpfr_init_set_ui(p->d1, 1, RND);
-    mpfr_init_set_ui(p->d4, 4, RND);
+    mpfr_init_set_ui(p->D1, 1, RND);
+    mpfr_init_set_ui(p->D4, 4, RND);
     p->a = t_jet(n); p->b = t_jet(n); p->c = t_jet(n);
     return p;
 }
@@ -27,11 +27,11 @@ void *get_p (int argc, char **argv, int n) {
 void ode (series x, series y, series z, components *c, void *params, int k) {
     parameters *p = (parameters *)params;
     //  x' = y(z - 1 + x^2) + Gx
-    mpfr_sub(p->a[k], *t_sqr(x, k), *t_const(p->d1, k), RND);
+    mpfr_sub(p->a[k], *t_sqr(x, k), *t_const(p->D1, k), RND);
     mpfr_add(p->a[k], z[k], p->a[k], RND);
     mpfr_fma(c->x, p->gamma, x[k], *t_prod(y, p->a, k), RND);
     //  y' = x(3z + 1 - x^2) + Gy
-    mpfr_fms(p->b[k], p->d4, z[k], p->a[k], RND);
+    mpfr_fms(p->b[k], p->D4, z[k], p->a[k], RND);
     mpfr_fma(c->y, p->gamma, y[k], *t_prod(x, p->b, k), RND);
     //  z' = -2z(A + xy)
     mpfr_add(p->c[k], *t_prod(x, y, k), *t_const(p->alpha, k), RND);
