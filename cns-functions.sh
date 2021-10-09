b612ef57cd2d0463aa351fe9aff55aad4376ba5e
Ian Smith <m4r35n357@gmail.com>
2021-10-09 09:38:59 +0100

    sensible colleciton of CNS methods back out bad CNS changes

--- cns-functions.sh
+++ cns-functions.sh
@@ -5,8 +5,18 @@
 fileA="/tmp/$USER/dataA"  # results of the "better" simulation
 fileB="/tmp/$USER/dataB"  # results of the requested simulation
 
+halfstep () {  # step / 2
+    start="$1 $2 $3 $4"
+    step=$(echo "scale=6; $5 / 2;" | /usr/bin/bc)
+    steps=$(($6 * 2))
+    shift 6
+    end="$*"
+    echo 'Better:' $start $step $steps $end >&2
+    $start $step $steps $end | sed -n '1~2p' >$fileA &
+}
+
 quarterstep () {  # step / 4
-    start="$1 $2 $max $4"
+    start="$1 $2 $3 $4"
     step=$(echo "scale=6; $5 / 4;" | /usr/bin/bc)
     steps=$(($6 * 4))
     shift 6
@@ -15,19 +25,9 @@ quarterstep () {  # step / 4
     $start $step $steps $end | sed -n '1~4p' >$fileA &
 }
 
-eightthstep () {  # step / 8
-    start="$1 $2 $max $4"
-    step=$(echo "scale=6; $5 / 8;" | /usr/bin/bc)
-    steps=$(($6 * 8))
-    shift 6
-    end="$*"
-    echo 'Better:' $start $step $steps $end >&2
-    $start $step $steps $end | sed -n '1~8p' >$fileA &
-}
-
-orderplus2 () {  # order + 2
-    start="$1 $2 $max"
-    order=$(($4 + 2))
+orderplus1 () {  # order + 1
+    start="$1 $2 $3"
+    order=$(($4 + 1))
     shift 4
     end="$*"
     echo 'Better:' $start $order $end >&2
@@ -35,7 +35,7 @@ orderplus2 () {  # order + 2
 }
 
 orderstep () {  # order + 1, step / 2
-    start="$1 $2 $max"
+    start="$1 $2 $3"
     order=$(($4 + 1))
     step=$(echo "scale=6; $5 / 2;" | /usr/bin/bc)
     steps=$(($6 * 2))
@@ -43,15 +43,4 @@ orderstep () {  # order + 1, step / 2
     end="$*"
     echo 'Better:' $start $order $step $steps $end >&2
     $start $order $step $steps $end | sed -n '1~2p' >$fileA &
-}
-
-orderstep2 () {  # order + 2, step / 4
-    start="$1 $2 $max"
-    order=$(($4 + 2))
-    step=$(echo "scale=6; $5 / 4;" | /usr/bin/bc)
-    steps=$(($6 * 4))
-    shift 6
-    end="$*"
-    echo 'Better:' $start $order $step $steps $end >&2
-    $start $order $step $steps $end | sed -n '1~4p' >$fileA &
 }
