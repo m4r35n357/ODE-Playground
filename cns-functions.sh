ec03500919e2edb8c903e71be23725d7125cd211
Ian Smith <m4r35n357@gmail.com>
2021-10-16 20:37:28 +0100

    repair CNS

--- cns-functions.sh
+++ cns-functions.sh
@@ -6,21 +6,21 @@ fileA="/tmp/$USER/dataA"  # results of the "better" simulation
 fileB="/tmp/$USER/dataB"  # results of the requested simulation
 
 halfstep () {  # step / 2
-    start="$1 $2 $3 $4"
-    step=$(echo "scale=6; $5 / 2;" | /usr/bin/bc)
-    steps=$(($6 * 2))
-    shift 6
+    start="$1 $2 $3"
+    step=$(echo "scale=6; $4 / 2;" | /usr/bin/bc)
+    steps=$(($5 * 2))
+    shift 5
     end="$*"
     echo 'Better:' $start $step $steps $end >&2
     $start $step $steps $end | sed -n '1~2p' >$fileA &
 }
 
 orderstep () {  # order + 1, step / 2
-    start="$1 $2 $3"
-    order=$(($4 + 1))
-    step=$(echo "scale=6; $5 / 2;" | /usr/bin/bc)
-    steps=$(($6 * 2))
-    shift 6
+    start="$1 $2"
+    order=$(($3 + 1))
+    step=$(echo "scale=6; $4 / 2;" | /usr/bin/bc)
+    steps=$(($5 * 2))
+    shift 5
     end="$*"
     echo 'Better:' $start $order $step $steps $end >&2
     $start $order $step $steps $end | sed -n '1~2p' >$fileA &
