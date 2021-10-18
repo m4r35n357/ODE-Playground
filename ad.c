20f4e997779bd2048c183bc3269968536499fa8b
Ian Smith <m4r35n357@gmail.com>
2021-10-18 17:40:31 +0100

    port libad-test from master branch

---
+++ ad.c
@@ -0,0 +1,145 @@
+/*
+ * (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "taylor-ode.h"
+#include "ad.h"
+
+static int order;
+
+void ad_init (int n) {
+    order = n;
+}
+
+void jet_output (series s, char* f_colour, char *fk_colour) {
+    printf("%s%9.6Lf ", f_colour, s[0]);
+    for (int i = 1; i < order; i++) {
+        printf("%s%9.6Lf ", fk_colour, s[i]);
+    }
+    printf("%s\n", f_colour);
+}
+
+void jet_to_derivs (series s) {
+    for (int i = 1, fac = 1; i < order; i++) {
+        fac *= i;
+        s[i] *= fac;
+    }
+}
+
+void derivative_output (series jet, char* f_colour, char *fk_colour) {
+    jet_to_derivs(jet);
+    jet_output(jet, f_colour, fk_colour);
+}
+
+series ad_set (series b, series a) {
+    for (int k = 0; k < order; k++) {
+        b[k] = a[k];
+    }
+    return b;
+}
+
+series ad_scale (series s, series u, real a) {
+    for (int k = 0; k < order; k++) {
+        s[k] = u[k] * a;
+    }
+    return s;
+}
+
+series ad_plus (series p, series u, series v) {
+    for (int k = 0; k < order; k++) {
+		p[k] = u[k] + v[k];
+    }
+    return p;
+}
+
+series ad_minus (series m, series u, series v) {
+    for (int k = 0; k < order; k++) {
+		m[k] = u[k] - v[k];
+    }
+    return m;
+}
+
+series ad_neg (series m, series u) {
+    for (int k = 0; k < order; k++) {
+        m[k] = - u[k];
+    }
+    return m;
+}
+
+series ad_abs (series a, series u) {
+    for (int k = 0; k < order; k++) {
+        a[k] = t_abs(u, k);
+    }
+    return a;
+}
+
+series ad_prod (series p, series u, series v) {
+    for (int k = 0; k < order; k++) {
+        p[k] = t_prod(u, v, k);
+    }
+    return p;
+}
+
+series ad_quot (series q, series u, series v) {
+    for (int k = 0; k < order; k++) {
+        t_quot(q, u, v, k);
+    }
+    return q;
+}
+
+series ad_inv (series i, series v) {
+    for (int k = 0; k < order; k++) {
+        t_inv(i, v, k);
+    }
+    return i;
+}
+
+series ad_sqr (series s, series u) {
+    for (int k = 0; k < order; k++) {
+        s[k] = t_sqr(u, k);
+    }
+    return s;
+}
+
+series ad_sqrt (series r, series u) {
+    for (int k = 0; k < order; k++) {
+        t_sqrt(r, u, k);
+    }
+    return r;
+}
+
+series ad_exp (series e, series u) {
+    for (int k = 0; k < order; k++) {
+        t_exp(e, u, k);
+    }
+    return e;
+}
+
+void ad_sin_cos (series s, series c, series u, geometry g) {
+    for (int k = 0; k < order; k++) {
+        t_sin_cos(s, c, u, k, g);
+    }
+}
+
+void ad_tan_sec2 (series t, series s2, series u, geometry g) {
+    for (int k = 0; k < order; k++) {
+        t_tan_sec2(t, s2, u, k, g);
+    }
+}
+
+series ad_pwr (series p, series u, real a) {
+    for (int k = 0; k < order; k++) {
+        t_pwr(p, u, a, k);
+    }
+    return p;
+}
+
+series ad_ln (series l, series u) {
+    for (int k = 0; k < order; k++) {
+        t_ln(l, u, k);
+    }
+    return l;
+}
---
+++ libad-test.c
@@ -0,0 +1,264 @@
+/*
+ * Automatic Differentiation of Taylor Seriesewest validation checks
+ *
+ * Example: ./libad-test-dbg 32 20 1 1e-18 [ 0 | 1 | 2 ]
+ *
+ * (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "taylor-ode.h"
+#include "ad.h"
+
+#define KNRM "\x1B[0;37m"
+#define KWHT "\x1B[1;37m"
+#define KGRN "\x1B[1;32m"
+#define KYLW "\x1B[1;33m"
+#define KRED "\x1B[1;31m"
+
+typedef enum {PASSED, SKIPPED, FAILED} result;
+
+static int n, debug = 0, total = 0, passed = 0, skipped = 0;
+
+static real x0, delta, tolerance;
+
+static void libad_test_init (void) {
+    ad_init(n);
+}
+
+typedef struct { real a, b, c; } parameters;
+
+void *get_p (int argc, char **argv, long order) {
+    (void)argc; (void)argv; (void)order;
+    parameters *p = malloc(sizeof (parameters));
+    p->a = 1.0;
+    p->b = 0.0;
+    p->c = -1.0;
+    return p;
+}
+
+components ode (series x, series y, series z, void *params, int k) {
+    parameters *p = (parameters *)params;
+    return (components) {
+        .x = p->a * x[k],
+        .y = p->b * y[k],
+        .z = p->c * z[k]
+    };
+}
+
+static result skip (char* name) {
+    total++;
+    skipped++;
+    printf("%sSKIPPED%s %s\n", KYLW, KNRM, name);
+    return SKIPPED;
+}
+
+static result compare (char* name, series a, series b) {
+    total++;
+    for (int k = 0; k < n; k++) {
+        delta = a[k] - b[k];
+        if (fabsl(delta) > tolerance) {
+            printf("%s FAILED%s %s  k: %d  LHS: %.6Le  RHS: %.6Le  diff %.3Le\n", KRED, KNRM, name, k, a[k], b[k], delta);
+            return FAILED;
+        }
+        if (debug >= 2 && k == 0) printf("\n");
+        if (debug >= 2) printf("%s  DEBUG%s  k: %2d  %+.6Le %+.6Le  diff %+.3Le\n", KNRM, KNRM, k, a[k], b[k], delta);
+    }
+    if (debug >= 1) printf("%s PASSED%s %s\n", KGRN, KNRM, name);
+    passed++;
+    return PASSED;
+}
+
+int main (int argc, char **argv) {
+    real PI_2;
+
+    assert(argc == 5 || argc == 6);
+    n = (int)strtol(argv[2], NULL, 10) + 1;
+    assert(n > 1);
+    libad_test_init();
+    x0 = strtold(argv[3], NULL);
+    tolerance = strtold(argv[4], NULL);
+    if (argc == 6) debug = (int)strtol(argv[5], NULL, 10);
+
+    series abs = t_jet(n);
+    series scale = t_jet(n);
+    series sum = t_jet(n);
+    series diff = t_jet(n);
+    series prod = t_jet(n);
+    series quot = t_jet(n);
+    series inv = t_jet(n);
+    series inv_x = t_jet(n);
+    series neg = t_jet(n);
+    series sqr_x = t_jet(n);
+    series sqr_sin_x = t_jet(n);
+    series sqr_cos_x = t_jet(n);
+    series sqr_tan_x = t_jet(n);
+    series sqrt_x = t_jet(n);
+    series pow = t_jet(n);
+    series exp_x = t_jet(n);
+    series neg_exp_x = t_jet(n);
+    series ln = t_jet(n);
+    series ln_x = t_jet(n);
+    series sin = t_jet(n);
+    series sin_2x = t_jet(n);
+    series cos = t_jet(n);
+    series cos_2x = t_jet(n);
+    series tan = t_jet(n);
+    series sec2 = t_jet(n);
+
+    series c1 = t_jet(n);
+    c1[0] = 1.0L;
+    series x = t_jet(n);
+    x[0] = x0;
+
+    PI_2 = 0.5 * MY_PI;
+
+    int x_positive = x[0] > 0;
+    int x_non_zero = x[0] != 0;
+    int x_lt_pi_2 = x[0] < PI_2;
+
+    printf("\n");
+    printf("Horner\n");
+    series __ = t_jet(n);
+    __[0] = 1;
+    __[1] = 3;
+    __[2] = 0;
+    __[3] = 2;
+    printf(" 23 %8.3Lf\n", t_horner(__, 3, 2.0L));
+    __[0] = 3;
+    __[1] = -1;
+    __[2] = 2;
+    __[3] = -4;
+    __[4] = 0;
+    __[5] = 1;
+    printf("153 %8.3Lf\n", t_horner(__, 5, 3.0L));
+    __[0] = 1;
+    __[1] = -4;
+    __[2] = 0;
+    __[3] = 0;
+    __[4] = 2;
+    __[5] = 3;
+    __[6] = 0;
+    __[7] = -2;
+    printf("201 %8.3Lf\n", t_horner(__, 7, -2.0L));
+
+    printf("\n");
+    printf("TSM\n");
+    tsm(argc, argv, 12, 10, 0.1L, 10, 1.0L, 1.0L, 1.0L);
+    real e1 = expl(1.0L), e0 = expl(0.0L), e_1 = expl(-1.0L);
+    printf("Check\n");
+    t_output(12, e1, e0, e_1, 1.0L, "_", "_", "_");
+
+    printf("\n");
+    ad_sqr(sqr_x, x);
+    if (x_non_zero) ad_inv(inv_x, x);
+    if (x_positive) ad_sqrt(sqrt_x, x);
+    char* name = "x * x == sqr(x)";
+    compare(name, ad_prod(prod, x, x), sqr_x);
+    name = "sqr(x) / x == x";
+    x_non_zero ? compare(name, ad_quot(quot, sqr_x, x), x) : skip(name);
+    name = "x * 1 / x == 1";
+    x_non_zero ? compare(name, ad_prod(prod, x, inv_x), c1) : skip(name);
+    name = "sqrt(x) * sqrt(x) == x";
+    x_positive ? compare(name, ad_prod(prod, sqrt_x, sqrt_x), x) : skip(name);
+    name = "x / sqrt(x) == sqrt(x)";
+    x_positive ? compare(name, ad_quot(quot, x, sqrt_x), sqrt_x) : skip(name);
+
+    if (debug != 0) printf("\n");
+    name = "x^2 == sqr(x)";
+    x_positive ? compare(name, ad_pwr(pow, x, 2.0L), sqr_x) : skip(name);
+    name = "x^1 == x";
+    x_positive ? compare(name, ad_pwr(pow, x, 1.0L), x) : skip(name);
+    name = "x^0.5 == sqrt(x)";
+    x_positive ? compare(name, ad_pwr(pow, x, 0.5L), sqrt_x): skip(name);
+    name = "x^0 == 1";
+    x_positive ? compare(name, ad_pwr(pow, x, 0.0L), c1) : skip(name);
+    name = "x^-0.5 == 1 / sqrt(x)";
+    x_positive ? compare(name, ad_pwr(pow, x, -0.5L), ad_inv(inv, sqrt_x)) : skip(name);
+    name = "x^-1 == 1 / x";
+    x_positive ? compare(name, ad_pwr(pow, x, -1.0L), inv_x) : skip(name);
+    name = "x^-2 == 1 / sqr(x)";
+    x_positive ? compare(name, ad_pwr(pow, x, -2.0L), ad_inv(inv, sqr_x)) : skip(name);
+
+    if (debug != 0) printf("\n");
+    name = "sqr(x) * x^-3 == 1 / x";
+    x_positive ? compare(name, ad_prod(prod, sqr_x, ad_pwr(pow, x, -3.0L)), inv_x) : skip(name);
+
+    name = "sqr(x)^0.5 == |x|";
+    x_non_zero ? compare(name, ad_pwr(pow, sqr_x, 0.5L), ad_abs(abs, x)) : skip(name);
+
+    if (debug != 0) printf("\n");
+    name = "log(e^x) == x";
+    compare(name, ad_ln(ln, ad_exp(exp_x, x)), x);
+    name = "log(sqr(x)) == log(x) * 2";
+    if (x_positive) ad_ln(ln_x, x);
+    x_positive ? compare(name, ad_ln(ln, sqr_x), ad_scale(scale, ln_x, 2.0L)) : skip(name);
+    name = "log(sqrt(x)) == log(x) / 2";
+    x_positive ? compare(name, ad_ln(ln, sqrt_x), ad_scale(scale, ln_x, 0.5L)) : skip(name);
+    name = "log(1 / x) == - log(x)";
+    x_positive ? compare(name, ad_ln(ln, inv_x), ad_neg(neg, ln_x)) : skip(name);
+    name = "log(x^-3) == - 3 * log(x)";
+    x_positive ? compare(name, ad_ln(ln, ad_pwr(pow, x, -3.0L)), ad_scale(scale, ln_x, -3.0L)) : skip(name);
+
+    if (debug != 0) printf("\n");
+    ad_sin_cos(sin, cos, x, HYP);
+    ad_tan_sec2(tan, sec2, x, HYP);
+    ad_sqr(sqr_sin_x, sin);
+    ad_sqr(sqr_cos_x, cos);
+    ad_sin_cos(sin_2x, cos_2x, ad_scale(scale, x, 2.0L), HYP);
+    name = "cosh^2(x) - sinh^2(x) == 1";
+    compare(name, ad_minus(diff, sqr_cos_x, sqr_sin_x), c1);
+    name = "sech^2(x) + tanh^2(x) == 1";
+    compare(name, ad_plus(sum, sec2, ad_sqr(sqr_tan_x, tan)), c1);
+    name = "tanh(x) == sinh(x) / cosh(x)";
+    compare(name, tan, ad_quot(quot, sin, cos));
+    name = "sech^2(x) == 1 / cosh^2(x)";
+    compare(name, sec2, ad_inv(inv, sqr_cos_x));
+    name = "sinh(2x) == 2 * sinh(x) * cosh(x)";
+    compare(name, sin_2x, ad_scale(scale, ad_prod(prod, sin, cos), 2.0L));
+    name = "cosh(2x) == cosh^2(x) + sinh^2(x)";
+    compare(name, cos_2x, ad_plus(sum, sqr_cos_x, sqr_sin_x));
+
+    if (debug != 0) printf("\n");
+    ad_exp(exp_x, x);
+    ad_exp(neg_exp_x, ad_neg(neg, x));
+    name = "cosh(x) == (e^x + e^-x) / 2";
+    compare(name, cos, ad_scale(scale, ad_plus(sum, exp_x, neg_exp_x), 0.5L));
+    name = "sinh(x) == (e^x - e^-x) / 2";
+    compare(name, sin, ad_scale(scale, ad_minus(diff, exp_x, neg_exp_x), 0.5L));
+
+    if (debug != 0) printf("\n");
+    ad_sin_cos(sin, cos, x, TRIG);
+    ad_tan_sec2(tan, sec2, x, TRIG);
+    ad_sqr(sqr_sin_x, sin);
+    ad_sqr(sqr_cos_x, cos);
+    ad_sin_cos(sin_2x, cos_2x, ad_scale(scale, x, 2.0L), TRIG);
+    name = "cos^2(x) + sin^2(x) == 1";
+    compare(name, ad_plus(sum, sqr_cos_x, sqr_sin_x), c1);
+    name = "sec^2(x) - tan^2(x) == 1";
+    x_lt_pi_2 ? compare(name, ad_minus(diff, sec2, ad_sqr(sqr_tan_x, tan)), c1) : skip(name);
+    name = "tan(x) == sin(x) / cos(x)";
+    x_lt_pi_2 ? compare(name, tan, ad_quot(quot, sin, cos)) : skip(name);
+    name = "sec^2(x) == 1 / cos^2(x)";
+    x_lt_pi_2 ? compare(name, sec2, ad_inv(inv, sqr_cos_x)) : skip(name);
+    name = "sin(2x) == 2 * sin(x) * cos(x)";
+    compare(name, sin_2x, ad_scale(scale, ad_prod(prod, sin, cos), 2.0L));
+    name = "cos(2x) == cos^2(x) - sin^2(x)";
+    compare(name, cos_2x, ad_minus(diff, sqr_cos_x, sqr_sin_x));
+
+    if (debug != 0) printf("\n");
+    printf("%sTotal%s: %d, %sPASSED%s %d", KWHT, KNRM, total, KGRN, KNRM, passed);
+    if (skipped > 0) {
+        printf(", %sSKIPPED%s %d", KYLW, KNRM, skipped);
+    }
+    if (passed == total - skipped) {
+        printf("\n\n");
+        return 0;
+    } else {
+        printf(", %sFAILED%s %d\n\n", KRED, KNRM, total - passed - skipped);
+        return 1;
+    }
+}
