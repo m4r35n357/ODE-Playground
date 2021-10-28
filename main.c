4a5fa9ff7855c924fd4f4b43c85600ee874ea9f8
Ian Smith <m4r35n357@gmail.com>
2021-10-28 12:53:27 +0100

    imore descriptive variable names in main()

--- main.c
+++ main.c
@@ -9,20 +9,23 @@
 #include "taylor-ode.h"
 
 int main (int argc, char **argv) {
-    int dp = (int)strtol(argv[1], NULL, BASE);
-    double precision = strtod(argv[2], NULL) * 3.322;
-    mpfr_set_default_prec((int)precision);
+    int display_precision = (int)strtol(argv[1], NULL, BASE);
+    double internal_precision = strtod(argv[2], NULL) * 3.322;
+    mpfr_set_default_prec((int)internal_precision);
     fprintf(stderr, " MPFR default precision: %lu bits\n", mpfr_get_default_prec());
-    int n = (int)strtol(argv[3], NULL, BASE);
-    mpfr_t h, x0, y0, z0;
-    mpfr_init_set_str(h, argv[4], BASE, RND);
+    t_init(display_precision);
+
+    mpfr_t step_size, x0, y0, z0;
+
+    int order = (int)strtol(argv[3], NULL, BASE);
+    mpfr_init_set_str(step_size, argv[4], BASE, RND);
     int steps = (int)strtol(argv[5], NULL, BASE);
+
     mpfr_init_set_str(x0, argv[6], BASE, RND);
     mpfr_init_set_str(y0, argv[7], BASE, RND);
     mpfr_init_set_str(z0, argv[8], BASE, RND);
 
-    t_init(dp);
-    tsm(argc, argv, n, h, steps, x0, y0, z0);
+    tsm(argc, argv, order, step_size, steps, x0, y0, z0);
 
     return 0;
 }
