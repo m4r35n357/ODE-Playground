2db1b234606ce6a92827ce4af33be9eef0a7a5be
Ian Smith <m4r35n357@gmail.com>
2021-11-22 20:35:51 +0000

    readability

--- taylor-ode.c
+++ taylor-ode.c
@@ -133,23 +133,25 @@ real t_exp (series e, series u, int k) {
 
 pair t_sin_cos (series s, series c, series u, int k, geometry g) {
     assert(s != c && s != u && c != u);
+    _Bool trig = g == TRIG;
     return k == 0 ? (pair) {
-        .a = s[k] = g == TRIG ? sinl(u[0]) : sinhl(u[0]),
-        .b = c[k] = g == TRIG ? cosl(u[0]) : coshl(u[0])
+        .a = s[k] = trig ? sinl(u[0]) : sinhl(u[0]),
+        .b = c[k] = trig ? cosl(u[0]) : coshl(u[0])
     } : (pair) {
         .a = s[k] = f_k(c, u, k, 0),
-        .b = c[k] = f_k(s, u, k, 0) * (g == TRIG ? - 1.0L : 1.0L)
+        .b = c[k] = f_k(s, u, k, 0) * (trig ? - 1.0L : 1.0L)
     };
 }
 
 pair t_tan_sec2 (series t, series s, series u, int k, geometry g) {
     assert(t != s && t != u && s != u);
+    _Bool trig = g == TRIG;
     return k == 0 ? (pair) {
-        .a = t[k] = g == TRIG ? tanl(u[0]) : tanhl(u[0]),
-        .b = s[k] = g == TRIG ? 1.0L + t[0] * t[0] : 1.0L - t[0] * t[0]
+        .a = t[k] = trig ? tanl(u[0]) : tanhl(u[0]),
+        .b = s[k] = trig ? 1.0L + t[0] * t[0] : 1.0L - t[0] * t[0]
     } : (pair) {
         .a = t[k] = f_k(s, u, k, 0),
-        .b = s[k] = f_k(t, t, k, 0) * (g == TRIG ? 2.0L : - 2.0L)
+        .b = s[k] = f_k(t, t, k, 0) * (trig ? 2.0L : - 2.0L)
     };
 }
 
