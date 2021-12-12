03351ed2272bed1c72b661c7914c2c048d44b76b
Ian Smith <m4r35n357@gmail.com>
2021-12-12 16:36:07 +0000

    nice example

--- tsm-rf.c
+++ tsm-rf.c
@@ -2,7 +2,8 @@
  * Rabinovich–Fabrikant System
  *
  * Example: ./tsm-rf-dbg 15 10 .01 50000 .05 -.05 .3 .2873 .1
- *          ./tsm-rf-dbg 15 16 .01 50000 .05 -.05 .3 .105 .1 | ./plot3d.py 5000
+ * Example: ./tsm-rf-dbg 15 12 .01 100000 .05 -.05 .3 .116364 .1
+ * Example: ./tsm-rf-dbg 15 16 .01 50000 .05 -.05 .3 .105 .1 | ./plot3d.py 5000
  *
  * (c) 2018-2021 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
  */
