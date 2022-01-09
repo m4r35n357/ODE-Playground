9a7057b7db90179529b62756d43ed93ce8e63eaa
Ian Smith <m4r35n357@gmail.com>
2022-01-09 09:52:29 +0000

    improve t_pwr() doc comment

--- taylor-ode.h
+++ taylor-ode.h
@@ -264,13 +264,11 @@ pair t_tan_sec2 (series T, series S2, series U, int k, geometry g);
  *
  * U[0].k.P[k] + sum{j=1->k-1} U[j].(k-j).P[k-j] =  a.sum{j=0->k-1} P[j].(k-j).U[k-j]
  *
- *                                   U[0].k.P[k] =  a.sum{j=0->k-1} P[j].(k-j).U[k-j]   - sum{j=1->k-1} U[j].(k-j).P[k-j]
+ *                                          P[k] = (a.sum{j=0->k-1} P[j].(k-j).U[k-j] - sum{j=1->k-1} U[j].(k-j).P[k-j]) / k.U[0]
  *
- *                                          P[k] = (a.sum{j=0->k-1} P[j].(k-j).U[k-j]/k - sum{j=1->k-1} U[j].(k-j).P[k-j]/k) / U[0]
+ *                                               = (a.sum{j=0->k-1} P[j].(k-j).U[k-j] - sum{j=1->k-1} j.P[j].U[k-j]) / k.U[0]        (by symmetry)
  *
- *                                               = (a.sum{j=0->k-1} P[j].(k-j).U[k-j]/k - sum{j=1->k-1} j.P[j].U[k-j]/k) / U[0]              (by symmetry)
- *
- *                                               = sum{j=0->k-1} (a.(k-j)-j).P[j].U[k-j] / (k.U[0])             ("extra" P[0] term is multiplied by zero!)
+ *                                               = sum{j=0->k-1} (a.(k-j)-j).P[j].U[k-j] / k.U[0]       ("extra" P[0] term is multiplied by zero!)
  */
 real t_pwr (series P, series U, real a, int k);
 
