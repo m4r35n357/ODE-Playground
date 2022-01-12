7500dc47c87cb6837045b73fb3bf0507609cce4b
Ian Smith <m4r35n357@gmail.com>
2022-01-12 19:59:52 +0000

    test output goes to stderr

--- libad-test.c
+++ libad-test.c
@@ -167,7 +167,7 @@ int main (int argc, char **argv) {
     t_output(e1, e0, e_1, D01, steps);
 
     fprintf(stderr, "\n");
-    fprintf(stdout, "%sRecurrence Relations, x = %.1Lf%s\n", KWHT, mpfr_get_ld(x[0], RND), KNRM);
+    fprintf(stderr, "%sRecurrence Relations, x = %.1Lf%s\n", KWHT, mpfr_get_ld(x[0], RND), KNRM);
 
     ad_sqr(sqr_x, x);
     if (x_non_zero) ad_inv(inv_x, x);
