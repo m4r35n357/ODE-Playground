07384aa9039320e281c9f0a6a1021c787320fe35
Ian Smith <m4r35n357@gmail.com>
2022-07-12 17:33:23 +0100

    things move now

--- h-nbody.c
+++ h-nbody.c
@@ -20,12 +20,12 @@ static real distance (real xA, real yA, real zA, real xB, real yB, real zB) {
 static real h (nbody *nb) {
     real energy = 0.0L;
     for (int i = 0; i < nb->n; i += 1) {
-        body a = nb->bodies[i];
-        energy += 0.5L * (a.p_x * a.p_x + a.p_y * a.p_y + a.p_z * a.p_z) / a.m;
+        body *a = &nb->bodies[i];
+        energy += 0.5L * (a->p_x * a->p_x + a->p_y * a->p_y + a->p_z * a->p_z) / a->m;
         for (int j = 0; j < nb->n; j += 1) {
             if (i > j) {
-                body b = nb->bodies[j];
-                energy -= nb->g * a.m * b.m / distance(a.q_x, a.q_y, a.q_z, b.q_x, b.q_y, b.q_z);
+                body *b = &nb->bodies[j];
+                energy -= nb->g * a->m * b->m / distance(a->q_x, a->q_y, a->q_z, b->q_x, b->q_y, b->q_z);
             }
         }
     }
@@ -55,11 +55,11 @@ void update_q (void *n_body, real c) {
     nbody *nb = (nbody *)n_body;
     real tmp;
     for (int i = 0; i < nb->n; i += 1) {
-        body a = nb->bodies[i];
-        tmp = c / a.m;
-        a.q_x += a.p_x * tmp;
-        a.q_y += a.p_y * tmp;
-        a.q_z += a.p_z * tmp;
+        body *a = &nb->bodies[i];
+        tmp = c / a->m;
+        a->q_x += a->p_x * tmp;
+        a->q_y += a->p_y * tmp;
+        a->q_z += a->p_z * tmp;
     }
 }
 
@@ -67,21 +67,21 @@ void update_q (void *n_body, real c) {
 void update_p (void *n_body, real c) {
     nbody *nb = (nbody *)n_body;
     for (int i = 0; i < nb->n; i += 1) {
-        body a = nb->bodies[i];
+        body *a = &nb->bodies[i];
         for (int j = 0; j < nb->n; j += 1) {
             if (i > j) {
-                body b = nb->bodies[j];
-                real d = distance(a.q_x, a.q_y, a.q_z, b.q_x, b.q_y, b.q_z);
-                real tmp = - c * nb->g * a.m * b.m / (d * d * d);
-                real dPx = (b.q_x - a.q_x) * tmp;
-                real dPy = (b.q_y - a.q_y) * tmp;
-                real dPz = (b.q_z - a.q_z) * tmp;
-                a.p_x -= dPx;
-                a.p_y -= dPy;
-                a.p_z -= dPz;
-                b.p_x += dPx;
-                b.p_y += dPy;
-                b.p_z += dPz;
+                body *b = &nb->bodies[j];
+                real d = distance(a->q_x, a->q_y, a->q_z, b->q_x, b->q_y, b->q_z);
+                real tmp = - c * nb->g * a->m * b->m / (d * d * d);
+                real dPx = (b->q_x - a->q_x) * tmp;
+                real dPy = (b->q_y - a->q_y) * tmp;
+                real dPz = (b->q_z - a->q_z) * tmp;
+                a->p_x -= dPx;
+                a->p_y -= dPy;
+                a->p_z -= dPz;
+                b->p_x += dPx;
+                b->p_y += dPy;
+                b->p_z += dPz;
             }
         }
     }
