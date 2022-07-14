m4r35n357/ODE-Playground

commit 3b3d40152053bc021d3f012d1ab0426d6cb561ee

only do COG once, at the end

h-nbody-gl.c
@@ -162,6 +162,7 @@ static void Animate (void) {
 
     if (!stopped) {
         if (!(nb = (nbody *)generate(c, nb))) glutLeaveMainLoop();
+        cog(nb);
         if (stepping) {
             stopped = GL_TRUE;
         }

h-nbody.c
@@ -13,7 +13,7 @@
 #include "symplectic.h"
 #include "h-nbody.h"
 
-static void cog (nbody *nb) {
+void cog (nbody *nb) {
     real X = 0.0L, Y = 0.0L, Z = 0.0L, mT = 0.0L;
     for (int i = 0; i < nb->n; i += 1) {
         body *a = &nb->bodies[i];
@@ -78,7 +78,6 @@ void update_q (void *n_body, real c) {
         a->q_y += a->p_y * tmp;
         a->q_z += a->p_z * tmp;
     }
-    cog(nb);
 }
 
 void update_p (void *n_body, real c) {

h-nbody.h
@@ -11,3 +11,5 @@ typedef struct Nbody {
     body *bodies;
     real g, h0;
 } nbody;
+
+void cog (nbody *nb);

commit 2f84eb030bd82ddf6267cef0d8c7129b637d601f

fix segfault at end of data, introduced in 3b3d40152053bc021d3f012d1ab0426d6cb561ee

h-nbody-gl.c
@@ -132,8 +132,11 @@ static void Animate (void) {
     glutWireSphere(0.4F, 10, 10);
 
     if (!stopped) {
-        if (!(nb = (nbody *)generate(c, nb))) glutLeaveMainLoop();
-        cog(nb);
+        if (!(nbody *)generate(c, nb)) {
+			glutLeaveMainLoop();
+		} else {
+        	cog(nb);
+		}
         if (stepping) {
             stopped = GL_TRUE;
         }