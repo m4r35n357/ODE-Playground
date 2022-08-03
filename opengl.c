a3ef21829727d8803ddf672d4b610f5c04f5288c
Ian Smith <m4r35n357@gmail.com>
2022-08-03 10:11:54 +0100

    common OpenGL code for N-body, Kerr and ODE

---
+++ opengl.c
@@ -0,0 +1,73 @@
+/*
+ *  OpenGL common code
+ *
+ * (c) 2018-2022 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+ */
+
+#include <string.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "opengl.h"
+
+void OpenGLInit (void) {
+    glShadeModel(GL_FLAT);
+    glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
+    glClearDepth(1.0F);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+}
+
+void ResizeWindow (int w, int h) {
+    float aspectRatio;
+    h = (h == 0) ? 1 : h;
+    w = (w == 0) ? 1 : w;
+    glViewport(0, 0, w, h);   // View port uses whole window
+    aspectRatio = (float)w / (float)h;
+    // Set up the projection view matrix (not very well!)
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0F, aspectRatio, 1.0F, 100.0F);
+    // Select the Modelview matrix
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void ApplicationInit (int argc, char** argv, char *title) {
+    // Initialize GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);  // Need to double buffer for animation
+    // Create and position the graphics window
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow(title);
+    // Initialize GLEW
+    glewInit();
+    // Initialize OpenGL
+    OpenGLInit();
+    // Set up callback functions
+    glutKeyboardFunc(KeyPressFunc);
+    glutSpecialFunc(SpecialKeyFunc);
+    glutReshapeFunc(ResizeWindow);
+    glutDisplayFunc(Animate);
+}
+
+void osd (int x, int y, float r, float g, float b, char *string) {
+    glColor3f(r, g, b);
+    glWindowPos2i(x, y);
+    int len = (int)strlen(string);
+    for (int i = 0; i < len; i++) {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, string[i]);
+    }
+}
+
+void buffer_point (int max_points, int *oldest, int *newest, _Bool *full) {
+    *newest += 1;
+    if (! *full && (*newest == max_points)) {
+        *full = 1;
+    }
+    if (*full) {
+        *oldest = (*newest + 1) % max_points;
+        *newest %= max_points;
+    }
+}
--- opengl.h
+++ opengl.h
@@ -5,22 +5,26 @@ typedef enum Display {BOTH=0, BALLS=1, LINES=2} display;
 /*
  * OpenGL stuff . . .
  */
-void OpenGLInit(void);
+void OpenGLInit (void);
+
+void ApplicationInit (int argc, char** argv, char *title);
 
 /*
  * Callbacks
  */
-void SpecialKeyFunc(int Key, int x, int y);
+void SpecialKeyFunc (int Key, int x, int y);
 
-void KeyPressFunc(unsigned char Key, int x, int y);
+void KeyPressFunc (unsigned char Key, int x, int y);
 
-void ResizeWindow(int w, int h);
+void ResizeWindow (int w, int h);
 
-void Animate(void);
+void Animate (void);
 
-void CloseWindow(void);
+void CloseWindow (void);
 
 /*
  * OSD/HUD
  */
-void osd(int x, int y, float r, float g, float b, char *string);
+void osd (int x, int y, float r, float g, float b, char *string);
+
+void buffer_point (int max_points, int *oldest, int *newest, _Bool *full);
