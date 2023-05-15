d2f0a45bf0d0a931f72a00de326338e4ac80341b
Ian Smith <m4r35n357@gmail.com>
2023-05-15 20:24:52 +0100

    comments

--- real.h
+++ real.h
@@ -9,25 +9,31 @@ extern const int BASE;
 typedef long double real;
 
 /*
- * For returning combined values
+ * For returning combined values from trig/hyp functions
  */
 typedef struct pair_l {
     real a, b;
 } pair;
 
 /*
- * Triple of long doubles
+ * Triple of long doubles, by coordinate
  */
 typedef struct triple_l {
     real x, y, z;
 } triplet;
 
+/*
+ * Integrator parameters
+ */
 typedef struct Controls {
     int order, step, steps;
     real step_size;
-    pair r2, r4, r6, r8;
+    pair r2, r4, r6, r8;  // symplectic only
 } controls;
 
+/*
+ * Report program arguments in colour
+ */
 #define PRINT_ARGS(argc, argv) \
     fprintf(stderr, "argc: \x1B[1;37m%d\x1B[0;37m, argv: [ \x1B[0;36m", argc); \
     for (int i = 0; i < argc; i++) { \
@@ -35,7 +41,11 @@ typedef struct Controls {
     } \
     fprintf(stderr, "\x1B[0;37m]\n");
 
-#define CHECK(x) do { \
+/*
+ * Unavoidable assert(), in colour
+ */
+#define CHECK(x) \
+    do { \
         if(!(x)) { \
             fprintf(stderr, \
                 "\x1B[1;31mFAIL\x1B[0;37m \x1B[1;37m%s\x1B[0;37m \x1B[0;35m%s\x1B[0;37m:\x1B[0;35m%i\x1B[0;37m\n", \
