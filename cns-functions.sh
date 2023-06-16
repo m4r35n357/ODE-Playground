37b09984c543fbd41280ab413e19754003cf4fd5
Ian Smith <m4r35n357@gmail.com>
2023-06-16 10:46:06 +0100

    update scripts

--- cns-functions.sh
+++ cns-functions.sh
@@ -1,24 +1,19 @@
 #
-#  (c) 2018-2022 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+#  (c) 2018-2023 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 #
 
 fileA="/tmp/$USER/dataA"  # results of the "better" simulation
 fileB="/tmp/$USER/dataB"  # results of the requested simulation
 
 halfstep () {  # step / 2
-    algorithm="$1"
-    case $algorithm in
-        *tsm-*)	start="$1 $2 $3";;
-        *rk4-*)	start="$1 $2 $(($3 * 2))";;
-        *)      echo "CNS: Bad Executable Name"; exit;;
+    case $1 in
+        *tsm-*-std) start="$1 $2 $3";;
+                 *) echo "CNS: Bad Executable Name";
+                    exit;;
     esac
     step=$(echo "scale=9; $4 / 2;" | /usr/bin/bc)
     steps=$(($5 * 2))
     shift 5
     end="$*"
-    echo 'Better:' $start $step $steps $end >&2
-    case $algorithm in
-        *tsm-*)	$start $step $steps $end | sed -n '1~2p' >$fileA &;;
-        *rk4-*)	$start $step $steps $end >$fileA &;;
-    esac
+    $start $step $steps $end | /bin/sed -n '1~2p' >$fileA &
 }
