20a75aae85d4c18e5e5043289065fe484aa9ee42
Ian Smith <m4r35n357@gmail.com>
2023-06-16 09:59:12 +0100

    remove c stuff

--- real.h
+++
@@ -1,9 +0,0 @@
-/*
- * (c) 2018-2022 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
- */
-
-#pragma once
-
-typedef long double real;
-
-static const real MY_PI = 3.1415926535897932384626433832795029L;
