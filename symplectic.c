3f307981d52339da2e9094abafa4bbd10dcf92b9
Ian Smith <m4r35n357@gmail.com>
2023-07-14 09:50:43 +0100

    symplectic refresh

--- symplectic.c
+++ symplectic.c
@@ -29,78 +29,76 @@ static void second_order (controls *cont, void *p, real cd) { (void)cont;
 }
 
 static void suzuki (controls *c, void *p, integrator base, real cd, pair weights) {
-    for (int step = 0; step < 5; step++) {
-        base(c, p, cd * (step == 2 ? weights.b : weights.a));
-    }
+    for (int step = 0; step < 5; step++) base(c, p, cd * (step == 2 ? weights.b : weights.a));
 }
 
-static void base4 (controls *c, void *p, real cd) {
+static void _base4 (controls *c, void *p, real cd) {
     suzuki(c, p, second_order, cd, c->r2);
 }
 
 static void fourth_order (controls *c, void *p, real h) {
-    base4(c, p, h);
+    _base4(c, p, h);
 }
 
-static void base6 (controls *c, void *p, real cd) {
-    suzuki(c, p, base4, cd, c->r4);
+static void _base6 (controls *c, void *p, real cd) {
+    suzuki(c, p, _base4, cd, c->r4);
 }
 
 static void sixth_order (controls *c, void *p, real h) {
-    base6(c, p, h);
+    _base6(c, p, h);
 }
 
-static void base8 (controls *c, void *p, real cd) {
-    suzuki(c, p, base6, cd, c->r6);
+static void _base8 (controls *c, void *p, real cd) {
+    suzuki(c, p, _base6, cd, c->r6);
 }
 
 static void eightth_order (controls *c, void *p, real h) {
-    base8(c, p, h);
+    _base8(c, p, h);
 }
 
-static void base10 (controls *c, void *p, real cd) {
-    suzuki(c, p, base8, cd, c->r8);
+static void _base10 (controls *c, void *p, real cd) {
+    suzuki(c, p, _base8, cd, c->r8);
 }
 
 static void tenth_order (controls *c, void *p, real h) {
-    base10(c, p, h);
+    _base10(c, p, h);
 }
 
 static pair w (int order) {  // composition increases order by one, then symmetry bumps that to the next even order!
-    real forward = 1.0L / (4.0L - powl(4.0L, 1.0L / (order + 1)));
-    return (pair) { .a = forward, .b = 1.0L - 4.0L * forward };
+    real _ = 1.0L / (4.0L - powl(4.0L, 1.0L / (order + 1)));
+    return (pair){.a = _, .b = 1.0L - 4.0L * _};
 }
 
 static integrator get_integrator (controls *c) {
-    integrator composer = NULL;
+    integrator _ = NULL;
     switch (c->order) {
-        case  2: composer =  second_order; break;
-        case  4: composer =  fourth_order; c->r2 = w(2); break;
-        case  6: composer =   sixth_order; c->r2 = w(2); c->r4 = w(4); break;
-        case  8: composer = eightth_order; c->r2 = w(2); c->r4 = w(4); c->r6 = w(6); break;
-        case 10: composer =   tenth_order; c->r2 = w(2); c->r4 = w(4); c->r6 = w(6); c->r8 = w(8); break;
+        case  2: _ =  second_order; break;
+        case  4: _ =  fourth_order; c->r2 = w(2); break;
+        case  6: _ =   sixth_order; c->r2 = w(2); c->r4 = w(4); break;
+        case  8: _ = eightth_order; c->r2 = w(2); c->r4 = w(4); c->r6 = w(6); break;
+        case 10: _ =   tenth_order; c->r2 = w(2); c->r4 = w(4); c->r6 = w(6); c->r8 = w(8); break;
     }
-    CHECK(composer);
-    return composer;
+    CHECK(_);
+    return _;
 }
 
 void solve (char **argv, controls *c, void *p, plotter output) {
     int display_precision = (int)strtol(argv[1], NULL, BASE); CHECK(display_precision >= 1 && display_precision <= 32);
-    integrator composer = get_integrator(c);
+    integrator evolve = get_integrator(c);
     for (int step = 0; step < c->steps; step++) {
         output(display_precision, p, step * c->step_size);
-        composer(c, p, c->step_size);
+        evolve(c, p, c->step_size);
     }
     output(display_precision, p, c->steps * c->step_size);
 }
 
 bool generate (controls *c, void *p) {
     static bool looping = false;
-    static integrator composer;
+    static integrator evolve;
     if (looping) goto resume; else looping = true;
-    composer = get_integrator(c);
+    evolve = get_integrator(c);
     for (c->step = 0; c->step < c->steps; c->step++) {
-        composer(c, p, c->step_size);
+        evolve(c, p, c->step_size);
         return true;
         resume: ;
     }
