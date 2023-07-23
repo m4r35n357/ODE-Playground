591041205c8001cd1a41e1a9297822b25c782e7f
Ian Smith <m4r35n357@gmail.com>
2023-07-23 12:30:40 +0100

    fix divergence for data with no turning point tags!

--- divergence.c
+++ divergence.c
@@ -22,11 +22,11 @@ int main(int argc, char **argv) {
         exit(5);
     }
     long double xA, yA, zA, xB, yB, zB, tA, tB, cpuA, cpuB;
-    char *format = "%Le %Le %Le %Le %1s %1s %1s %Le", tag[2];
+    char *format = "%Le %Le %Le %Le %Le";
     for (int i = 3; i < argc; i++) {
         long double threshold = strtold(argv[i], NULL);
-        while(fscanf(fileA, format, &xA, &yA, &zA, &tA, tag, tag, tag, &cpuA) != EOF &&
-              fscanf(fileB, format, &xB, &yB, &zB, &tB, tag, tag, tag, &cpuB) != EOF) {
+        while(fscanf(fileA, format, &xA, &yA, &zA, &tA, &cpuA) != EOF &&
+              fscanf(fileB, format, &xB, &yB, &zB, &tB, &cpuB) != EOF) {
             if (sqrtl((xA - xB) * (xA - xB) + (yA - yB) * (yA - yB) + (zA - zB) * (zA - zB)) > threshold) {
                 printf("%s %.1Le  %s %6.3Lf  %s %.3Lf\n", "threshold:", threshold, "t:", tB, "cpu:", cpuB);
                 break;
