fa8e176a4d4aae6ccfe6e27875d21624427147b9
Ian Smith <m4r35n357@gmail.com>
2023-07-26 20:05:20 +0100

    Halvorsen fix

--- tsm-halvorsen.c
+++ tsm-halvorsen.c
@@ -9,28 +9,29 @@
 #include <mpfr.h>
 #include "taylor-ode.h"
 
-struct Parameters { mpfr_t a, b; };
+struct Parameters { mpfr_t a, D4; };
 
 parameters *get_p (int argc, char **argv, int n) { (void)n;
-    CHECK(argc == 11);
+    CHECK(argc == 10);
     parameters *p = malloc(sizeof (parameters));
-    t_params(argv, argc, &p->a, &p->b);
+    t_params(argv, argc, &p->a);
+    mpfr_init_set_si(p->D4, 4, RND);
     return p;
 }
 
 void ode (triplet *v_k, series x, series y, series z, parameters *p, int k) {
-    //  x' = - Ax - By - Bz - y^2
-    mpfr_fmma(v_k->x, p->b, y[k], p->b, z[k], RND);
+    //  x' = - Ax - 4y - 4z - y^2
+    mpfr_fmma(v_k->x, p->D4, y[k], p->D4, z[k], RND);
     mpfr_fma(v_k->x, p->a, x[k], v_k->x, RND);
     mpfr_add(v_k->x, *t_sqr(y, k), v_k->x, RND);
     mpfr_neg(v_k->x, v_k->x, RND);
-    //  y' = - Ay - Bz - Bx - z^2
-    mpfr_fmma(v_k->y, p->b, z[k], p->b, x[k], RND);
+    //  y' = - Ay - 4z - 4x - z^2
+    mpfr_fmma(v_k->y, p->D4, z[k], p->D4, x[k], RND);
     mpfr_fma(v_k->y, p->a, y[k], v_k->y, RND);
     mpfr_add(v_k->y, *t_sqr(z, k), v_k->y, RND);
     mpfr_neg(v_k->y, v_k->y, RND);
-    //  z' = - Az - Bx - By - x^2
-    mpfr_fmma(v_k->z, p->b, x[k], p->b, y[k], RND);
+    //  z' = - Az - 4x - 4y - x^2
+    mpfr_fmma(v_k->z, p->D4, x[k], p->D4, y[k], RND);
     mpfr_fma(v_k->z, p->a, z[k], v_k->z, RND);
     mpfr_add(v_k->z, *t_sqr(x, k), v_k->z, RND);
     mpfr_neg(v_k->z, v_k->z, RND);
