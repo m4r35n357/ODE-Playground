e331ffc509b81995a38435c12c834c4ccefe883d
Ian Smith <m4r35n357@gmail.com>
2023-07-27 12:17:38 +0100

    add function name to "assert" string

--- real.h
+++ real.h
@@ -46,21 +46,21 @@ typedef struct Controls {
  * Report program arguments in colour
  */
 #define PRINT_ARGS(argc, argv) \
-    fprintf(stderr, "argc: \x1B[1;37m%d\x1B[0;37m, argv: [ \x1B[0;36m", argc); \
-    for (int i = 0; i < argc; i++) { \
-        fprintf(stderr, "%s ", argv[i]); \
-    } \
-    fprintf(stderr, "\x1B[0;37m]\n");
+fprintf(stderr, "argc: \x1B[1;37m%d\x1B[0;37m, argv: [ \x1B[0;36m", argc); \
+for (int i = 0; i < argc; i++) { \
+    fprintf(stderr, "%s ", argv[i]); \
+} \
+fprintf(stderr, "\x1B[0;37m]\n");
 
 /*
  * Unavoidable assert(), in colour
  */
 #define CHECK(x) \
-    do { \
-        if(!(x)) { \
-            fprintf(stderr, \
-                "\x1B[1;31mFAIL\x1B[0;37m \x1B[1;37m%s\x1B[0;37m \x1B[0;35m%s\x1B[0;37m:\x1B[0;35m%i\x1B[0;37m\n", \
-                #x, __FILE__, __LINE__); \
-            exit(1); \
-        } \
-    } while (0)
+do { \
+    if(!(x)) { \
+        fprintf(stderr, \
+            "\x1B[1;31mFAIL\x1B[0;37m \x1B[1;37m%s\x1B[0;37m %s() \x1B[1;37m%s\x1B[0;37m:\x1B[1;37m%i\x1B[0;37m\n", \
+            #x, __func__, __FILE__, __LINE__); \
+        exit(1); \
+    } \
+} while (0)
