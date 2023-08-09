2b34fd26d153aa0025d4facce35df039fe26cd7f
Ian Smith <m4r35n357@gmail.com>
2023-08-09 11:52:46 +0100

    do more work in _tag_()

--- taylor-ode.c
+++ taylor-ode.c
@@ -75,17 +75,18 @@ static void _next_ (series3 *j, int n, real h) {
     j->z[0] = t_horner(j->z, n, h);
 }
 
-static char *_tag_ (series j, real slope, char *min, char *max) {
-    return j[1] * slope < 0.0L ? (j[2] > 0.0L ? min : max) : "_";
+static char *_tag_ (series j, real *slope, char *min, char *max) {
+    char *tag = *slope * j[1] < 0.0L ? (j[2] > 0.0L ? min : max) : "_";
+    *slope = j[1];
+    return tag;
 }
 
 void tsm_stdout (int dp, controls *c, series3 *j, parameters *p, clock_t t0) {
-    real slope_x = 0.0L, slope_y = 0.0L, slope_z = 0.0L;
+    real slopeX = 0.0L, slopeY = 0.0L, slopeZ = 0.0L;
     for (int step = 0; step < c->steps; step++) {
         _diff_(j, p, c->order);
         _out_(dp, j->x[0], j->y[0], j->z[0], c->step_size * step,
-             _tag_(j->x, slope_x, "x", "X"), _tag_(j->y, slope_y, "y", "Y"), _tag_(j->z, slope_z, "z", "Z"), t0);
-        slope_x = j->x[1]; slope_y = j->y[1]; slope_z = j->z[1];
+             _tag_(j->x, &slopeX, "x", "X"), _tag_(j->y, &slopeY, "y", "Y"), _tag_(j->z, &slopeZ, "z", "Z"), t0);
         _next_(j, c->order, c->step_size);
     }
     _out_(dp, j->x[0], j->y[0], j->z[0], c->step_size * c->steps, "_", "_", "_", t0);
