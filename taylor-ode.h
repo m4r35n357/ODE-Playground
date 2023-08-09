2755566361c9a3e2344bd8d657cdc7d74d4ce289
Ian Smith <m4r35n357@gmail.com>
2023-08-09 10:34:33 +0100

    restore some updated docs

--- taylor-ode.h
+++ taylor-ode.h
@@ -59,6 +59,20 @@ typedef struct triple_s {
 } series3;
 
 /*
+ * For returning x, y, z velocities from the model
+ */
+typedef struct triple_m {
+    mpfr_t x, y, z;
+} triplet;
+
+/*
+ * For returning combined recurrence values
+ */
+typedef struct pair_m {
+    mpfr_t *a, *b;
+} pair;
+
+/*
  * Prints a line of data to stdout
  */
 void t_out (mpfr_t x, mpfr_t y, mpfr_t z, mpfr_t h, int step, clock_t since);
@@ -81,15 +95,6 @@ mpfr_t *t_horner (series S, int n, mpfr_t h);
 void tsm (int places, int n, mpfr_t h, int steps, series3 *jets, parameters *P, clock_t since);
 
 /*
- * For returning x, y, z velocities from the model
- */
-typedef struct {
-    mpfr_t x;
-    mpfr_t y;
-    mpfr_t z;
-} triplet;
-
-/*
  * Get a blob of parameter data from the model to be passed into ode()
  */
 parameters *get_p (int argc, char **argv, int order);
@@ -105,7 +110,7 @@ void ode (triplet *V, series X, series Y, series Z, parameters *P, int k);
  */
 
 /*
- * Returns value if k is 0, and zero otherwise.  For handling _additive_ constants.
+ * Returns value if k is 0, and zero otherwise.  For handling _additive_ constants in ODE models.
  */
 mpfr_t *t_const (int n, mpfr_t a);
 
@@ -116,68 +121,149 @@ mpfr_t *t_abs (series U, int k);
 
 /*
  * Returns a pointer to kth element of the product of U and V, no user-supplied jet storage needed
+ *
+ *     P = U.V
+ *
+ *  P[k] = sum{j=0->k} U[j].V[k-j]
  */
 mpfr_t *t_mul (series U, series V, int k);
 
 /*
  * Returns a pointer to kth element of the square of U, no user-supplied jet storage needed
+ *
+ *     S = U.U
+ *
+ *  S[k] = sum{j=0->k} U[j].U[k-j]
  */
 mpfr_t *t_sqr (series U, int k);
 
 /*
  * Returns a pointer to kth element of U / V, results stored in user-supplied jet QUOT
+ *
+ *     Q = U / V ==> U = Q.V
+ *
+ *                U[k] = sum{j=0->k} Q[j].V[k-j]
+ *
+ *                     = sum{j=0->k-1} Q[j].V[k-j] + Q[k].V[0]
+ *
+ *                Q[k] = (U[k] - sum{j=0->k-1} Q[j].V[k-j]) / V[0]
+ *
+ *                     =  U[0] / V[0]                                 if k == 0
+ *
+ *                     = (U[k] - sum{j=0->k-1} Q[j].V[k-j]) / V[0]    otherwise
+ *
+ * If U == NULL, returns kth element of 1 / V, results stored in user-supplied jet Q,
+ *
+ * from above,    Q[k] = 1.0 / V[0]                                   if k == 0
+ *
+ *                Q[k] = - sum{j=0->k-1} Q[j].V[k-j] / V[0]           otherwise
  */
 mpfr_t *t_div (series QUOT, series U, series V, int k);
 
 /*
  * Returns a pointer to kth element of the square root of U, results stored in user-supplied jet ROOT
+ *
+ *     U = R.R
+ *
+ *  U[k] = sum{j=0->k} R[j].R[k - j]
+ *
+ *       = sum{j=1->k-1} R[j].R[k - j] + 2.R[k].R[0]
+ *
+ *  R[k] = (U[k] - sum{j=1->k-1} R[j].R[k-j]) / 2.R[0]
  */
 mpfr_t *t_sqrt (series ROOT, series U, int k);
 
 /*
  * Returns a pointer to kth element of the exponential of U, results stored in user-supplied jet EXP
+ *
+ *      EXP' = EXP.U'
+ *
+ *    EXP[k] = sum{j=0->k-1} EXP[j].(k-j).U[k-j]/k
  */
 mpfr_t *t_exp (series EXP, series U, int k);
 
 /*
- * For returning combined recurrence values
- */
-typedef struct {
-    mpfr_t *a;
-    mpfr_t *b;
-} pair;
-
-/*
  * Returns pointers to kth elements of both sine and cosine of U, results stored in user-supplied jets SIN and COS
+ *
+ *      SIN' =  COS.U' = COSH.U'
+ *      COS' = -SIN.U' = SINH.U'
+ *
+ *    SIN[k] = sum{j=0->k-1}     COS[j].(k-j).U[k-j]/k
+ *    COS[k] = sum{j=0->k-1} +/- SIN[j].(k-j).U[k-j]/k
  */
 pair t_sin_cos (series SIN, series COS, series U, int k, bool trig);
 
 /*
  * Returns pointers to kth elements of both tangent and squared secant of U, results stored in user-supplied jets TAN and SEC2
+ *
+ *      TAN' =  SEC^2.U' = SECH^2.U'
+ *     SEC2' = 2.TAN.TAN' = -2.TANH.TANH'
+ *
+ *    TAN[k] = sum{j=0->k-1}     SEC2[j].(k-j).U[k-j]/k
+ *   SEC2[k] = sum{j=0->k-1} +/-2 TAN[j].(k-j).TAN[k-j]/k
  */
 pair t_tan_sec2 (series TAN, series SEC2, series U, int k, bool trig);
 
 /*
- * Returns a pointer to kth element of P = U^a (where a is scalar), results stored in user-supplied jet PWR
- */
-mpfr_t *t_pwr (series PWR, series U, mpfr_t a, int k);
-
-/*
  * Returns a pointer to kth element of the natural logarithm of U, results stored in user-supplied jet LN
+ *
+ * This is the simplest "reverse" example, where the wanted quantity is now on the RHS, and G = DU_DF!
+ *
+ *         LN' = [1 / U].U'
+ *          U' = U.LN'
+ *
+ *       LN[k] = (U[k] - sum{j=1->k-1} U[j].(k-j).LN[k-j]/k) / U[0]     "reverse"
  */
 mpfr_t *t_ln (series LN, series U, int k);
 
 /*
- * Returns pointers to kth elements of arcsin/arsinh of SIN, and G == 1 / DF_DU, results stored in user-supplied jets U and DU_DF
+ * Returns pointers to kth elements of arcsin/arsinh of SIN, and G == DF_DU, results stored in user-supplied jets U and G
+ *
+ *        SIN' =     G.U'  ==>  G = COS(U) = COSH(U)
+ *          G' =  -SIN.U' = SINH.U'
+ *
+ *        U[k] = (SIN[k] - sum{j=1->k-1} G[j].(k-j).U[k-j]/k) / G[0]
+ *
+ *        G[k] = sum{j=0->k-1} +/- SIN[j].(k-j).U[k-j]/k
  */
-pair t_asin (series U, series DU_DF, series SIN, int k, bool trig);
+pair t_asin (series U, series G, series SIN, int k, bool trig);
 
 /*
- * Returns pointers to kth elements of arccos/arcosh of COS, and G == 1 / DF_DU, results stored in user-supplied jets U and DU_DF
+ * Returns pointers to kth elements of arccos/arcosh of COS, and G == DF_DU, results stored in user-supplied jets U and G
+ *
+ *        COS' =   G.U'  ==>  G = -SIN(U) = SINH(U)
+ *          G' = COS.U' = COSH.U'
+ *
+ *        U[k] = (COS[k] -/+ sum{j=1->k-1} G[j].(k-j).U[k-j]/k) / G[0]
+ *
+ *        G[k] = sum{j=0->k-1} COS[j].(k-j).U[k-j]/k
  */
-pair t_acos (series U, series DU_DF, series COS, int k, bool trig);
+pair t_acos (series U, series G, series COS, int k, bool trig);
 
 /*
- * Returns pointers to kth elements of arctan/artanh of TAN, and G == 1 / DF_DU, results stored in user-supplied jets U and DU_DF
+ * Returns pointers to kth elements of arctan/artanh of TAN, and G == DF_DU, results stored in user-supplied jets U and G
+ *
+ *        TAN' =     G.U'  ==>  G = SEC^2(U) = SECH^2(U)
+ *          G' = 2.TAN.TAN' = -2.TANH.TANH'
+ *
+ *        U[k] = (TAN[k] - sum{j=1->k-1} G[j].(k-j).U[k-j]/k) / G[0]
+ *
+ *        G[k] = sum{j=0->k-1} +/-2 TAN[j].(k-j).TAN[k-j]/k
  */
-pair t_atan (series U, series DU_DF, series TAN, int k, bool trig);
+pair t_atan (series U, series G, series TAN, int k, bool trig);
+
+/*
+ * Returns kth element of P = U^a (where a is scalar), results stored in user-supplied jet PWR
+ *
+ *        dp/dt =   (dp/du).(du/dt)
+ *   PWR'= U^a' = a.U^(a-1).U'
+ *       U.U^a' = a.U^a.U'
+ *       U.PWR' = a.PWR.U'
+ *
+ *          P_k = a * sum{j=0->k-1} PWR[j].(k-j).U[k-j]/k                   (RHS)
+ *
+ *       PWR[k] = (P_k - sum{j=1->k-1} U[j].(k-j).PWR[k-j]/k) / U[0]        (LHS)
+ *
+ *              = (a * sum{j=0->k-1} PWR[j].(k-j).U[k-j] - sum{j=1->k-1} U[j].(k-j).PWR[k-j]) / k.U[0]
+ */
+mpfr_t *t_pwr (series PWR, series U, mpfr_t a, int k);
