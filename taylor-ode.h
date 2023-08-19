42145a6aaba7c16b318286239b744ad4ceb31576
Ian Smith <m4r35n357@gmail.com>
2023-08-19 09:34:02 +0100

    comments again

--- taylor-ode.h
+++ taylor-ode.h
@@ -82,7 +82,7 @@ triplet ode (series X, series Y, series Z, parameters *P, int k);
  */
 
 /*
- * Returns kth element of the absolute value of U, no user-supplied jet storage needed
+ * Returns kth element of the absolute value of U, no jet storage needed
  */
 real t_abs (series U, int k);
 
@@ -91,62 +91,62 @@ real t_abs (series U, int k);
  */
 
 /*
- * Returns kth element of the product of U and W, no user-supplied jet storage needed
+ * Returns kth element of the product of U and W, no jet storage needed
  */
 real t_mul (series U, series W, int k);
 
 /*
- * Returns kth element of the square of U, no user-supplied jet storage needed
+ * Returns kth element of the square of U, no jet storage needed
  */
 real t_sqr (series U, int k);
 
 /*
- * Returns kth element of U / W, results stored in user-supplied jet QUOT
+ * Returns kth element of U / W, results stored in jet QUOT
  */
 real t_div (series QUOT, series U, series W, int k);
 
 /*
- * Returns kth element of the square root of U, results stored in user-supplied jet ROOT
+ * Returns kth element of the square root of U, results stored in jet ROOT
  */
 real t_sqrt (series ROOT, series U, int k);
 
 /*
- * Returns kth element of the exponential of U, results stored in user-supplied jet EXP
+ * Returns kth element of the exponential of U, results stored in jet EXP
  */
 real t_exp (series EXP, series U, int k);
 
 /*
- * Returns kth elements of both sine and cosine of U, results stored in user-supplied jets SIN and COS
+ * Returns kth elements of both sine and cosine of U, results stored in jets SIN and COS
  */
 pair t_sin_cos (series SIN, series COS, series U, int k, bool trig);
 
 /*
- * Returns kth elements of both tangent and squared secant of U, results stored in user-supplied jets TAN and SEC2
+ * Returns kth elements of both tangent and squared secant of U, results stored in jets TAN and SEC2
  */
 pair t_tan_sec2 (series TAN, series SEC2, series U, int k, bool trig);
 
 /*
- * Returns kth element of the logarithm (inverse of EXP), results stored in user-supplied jet U
+ * Returns kth element of the logarithm (inverse of EXP), results stored in jet U
  */
 real t_ln (series U, series EXP, int k);
 
 /*
- * Returns kth elements of arcsin/arsinh of (inverse of SIN/SINH), results stored in user-supplied jets U and COSH
+ * Returns kth elements of arcsin/arsinh (inverse of SIN/SINH), results stored in jets U and COSH
  */
 pair t_asin (series U, series COSH, series SINH, int k, bool trig);
 
 /*
- * Returns kth elements of arccos/arcosh (inverse of COS/COSH), results stored in user-supplied jets U and SINH
+ * Returns kth elements of arccos/arcosh (inverse of COS/COSH), results stored in jets U and SINH
  */
 pair t_acos (series U, series SINH, series COSH, int k, bool trig);
 
 /*
- * Returns kth elements of arctan/artanh (inverse of TAN/TANH), results stored in user-supplied jets U and SEC2
+ * Returns kth elements of arctan/artanh (inverse of TAN/TANH), results stored in jets U and SEC2
  */
 pair t_atan (series U, series SEC2, series TAN, int k, bool trig);
 
 /*
- * Returns kth element of P = U^a (where a is scalar), results stored in user-supplied jet PWR
+ * Returns kth element of P = U^a (where a is scalar), results stored in jet PWR
  */
 real t_pwr (series PWR, series U, real a, int k);
 
