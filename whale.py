a7041c94ba98a6b541b77416f5803eee461c102c
Ian Smith <m4r35n357@gmail.com>
2023-12-23 16:47:34 +0000

    refactoring

--- whale.py
+++ whale.py
@@ -6,134 +6,112 @@ import copy # array-copying convenience
 import sys # max float
 
 # -------fitness functions---------
-
-# rastrigin function
-def fitness_rastrigin(position):
-	A = 10.0
+def rastrigin(position):
+	a = 10.0
 	n = len(position)
-	fitness_value = A * n
-	for i in range(len(position)):
-		xi = position[i]
-		fitness_value += xi * xi - A * math.cos(2 * math.pi * xi)
-	return fitness_value
+	value = a * n
+	for j in range(n):
+		xi = position[j]
+		value += xi * xi - a * math.cos(2.0 * math.pi * xi)
+	return value
 
+def sphere(position):
+	value = 0.0
+	for j in range(len(position)):
+		xi = position[j]
+		value += xi * xi
+	return value
 
-# sphere function
-def fitness_sphere(position):
-	fitness_value = 0.0
-	for i in range(len(position)):
-		xi = position[i]
-		fitness_value += (xi * xi)
-	return fitness_value
-
-
-# -------------------------
-
-
-# whale class
-class whale:
-	def __init__(self, fitness, dim, minx, maxx, seed):
+class Whale:
+	def __init__(self, cost, n, min_x, max_x, seed):
 		self.rnd = random.Random(seed)
-		self.position = [0.0 for i in range(dim)]
+		self.position = [0.0 for j in range(n)]
+		for j in range(n):
+			self.position[j] = ((max_x - min_x) * self.rnd.random() + min_x)
+		self.value = cost(self.position) # curr fitness
 
-		for i in range(dim):
-			self.position[i] = ((maxx - minx) * self.rnd.random() + minx)
-
-		self.fitness = fitness(self.position) # curr fitness
-
-
-# whale optimization algorithm(WOA)
-def woa(fitness, max_iter, n, dim, minx, maxx):
+def woa(cost, i_max, n_whales, n, min_x, max_x):
 	rnd = random.Random(0)
 
 	# create n random whales
-	whalePopulation = [whale(fitness, dim, minx, maxx, i) for i in range(n)]
+	whale = [Whale(cost, n, min_x, max_x, j) for j in range(n_whales)]
 
 	# compute the value of best_position and best_fitness in the whale Population
-	Xbest = [0.0 for i in range(dim)]
-	Fbest = sys.float_info.max
+	x_best = [0.0 for j in range(n)]
+	f_best = sys.float_info.max
 
-	for i in range(n): # check each whale
-		if whalePopulation[i].fitness < Fbest:
-			Fbest = whalePopulation[i].fitness
-			Xbest = copy.copy(whalePopulation[i].position)
+	for k in range(n_whales): # check each whale
+		if whale[k].value < f_best:
+			f_best = whale[k].value
+			x_best = copy.copy(whale[k].position)
 
 	# main loop of woa
-	Iter = 0
-	while Iter < max_iter:
-
-		# after every 10 iterations
-		# print iteration number and best fitness value so far
-		if Iter % 10 == 0 and Iter > 1:
-			print("Iter = " + str(Iter) + " best fitness = %.3f" % Fbest)
+	iteration = 0
+	while iteration < i_max:
+		# after every 10 iterations print iteration number and best fitness value so far
+		if iteration % 10 == 0 and iteration > 1:
+			print("Iter = " + str(iteration) + " best fitness = %.3f" % f_best)
 
 		# linearly decreased from 2 to 0
-		a = 2 * (1 - Iter / max_iter)
-		a2=-1+Iter*((-1)/max_iter)
+		a = 2 * (1 - iteration / i_max)
+		a2 = -1 + iteration * ((-1) / i_max)
 
-		for i in range(n):
+		for k in range(n_whales):
 			A = 2 * a * rnd.random() - a
 			C = 2 * rnd.random()
 			b = 1
-			l = (a2-1)*rnd.random()+1
+			l = (a2 - 1) * rnd.random() + 1
 			p = rnd.random()
 
-			D = [0.0 for i in range(dim)]
-			D1 = [0.0 for i in range(dim)]
-			Xnew = [0.0 for i in range(dim)]
-			Xrand = [0.0 for i in range(dim)]
+			d = [0.0 for j in range(n)]
+			d_1 = [0.0 for j in range(n)]
+			x_new = [0.0 for j in range(n)]
 			if p < 0.5:
 				if abs(A) > 1:
-					for j in range(dim):
-						D[j] = abs(C * Xbest[j] - whalePopulation[i].position[j])
-						Xnew[j] = Xbest[j] - A * D[j]
+					for j in range(n):
+						d[j] = abs(C * x_best[j] - whale[k].position[j])
+						x_new[j] = x_best[j] - A * d[j]
 				else:
-					p = random.randint(0, n - 1)
-					while p == i:
-						p = random.randint(0, n - 1)
+					p = random.randint(0, n_whales - 1)
+					while p == k:
+						p = random.randint(0, n_whales - 1)
 
-					Xrand = whalePopulation[p].position
+					x_rand = whale[p].position
 
-					for j in range(dim):
-						D[j] = abs(C * Xrand[j] - whalePopulation[i].position[j])
-						Xnew[j] = Xrand[j] - A * D[j]
+					for j in range(n):
+						d[j] = abs(C * x_rand[j] - whale[k].position[j])
+						x_new[j] = x_rand[j] - A * d[j]
 			else:
-				for j in range(dim):
-					D1[j] = abs(Xbest[j] - whalePopulation[i].position[j])
-					Xnew[j] = D1[j] * math.exp(b * l) * math.cos(2 * math.pi * l) + Xbest[j]
+				for j in range(n):
+					d_1[j] = abs(x_best[j] - whale[k].position[j])
+					x_new[j] = d_1[j] * math.exp(b * l) * math.cos(2 * math.pi * l) + x_best[j]
 
-			for j in range(dim):
-				whalePopulation[i].position[j] = Xnew[j]
+			for j in range(n):
+				whale[k].position[j] = x_new[j]
 
-		for i in range(n):
-			# if Xnew < minx OR Xnew > maxx
-			# then clip it
-			for j in range(dim):
-				whalePopulation[i].position[j] = max(whalePopulation[i].position[j], minx)
-				whalePopulation[i].position[j] = min(whalePopulation[i].position[j], maxx)
+		for k in range(n_whales):
+			# if Xnew < minx OR Xnew > maxx then clip it
+			for j in range(n):
+				whale[k].position[j] = max(whale[k].position[j], min_x)
+				whale[k].position[j] = min(whale[k].position[j], max_x)
 
-			whalePopulation[i].fitness = fitness(whalePopulation[i].position)
+			whale[k].value = cost(whale[k].position)
 
-			if whalePopulation[i].fitness < Fbest:
-				Xbest = copy.copy(whalePopulation[i].position)
-				Fbest = whalePopulation[i].fitness
+			if whale[k].value < f_best:
+				x_best = copy.copy(whale[k].position)
+				f_best = whale[k].value
 
 
-		Iter += 1
+		iteration += 1
 	# end-while
-
-	# returning the best solution
-	return Xbest
-
+	return x_best
 
 # ----------------------------
-
-
 # Driver code for rastrigin function
 
 print("\nBegin whale optimization algorithm on rastrigin function\n")
 dim = 3
-fitness = fitness_rastrigin
+model = rastrigin
 
 print("Goal is to minimize Rastrigin's function in " + str(dim) + " variables")
 print("Function has known min = 0.0 at (", end="")
@@ -148,12 +126,12 @@ print("Setting num_whales = " + str(num_whales))
 print("Setting max_iter = " + str(max_iter))
 print("\nStarting WOA algorithm\n")
 
-best_position = woa(fitness, max_iter, num_whales, dim, -10.0, 10.0)
+best_x = woa(model, max_iter, num_whales, dim, -10.0, 10.0)
 
 print("\nWOA completed\n")
 print("\nBest solution found:")
-print(["%.6f" % best_position[k] for k in range(dim)])
-err = fitness(best_position)
+print(["%.6f" % best_x[k] for k in range(dim)])
+err = model(best_x)
 print("fitness of best solution = %.6f" % err)
 
 print("\nEnd WOA for rastrigin\n")
@@ -164,7 +142,7 @@ print()
 # Driver code for Sphere function
 print("\nBegin whale optimization algorithm on sphere function\n")
 dim = 3
-fitness = fitness_sphere
+model = sphere
 
 print("Goal is to minimize sphere function in " + str(dim) + " variables")
 print("Function has known min = 0.0 at (", end="")
@@ -179,12 +157,12 @@ print("Setting num_whales = " + str(num_whales))
 print("Setting max_iter = " + str(max_iter))
 print("\nStarting WOA algorithm\n")
 
-best_position = woa(fitness, max_iter, num_whales, dim, -10.0, 10.0)
+best_x = woa(model, max_iter, num_whales, dim, -10.0, 10.0)
 
 print("\nWOA completed\n")
 print("\nBest solution found:")
-print(["%.6f" % best_position[k] for k in range(dim)])
-err = fitness(best_position)
+print(["%.6f" % best_x[k] for k in range(dim)])
+err = model(best_x)
 print("fitness of best solution = %.6f" % err)
 
 print("\nEnd WOA for sphere\n")
