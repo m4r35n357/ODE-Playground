2ab4a3f7aacaf953fff69aa22a011897e8d036fd
Ian Smith <m4r35n357@gmail.com>
2024-01-14 12:22:48 +0000

    dimensions

--- whale.py
+++ whale.py
@@ -6,25 +6,23 @@ from copy import copy # array-copying convenience
 from sys import float_info # max float
 
 class Whale:
-	def __init__(self, cost, n, min_x, max_x, seed):
+	def __init__(self, cost, n_dim, min_x, max_x, seed):
 		self.rnd = Random(seed)
-		self.x = [0.0 for _ in range(n)]
-		for j in range(n):
+		self.x = [0.0 for _ in range(n_dim)]
+		for j in range(n_dim):
 			self.x[j] = ((max_x - min_x) * self.rnd.random() + min_x)
 		self.value = cost(self.x) # curr fitness
 
-def woa(cost, i_max, n_whales, n, min_x, max_x):
+def woa(cost, i_max, n_whales, dim, min_x, max_x):
 	rnd = Random(0)
 	# create n random whales
-	whales = [Whale(cost, n, min_x, max_x, i) for i in range(n_whales)]
-	# compute the value of best_position and best_fitness in the whale Population
-	x_p = [0.0 for _ in range(n)]
+	whales = [Whale(cost, dim, min_x, max_x, i) for i in range(n_whales)]
+	x_p = [0.0 for _ in range(dim)]
 	f_best = float_info.max
 	for i in range(n_whales): # find best whale
 		if whales[i].value < f_best:
 			f_best = whales[i].value
 			x_p = copy(whales[i].x)
-	# main loop of woa
 	iteration = 0
 	while iteration < i_max:
 		if iteration % 10 == 0 and iteration > 1:
@@ -37,31 +35,31 @@ def woa(cost, i_max, n_whales, n, min_x, max_x):
 			l = 1.0 - (iteration / i_max + 2.0) * rnd.random()
 #			l = 2.0 * rnd.random() - 1.0
 			p = rnd.random()
-			d = [0.0 for _ in range(n)]
-			d_dash = [0.0 for _ in range(n)]
-			d_ddash = [0.0 for _ in range(n)]
-			x_new = [0.0 for _ in range(n)]
-			if p < 0.5:
+			d = [0.0 for _ in range(dim)]
+			d_dash = [0.0 for _ in range(dim)]
+			d_ddash = [0.0 for _ in range(dim)]
+			x_next = [0.0 for _ in range(dim)]
+			if rnd.random() < 0.5:
 				if abs(aa) > 1.0:
-					for j in range(n):
+					for j in range(dim):
 						d[j] = abs(cc * x_p[j] - whales[i].x[j])
-						x_new[j] = x_p[j] - aa * d[j]
+						x_next[j] = x_p[j] - aa * d[j]
 				else:
 					p = randint(0, n_whales - 1)
 					while p == i:
 						p = randint(0, n_whales - 1)
 					x_rand = whales[p].x
-					for j in range(n):
+					for j in range(dim):
 						d_ddash[j] = abs(cc * x_rand[j] - whales[i].x[j])
-						x_new[j] = x_rand[j] - aa * d_ddash[j]
+						x_next[j] = x_rand[j] - aa * d_ddash[j]
 			else:
-				for j in range(n):
+				for j in range(dim):
 					d_dash[j] = abs(x_p[j] - whales[i].x[j])
-					x_new[j] = d_dash[j] * exp(b * l) * cos(2 * pi * l) + x_p[j]
-			for j in range(n):
-				whales[i].x[j] = x_new[j]
+					x_next[j] = d_dash[j] * exp(b * l) * cos(2 * pi * l) + x_p[j]
+			for j in range(dim):
+				whales[i].x[j] = x_next[j]
 		for i in range(n_whales):
-			for j in range(n):
+			for j in range(dim):
 				whales[i].x[j] = max(whales[i].x[j], min_x)
 				whales[i].x[j] = min(whales[i].x[j], max_x)
 			whales[i].value = cost(whales[i].x)
