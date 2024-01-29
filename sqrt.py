afbbcdf53d314eab4607f4e07c0b7f24a685c9b2
Ian Smith <m4r35n357@gmail.com>
2024-01-29 21:48:42 +0000

    sqrt model

---
+++ sqrt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from sys import argv
+from math import sqrt, fabs
+from whale import woa
+
+def sq_rt(position):
+	value = 0.0
+	for j in range(len(position)):
+		xi = position[j]
+		value += sqrt(fabs(xi))
+	return value
+
+model = sq_rt
+if len(argv) == 1:
+	dim = 3
+	num_whales = 50
+	max_iter = 100
+else:
+	dim = int(argv[1])
+	num_whales = int(argv[2])
+	max_iter = int(argv[3])
+
+best_x = woa(model, max_iter, num_whales, dim, -10.0, 10.0)
+
+print("Best solution found:")
+print(["%.6f" % best_x[k] for k in range(dim)])
+print("fitness of best solution = %.6f\n" % model(best_x))
+for i in range(dim):
+	print("", best_x[i], end=" ")
+print("")
