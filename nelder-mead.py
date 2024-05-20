c1b1e0586d51b6286383bce6715d1b00d2aaa5b2
Ian Smith <m4r35n357@gmail.com>
2024-05-20 16:03:58 +0100

    get it working

--- nelder-mead.py
+++ nelder-mead.py
@@ -1,4 +1,5 @@
-from math import sqrt
+from math import sqrt, fsum
+from random import random
 from sys import stderr, argv
 from copy import copy
 from operator import attrgetter
@@ -13,6 +14,11 @@ class Simplex:
         n = len(x0)
         self.iterations = self.evaluations = 0
         self.vertices = [Point(n) for _ in range(n + 1)]
+        self.centroid = Point(n)
+        self.reflect = Point(n)
+        self.expand = Point(n)
+        self.contract_out = Point(n)
+        self.contract_in = Point(n)
         b = 0.0
         for j in range(n):
             c = sqrt(1.0 - b)
@@ -26,63 +32,7 @@ class Simplex:
                 self.vertices[i].x[j] = size * self.vertices[i].x[j] + x0[j]
             self.vertices[i].f = function(self.vertices[i].x)
             self.evaluations += 1
-        self.vertices.sort(key=attrgetter('f'))
-
-def project(start, factor, pa, pb):
-    return [start.x[j] + factor * (pb.x[j] - pa.x[j]) for j in range(len(start))]
-
-def nelder_mead(function, n, max_iterations, s):
-    alpha = 1.0
-    gamma = 2.0
-    rho = 0.5
-    sigma = 0.5
-    centroid = [0.0 for _ in range(n)]
-    for j in range(n):
-        centroid[j] = 0.0
-        for i in range(n):
-            centroid[j] += s.vertices[i].x[j]
-    reflect = Point(n)
-    reflect.x = [centroid[j] + alpha * (s.vertices[n].x[j] - centroid[j]) for j in range(n)]
-    reflect.f = function(reflect.x)
-    s.evaluations += 1
-    while s.iterations < max_iterations:
-        shrink = False
-        if s.vertices[0].f <= reflect.f < s.vertices[n - 1].f:
-            s.vertices[n] = copy(reflect)
-        elif reflect.f < s.vertices[0].f:
-            expand = Point(n)
-            expand.x = [centroid[j] + gamma * (s.vertices[n].x[j] - centroid[j]) for j in range(n)]
-            expand.f = function(expand.x)
-            s.evaluations += 1
-            if expand.f < reflect.f:
-                s.vertices[n] = copy(expand)
-            else:
-                s.vertices[n] = copy(reflect)
-        elif reflect.f < s.vertices[n].f:
-            contract_out = Point(n)
-            contract_out.x = [centroid[j] + rho * (s.vertices[n].x[j] - centroid[j]) for j in range(n)]
-            contract_out.f = function(contract_out.x)
-            s.evaluations += 1
-            if contract_out.f < reflect.f:
-                shrink = True
-        else:
-            contract_in = Point(n)
-            contract_in.x = [centroid[j] - rho * (s.vertices[n].x[j] - centroid[j]) for j in range(n)]
-            contract_in.f = function(contract_in.x)
-            s.evaluations += 1
-            if contract_in.f < reflect.f:
-                shrink = True
-        if shrink:
-            for i in range(n + 1):
-                for j in range(n):
-                    s.vertices[i].x[j] = [s.vertices[0].x[j] - sigma * (s.vertices[i].x[j] - s.vertices[0].x[j]) for j in range(n)]
-                s.vertices[i].f = function(s.vertices[i].x)
-                s.evaluations += 1
-        s.vertices.sort(key=attrgetter('f'))
-        s.iterations += 1
-        print(f'{s.iterations:4} {s.evaluations:6}  [ ', end='')
-        print(''.join(f'{term: .6f} ' for term in s.vertices[0].x), end='')
-        print(f']  {s.vertices[0].f: .6f} ')
+        sort(self, n)
 
 def rosenbrock(position):
     value = 0.0
@@ -91,10 +41,60 @@ def rosenbrock(position):
         value += (1.0 - xi)**2 + 100.0 * (position[j + 1] - xi * xi)**2
     return value
 
+def project(new, s, function, n, factor, pa, pb):
+    new.x = [pb.x[j] + factor * (pb.x[j] - pa.x[j]) for j in range(n)]
+    new.f = function(new.x)
+    s.evaluations += 1
+    return new
+
+def distance (a, b, n):
+    return sqrt(fsum((a.x[i] - b.x[i])**2 for i in range(n)))
+
+def sort (s, n):
+    s.vertices.sort(key=attrgetter('f'))
+    for j in range(n):
+        s.centroid.x[j] = 0.0
+        for i in range(n):
+            s.centroid.x[j] += s.vertices[i].x[j]
+        s.centroid.x[j] /= n
+
+def nelder_mead(function, n, max_iterations, s):
+    while ((distance(s.vertices[0], s.vertices[n], n) > 1.0e-6) or (s.vertices[0].f - s.vertices[n].f > 1.0e-6)) and s.iterations < max_iterations:
+        shrink = False
+        s.reflect = project(s.reflect, s, function, n, 1.0, s.vertices[n], s.centroid)
+        if s.vertices[0].f <= s.reflect.f < s.vertices[n - 1].f:
+            s.vertices[n] = copy(s.reflect)
+        elif s.reflect.f < s.vertices[0].f:
+            s.expand = project(s.expand, s, function, n, 2.0, s.vertices[n], s.centroid)
+            if s.expand.f < s.reflect.f:
+                s.vertices[n] = copy(s.expand)
+            else:
+                s.vertices[n] = copy(s.reflect)
+        elif s.reflect.f < s.vertices[n].f:
+            s.contract_out = project(s.contract_out, s, function, n, 0.5, s.vertices[n], s.centroid)
+            if s.contract_out.f < s.reflect.f:
+                s.vertices[n] = copy(s.contract_out)
+            else:
+                shrink = True
+        else:
+            s.contract_in = project(s.contract_in, s, function, n, -0.5, s.vertices[n], s.centroid)
+            if s.contract_in.f < s.vertices[n].f:
+                s.vertices[n] = copy(s.contract_in)
+            else:
+                shrink = True
+        if shrink:
+            for i in range(1, n + 1):
+                s.vertices[i] = project(s.vertices[i], s, function, n, -0.5, s.vertices[i], s.vertices[0])
+        sort(s, n)
+        s.iterations += 1
+        print(f'{s.iterations:4} {s.evaluations:6}  [ ', end='')
+        print(''.join(f'{term: .6f} ' for term in s.vertices[0].x), end='')
+        print(f']  {s.vertices[0].f: .6f} ')
+
 if __name__ == "__main__":
     dim = int(argv[1])
     max_iter = int(argv[2])
-    initial_point = [0.0, 0.0, 0.0]
+    initial_point = [(10.0 * random()) for _ in range(dim)]
     initial_simplex = Simplex(rosenbrock, initial_point, 1.0)
     nelder_mead(rosenbrock, dim, max_iter, initial_simplex)
 else:
