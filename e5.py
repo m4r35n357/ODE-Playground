31b2762842444f3f123e0bf3bd221feb20a33d32
Ian Smith <m4r35n357@gmail.com>
2024-06-19 18:26:24 +0100

    improve frequency response plotting for e5

--- e5.py
+++ e5.py
@@ -2,7 +2,7 @@ from sys import argv
 from math import log10
 import matplotlib.pyplot as plt
 
-def e5(n, p, w):
+def e5(p, w):
     g = complex(1.0, w * p[0])
     g = 1.0 / g + 1.0 / (complex(0.0, w * p[2]) + 1.0 / complex(0.0, w * p[1]))
     g = 1.0 / g + complex(0.0, w * p[3])
@@ -10,25 +10,31 @@ def e5(n, p, w):
     g = 1.0 / g + complex(0.0, w * p[0])
     return 1.0 - abs((g - 1.0) / (g + 1.0))**2
 
-dim = int(argv[1])
-values = [0.0] * dim
-for i in range(dim):
-    values[i] = float(argv[i + 2])
-L = 1025
-w_axis = [0.0] * L
-response = [0.0] * L
-for i in range(L):
-    w_axis[i] = pow(10.0, 5.0 * (2.0 * i / L - 1.0))
-    t = e5(dim, values, w_axis[i])
-    response[i] = 10.0 * log10(t) if t > 1.0e-18 else -180.0
-
-if len(argv) == 8:
+if len(argv) == 11:
+    pb = float(argv[1])
+    sb = float(argv[2])
+    ksi = float(argv[3])
+    dim = int(argv[4])
+    values = [0.0] * dim
+    for i in range(dim):
+        values[i] = float(argv[i + 5])
+    L = 1025
+    w_axis = [0.0] * L
+    response = [0.0] * L
+    for i in range(L):
+        w_axis[i] = pow(10.0, 5.0 * (2.0 * i / L - 1.0))
+        t = e5(values, w_axis[i])
+        response[i] = 10.0 * log10(t) if t > 1.0e-18 else -180.0
     plt.figure()
     plt.semilogx()
     plt.plot(w_axis, response)
     plt.xlabel('w / w0')
     plt.ylabel('Transmission [dB]')
+    plt.hlines(y=10.0 * log10(pb), xmin=1.0e-5, xmax=1.0e5, colors='r')
+    plt.hlines(y=10.0 * log10(sb), xmin=1.0e-5, xmax=1.0e5, colors='r')
+    plt.vlines(x=1.0, ymin=-70.0, ymax=0.0, colors='b')
+    plt.vlines(x=ksi, ymin=-70.0, ymax=0.0, colors='b')
     plt.grid()
     plt.show()
 else:
-    raise Exception(f'>>> Wrong number of arguments ({len(argv):8})<<<')
+    raise Exception(f'>>> Wrong number of arguments ({len(argv):11})<<<')
