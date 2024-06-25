5a73acb9f01621a4fc86cd3556aa10a7c8ba629d
Ian Smith <m4r35n357@gmail.com>
2024-06-25 13:28:21 +0100

    renaming

--- nelder_mead.py
+++ nelder_mead.py
@@ -10,7 +10,7 @@ class Point:
         self.f = 0.0
 
 class Simplex:
-    def __init__(self, function, x0, size):
+    def __init__(self, cost, x0, size):
         n = len(x0)
         self.iterations = self.evaluations = 0
         self.vertices = [Point(n) for _ in range(n + 1)]
@@ -29,13 +29,13 @@ class Simplex:
             b += r * r
         for i in range(n + 1):
             self.vertices[i].x = [size * self.vertices[i].x[j] + x0[j] for j in range(n)]
-            self.vertices[i].f = function(self.vertices[i].x)
+            self.vertices[i].f = cost(self.vertices[i].x)
             self.evaluations += 1
         sort(self, n)
 
-def project(new, s, function, n, factor, pa, pb):
+def project(new, s, cost, n, factor, pa, pb):
     new.x = [pb.x[j] + factor * (pb.x[j] - pa.x[j]) for j in range(n)]
-    new.f = function(new.x)
+    new.f = cost(new.x)
     s.evaluations += 1
     return new
 
@@ -46,33 +46,33 @@ def sort (s, n):
     s.vertices.sort(key=attrgetter('f'))
     s.centroid.x = [fsum(s.vertices[i].x[j] for i in range(n)) / n for j in range(n)]
 
-def nelder_mead(function, n, max_iterations, s):
+def nelder_mead(cost, n, max_iterations, s):
     while ((distance(s.vertices[0], s.vertices[n], n) > 1.0e-6) or (s.vertices[0].f - s.vertices[n].f > 1.0e-6)) and s.iterations < max_iterations:
         shrink = False
-        s.reflect = project(s.reflect, s, function, n, 1.0, s.vertices[n], s.centroid)
+        s.reflect = project(s.reflect, s, cost, n, 1.0, s.vertices[n], s.centroid)
         if s.vertices[0].f <= s.reflect.f < s.vertices[n - 1].f:
             s.vertices[n] = copy(s.reflect)
         elif s.reflect.f < s.vertices[0].f:
-            s.expand = project(s.expand, s, function, n, 2.0, s.vertices[n], s.centroid)
+            s.expand = project(s.expand, s, cost, n, 2.0, s.vertices[n], s.centroid)
             if s.expand.f < s.reflect.f:
                 s.vertices[n] = copy(s.expand)
             else:
                 s.vertices[n] = copy(s.reflect)
         elif s.reflect.f < s.vertices[n].f:
-            s.contract_out = project(s.contract_out, s, function, n, 0.5, s.vertices[n], s.centroid)
+            s.contract_out = project(s.contract_out, s, cost, n, 0.5, s.vertices[n], s.centroid)
             if s.contract_out.f < s.reflect.f:
                 s.vertices[n] = copy(s.contract_out)
             else:
                 shrink = True
         else:
-            s.contract_in = project(s.contract_in, s, function, n, -0.5, s.vertices[n], s.centroid)
+            s.contract_in = project(s.contract_in, s, cost, n, -0.5, s.vertices[n], s.centroid)
             if s.contract_in.f < s.vertices[n].f:
                 s.vertices[n] = copy(s.contract_in)
             else:
                 shrink = True
         if shrink:
             for i in range(1, n + 1):
-                s.vertices[i] = project(s.vertices[i], s, function, n, -0.5, s.vertices[i], s.vertices[0])
+                s.vertices[i] = project(s.vertices[i], s, cost, n, -0.5, s.vertices[i], s.vertices[0])
         sort(s, n)
         s.iterations += 1
         print(f'{s.iterations:4} {s.evaluations:6}  [ ', end='')
