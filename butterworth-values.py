ee3ecbf73cd03ad2d3df9690eea758b8514f9802
Ian Smith <m4r35n357@gmail.com>
2024-11-26 10:24:34 +0000

    remove old filter files (see VisualOptimizers project)

--- butterworth-values.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-from math import pi, sin
-
-order = int(argv[1])
-for i in range(order):
-    print(f'{2.0 * sin((2.0 * (i + 1.0) - 1.0) * pi / (2 * order)): .6f} ', end='')
-print()
