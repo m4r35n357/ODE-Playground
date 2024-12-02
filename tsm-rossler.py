66cba2cdc856749bf24c2c0b9ff1a9b8529aa86c
Ian Smith <m4r35n357@gmail.com>
2024-12-02 14:44:41 +0000

    Python & bc rossler reorder bc functions

---
+++ tsm-rossler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+#  (c) 2018-2023 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
+
+from sys import argv
+from collections import namedtuple
+from ad import tsm, t_prod, Components, t_jet
+
+class Parameters(namedtuple('ParametersType', ['a', 'b', 'c'])):
+    pass
+
+def get_p(order):
+    return Parameters(a = float(argv[8]),
+                      b = t_jet(order, float(argv[9])),
+                      c = float(argv[10]))
+
+def ode(x, y, z, p, i, k):
+    return Components(x = - y[k] - z[k],
+                      y = x[k] + p.a * y[k],
+                      z = p.b[k] + t_prod(x, z, k) - p.c * z[k])
+
+tsm(ode, get_p, None)
