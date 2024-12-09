d392bdc20f9dbcc045d4c37115ae90856262d946
Ian Smith <m4r35n357@gmail.com>
2024-12-09 19:22:42 +0000

    get print_args from c branch

---
+++ print_args.py
@@ -0,0 +1,9 @@
+from sys import argv, stderr
+
+print(f'\033[1;30margs \033[0m{len(argv)}\033[1;30m, argv [\033[0;33m', file=stderr, end='')
+for i in range(len(argv)):
+    print(f' {argv[i]}', file=stderr, end='')
+print(" \033[1;30m]\033[0m", file=stderr)
+
+print(f'{__name__} module loaded', file=stderr)
+
