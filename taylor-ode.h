10bc4817b00fea5f3d39f8a5d49d6d7360fc8ab0
Ian Smith <m4r35n357@gmail.com>
2025-01-01 12:45:17 +0000

    remove obsolete docs - see pure_c branch now!

--- taylor-ode.h
+++ taylor-ode.h
@@ -39,38 +39,6 @@ typedef struct triple_m {
 } triplet;
 
 /*
- * The Taylor Series Method (TSM) in brief; to solve a system of Ordinary Differential Equations defined by:
- *
- *                         v(t) = ode(u(t))                  (where u represents x, y, z coordinates)
- *
- * We plug u(t) into the equations, then use the values of the first derivatives v(t) to estimate the next u values in the sequence.
- *
- * Now, a time-varying quantity x can be represented locally (instantaneously) as a Taylor Series:
- *
- *             u(t0 + h) = u(t) = sum{k=0->inf} U[k].h^k    where U[0] = u(t0), U[k] = (d/dt)^k u(t0) / k! and h = t - t0  (A)
- *
- * (this summation is best performed using Horner's method.) Similarly, the velocity v can be represented as:
- *
- *                         v(t) = sum{k=0->inf} V[k].h^k                                                                   (B)
- *
- * Where V[k] is the result of evaluating the ODE equation, but with all variables expressed as Taylor Series, X[k].
- *
- *                         V[k] = ODE(U[k])
- *
- * Furthermore, by explicitly differentiating (A) wrt t, we obtain an alternative description of the velocity:
- *
- *                    d/dt u(t) = sum{k=1->inf} k.U[k].h^(k-1)
- *
- *                              = sum{k=0->inf} (k+1).U[k+1].h^k                                                           (C)
- *
- * Comparing (B) and (C),  V[k] = (k+1).U[k+1]        *** this IDENTITY is also used in deriving the recurrences below ***
- *
- *                   ==> U[k+1] = V[k] / (k + 1)
- *
- * which (together with all lower U derivatives) we then use to find V[k+1], and so on . . ..
- */
-
-/*
  * Prints a line of data to stdout
  */
 void _out_ (real x, real y, real z, real step_size, int step, clock_t since);
@@ -101,10 +69,6 @@ real *horner (const series U, int order, real step_size);
 void tsm (int order, real step_size, int steps, triplet *V, xyz *JETS, const model *p, clock_t since);
 
 /*
- * Obligatory client method signatures
- */
-
-/*
  * Populate parameter data from command arguments
  */
 model *tsm_init_p (int argc, char **argv, int order);
@@ -115,231 +79,71 @@ model *tsm_init_p (int argc, char **argv, int order);
 void ode (triplet *V, series X, series Y, series Z, const model *p, int k);
 
 /*
- * Basic Taylor Series functions
- */
-
-/*
  * Returns a pointer to kth element of the absolute value of U, no jet storage needed
  */
 real *t_abs (const series U, int k);
 
 /*
- * Taylor Series recurrence relationships
- */
-
-/*
- * Cauchy product for C = A.B
- *
- *   if c(t) = a(t).b(t)
- *
- * then c(t) = sum{k=0->inf} C(k).h^k
- *
- *           = sum{j=0->inf} A(j).h^j sum{i=0->inf} B(i).h^i
- *
- *           = sum{k=0->inf} sum{j=0->k} A[j].B[k - j].h^k     where k = i + j  ==>  i = k - j
- *
- *  ==> C[k] = sum{j=0->k} A[j].B[k-j] = sum{j=0->k} A[k-j].B[j]
- */
-
-/*
  * Returns a pointer to kth element of the product of U and V
- *
- *     PROD = U.V
- *
- *   PROD_k = sum{j=0->k} U[j].V[k-j]
  */
 real *t_mul (const series U, const series V, const int k);
 
 /*
  * Returns a pointer to kth element of the square of U
- *
- *     SQR = U.U
- *
- *   SQR_k = sum{j=0->k} U[j].U[k-j]
  */
 real *t_sqr (const series U, int k);
 
 /*
  * Calculates kth element of U / V, results stored in QUOT
- *
- *     QUOT = U / V ==> U = QUOT.V
- *
- *                   U[k] = sum{j=0->k} QUOT[j].V[k-j]
- *
- *                        = sum{j=0->k-1} QUOT[j].V[k-j] + QUOT[k].V[0]
- *
- *                QUOT[k] = (U[k] - sum{j=0->k-1} QUOT[j].V[k-j]) / V[0]
- *
- *                        =  U[0] / V[0]                                    if k == 0
- *
- *                        = (U[k] - sum{j=0->k-1} QUOT[j].V[k-j]) / V[0]    otherwise
  */
 void t_div (series QUOT, const series U, const series V, int k);
 
 /*
  * Calculates kth element of 1 / V, results stored in REC,
- *
- * from above,    REC[k] = 1.0 / V[0]                                      if k == 0
- *
- *                REC[k] = - sum{j=0->k-1} REC[j].V[k-j] / V[0]           otherwise
  */
 void t_rec (series REC, const series V, int k);
 
 /*
  * Calculates kth element of the square root of U, results stored in ROOT
- *
- *        U = ROOT.ROOT
- *
- *     U[k] = sum{j=0->k} ROOT[j].ROOT[k - j]
- *
- *          = sum{j=1->k-1} ROOT[j].ROOT[k - j] + 2.ROOT[k].ROOT[0]
- *
- *  ROOT[k] = (U[k] - sum{j=1->k-1} ROOT[j].ROOT[k-j]) / 2.ROOT[0]
  */
 void t_sqrt (series ROOT, const series U, int k);
 
 /*
- * Applying the chain rule for the derivative of a composed function f(u(t)) creates another Cauchy product:
- *
- *           F' = (df/dt) = (df/du).(du/dt) = dFdU.U'
- *
- *  Using F'[k] = (k+1).F[k+1]   (the IDENTITY from earlier)
- *
- * ==>  F'[k-1] = k.F[k], because we WANT F[k], and we can now replace F' with F, and U' with U as follows:
- *
- * Starting from the Cauchy product:
- *
- *        F'[k] = sum{j=1->k} dFdU[j].U'[k-j]              differentiated series has one fewer terms
- *
- *  rewrite it with j index starting from 0, for "neatness":
- *
- *      F'[k-1] = sum{j=0->k-1} dFdU[j].U'[k-1-j]
- *
- * then make the IDENTITY substitutions
- *
- *       k.F[k] = sum{j=0->k-1} dFdU[j].(k-j).U[k-j]         only for k > 0.  Use a mathematical function call for G[0]
- *
- *     ==> F[k] = sum{j=0->k-1} dFdU[j].(k-j).U[k-j] / k                             "forward"
- *
- *              = dFdU[0].U[k] + sum{j=1->k-1} dFdU[j].(k-j).U[k-j] / k
- *
- *     ==> U[k] = (F[k] - sum{j=1->k-1} dFdU[j].(k-j).U[k-j]) / k) / dFdU[0]         "inverse"
- */
-
-/*
  * Calculates kth element of the exponential of U, results stored in EXP
- *
- *      EXP' = dFdU.U' = EXP.U'
- *
- *    EXP[0] = exp(U[0])
- *
- *    EXP[k] = sum{j=0->k-1} EXP[j].(k-j).U[k-j] / k
  */
 void t_exp (series EXP, const series U, int k);
 
 /*
  * Calculates kth elements of both sine and cosine of U, results stored in SIN and COS
- *
- *      SINH' =   dFdU.U' = COSH.U' (=  COS.U')
- *      COSH' = d2FdU2.U' = SINH.U' (= -SIN.U')
- *
- *    SINH[0] = sinh(U[0])
- *
- *    COSH[0] = cosh(U[0])
- *
- *    SINH[k] = sum{j=0->k-1} COSH[j].(k-j).U[k-j] / k
- *
- *    COSH[k] = sum{j=0->k-1} SINH[j].(k-j).U[k-j] / k
  */
 void t_sin_cos (series SIN, series COS, const series U, int k, bool trig);
 
 /*
  * Calculates kth elements of both tan(h) and sec(h)^2 of U, results stored in TAN(H) and SEC(H)2
- *
- *      TAN' =   dFdU.U' = SEC^2.U'   (=   SECH2.U')
- *     SEC2' = d2FdU2.U' = 2.TAN.TAN' (= -2.TANH.TANH')
- *
- *    TAN[0] = tan(U[0])
- *
- *   SEC2[0] = sec(U[0]).sec(U[0])
- *
- *    TAN[k] = sum{j=0->k-1}  SEC2[j].(k-j).U[k-j] / k
- *
- *   SEC2[k] = sum{j=0->k-1} 2.TAN[j].(k-j).TAN[k-j] / k
  */
 void t_tan_sec2 (series TAN, series SEC2, const series U, int k, bool trig);
 
 /*
  * Calculates kth element of the logarithm (inverse of EXP), results stored in U
- *
- * This is the simplest "reverse" example, where the wanted quantity is now on the RHS
- *
- *        EXP' =   dFdU.U' = EXP.U'
- *
- *        U[0] = ln(EXP[0])
- *
- *        U[k] = (EXP[k] - sum{j=1->k-1} EXP[j].(k-j).U[k-j] / k) / EXP[0]       "reverse"
  */
 void t_ln (series U, const series EXP, int k);
 
 /*
  * Calculates kth elements of arcsin/arsinh (inverse of SIN/SINH), results stored in U and COSH
- *
- *       SINH' =   dFdU.U' = COSH.U' (=  COS.U')
- *       COSH' = d2FdU2.U' = SINH.U' (= -SIN.U')
- *
- *        U[0] = arsinh(SINH[0])
- *
- *     COSH[0] = cosh(U[0])
- *
- *        U[k] = (SINH[k] - sum{j=1->k-1} COSH[j].(k-j).U[k-j] / k) / COSH[0]    "reverse"
- *
- *     COSH[k] = sum{j=0->k-1} SINH[j].(k-j).U[k-j] / k                          "forward"
  */
 void t_asin_cos (series U, series COSH, const series SINH, int k, bool trig);
 
 /*
  * Calculates kth elements of arccos/arcosh (inverse of COS/COSH), results stored in U and SINH
- *
- *       COSH' =   dFdU.U' = SINH.U' (= -SIN.U')
- *       SINH' = d2FdU2.U' = COSH.U' (=  COS.U')
- *
- *        U[0] = arcosh(COSH[0])
- *
- *     SINH[0] = sinh(U[0])
- *
- *        U[k] = (COSH[k] - sum{j=1->k-1} SINH[j].(k-j).U[k-j] / k) / SINH[0]    "reverse"
- *
- *     SINH[k] = sum{j=0->k-1} COSH[j].(k-j).U[k-j] / k                          "forward"
  */
 void t_acos_sin (series U, series SINH, const series COSH, int k, bool trig);
 
 /*
  * Calculates kth elements of arctan/artanh (inverse of TAN/TANH), results stored in U and SEC2
- *
- *        TAN' =   dFdU.U' = SEC2.U'    (=  SECH^2.U')
- *       SEC2' = d2FdU2.U' = 2.TAN.TAN' (= -2.TANH.TANH')
- *
- *        U[0] = arctan(TAN[0])
- *
- *     SEC2[0] = sec(U[0]).sec(U[0])
- *
- *        U[k] = (TAN[k] - sum{j=1->k-1} SEC2[j].(k-j).U[k-j] / k) / SEC2[0]     "reverse"
- *
- *     SEC2[k] = sum{j=0->k-1} 2.TAN[j].(k-j).TAN[k-j] / k                       "forward"
  */
 void t_atan_sec2 (series U, series SEC2, const series TAN, int k, bool trig);
 
 /*
  * Calculates kth element of P = U^a (where a is scalar), results stored in PWR
- *
- *        dp/dt =   (dp/du).(du/dt)
- *   PWR'= U^a' = a.U^(a-1).U'
- *       U.U^a' = a.U^a.U'
- *       U.PWR' = a.PWR.U' = E_k
- *
- *          E_k = a * sum{j=0->k-1} PWR[j].(k-j).U[k-j]/k                   (RHS)
- *
- *       PWR[k] = (E_k - sum{j=1->k-1} U[j].(k-j).PWR[k-j]/k) / U[0]        (LHS)
  */
 void t_pwr (series PWR, series const U, real a, int k);
