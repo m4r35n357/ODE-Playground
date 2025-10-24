b6fbf237e762b1d3bad2a964a010fc27374dfd3e
Ian Smith <m4r35n357@gmail.com>
2025-10-24 16:52:23 +0100

    better CNS threshold range and default sync with pure_c playground setup

--- base.sh
+++ base.sh
@@ -1,15 +1,15 @@
 #
 #  (c) 2018-2025 m4r35n357@gmail.com (Ian Smith), for licencing see the LICENCE file
 
-GRY='\e[1;30m'
-RED='\e[1;31m'
-GRN='\e[1;32m'
-YLW='\e[1;33m'
-BLU='\e[1;34m'
-MGT='\e[0;35m'
-CYN='\e[0;36m'
-WHT='\e[1;37m'
-NRM='\e[0m'
+GRY='\033[1;30m'
+RED='\033[1;31m'
+GRN='\033[1;32m'
+YLW='\033[1;33m'
+BLU='\033[1;34m'
+MGT='\033[0;35m'
+CYN='\033[0;36m'
+WHT='\033[1;37m'
+NRM='\033[0m'
 
 args="$0 $*"
 echo "${GRY}args ${NRM}$(($# + 1))${GRY}, argv [ ${MGT}${args}${GRY} ]${NRM}" >&2
